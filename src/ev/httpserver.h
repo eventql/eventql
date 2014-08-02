4c80bb977412ef2c72dd0c58c775ba103369fc61
Paul Asmuth <paul@paulasmuth.com>
2014-08-02 19:26:40 +0100

    web interface mockup, codemirror, http server stub

---
+++ src/ev/httpserver.h
@@ -0,0 +1,28 @@
+/**
+ * This file is part of the "FnordMetric" project
+ *   Copyright (c) 2011-2014 Paul Asmuth, Google Inc.
+ *
+ * FnordMetric is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License v3.0. You should have received a
+ * copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _FNORDMETRIC_WEB_HTTPSERVER_H
+#define _FNORDMETRIC_WEB_HTTPSERVER_H
+#include <memory>
+#include <vector>
+#include "../ev/acceptor.h"
+
+namespace fnordmetric {
+namespace ev {
+
+class ThreadedHTTPServer : public ev::Acceptor::CallbackInterface {
+public:
+  void onConnection(int fd) override;
+protected:
+};
+
+}
+}
+#endif
