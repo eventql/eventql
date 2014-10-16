ee77cd9736ca0c1dda4fb0c15a6c24c0d4500633
Christian Parpart <trapni@gmail.com>
2014-10-16 04:59:21 -0400

    ConsoleLogger: adds some thread-ID hint to log message

--- src/logging/LogTarget.cpp
+++ src/logging/LogTarget.cpp
@@ -1,4 +1,6 @@
 #include <xzero/logging/LogTarget.h>
+#include <unordered_map>
+#include <mutex>
 #include <stdio.h>
 #include <stdarg.h>
 
@@ -8,24 +10,50 @@ namespace xzero {
 class ConsoleLogger : public LogTarget {
  public:
   void trace(const std::string& msg) override {
-    fprintf(stderr, "[trace] %s\n", msg.c_str());
+    std::lock_guard<std::mutex> _lg(lock_);
+    fprintf(stderr, "[thread:%d] [trace] %s\n", threadId(), msg.c_str());
   }
 
   void debug(const std::string& msg) override {
-    fprintf(stderr, "[debug] %s\n", msg.c_str());
+    std::lock_guard<std::mutex> _lg(lock_);
+    fprintf(stderr, "[thread:%d] [debug] %s\n", threadId(), msg.c_str());
   }
 
   void info(const std::string& msg) override {
-    fprintf(stderr, "[info] %s\n", msg.c_str());
+    std::lock_guard<std::mutex> _lg(lock_);
+    fprintf(stderr, "[thread:%d] [info] %s\n", threadId(), msg.c_str());
   }
 
   void warn(const std::string& msg) override {
-    fprintf(stderr, "[warning] %s\n", msg.c_str());
+    std::lock_guard<std::mutex> _lg(lock_);
+    fprintf(stderr, "[thread:%d] [warning] %s\n", threadId(), msg.c_str());
   }
 
   void error(const std::string& msg) override {
-    fprintf(stderr, "[error] %s\n", msg.c_str());
+    std::lock_guard<std::mutex> _lg(lock_);
+    fprintf(stderr, "[thread:%d] [error] %s\n", threadId(), msg.c_str());
   }
+
+  int threadId() {
+    pthread_t tid = pthread_self();
+    auto i = threadMap_.find(tid);
+    if (i != threadMap_.end()) {
+      indent(i->second);
+      return i->second;
+    }
+    threadMap_[tid] = threadMap_.size() + 1;
+    indent(threadMap_.size());
+    return threadMap_.size();
+  }
+
+  void indent(int t) {
+    for (int i = 1; i < t; ++i) {
+      fprintf(stderr, "  ");
+    }
+  }
+
+  std::mutex lock_;
+  std::unordered_map<pthread_t, int> threadMap_;
 
   static ConsoleLogger* get();
 };
