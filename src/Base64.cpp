43f10a8de653d32b44b83d1952b6ccc062809040
Christian Parpart <trapni@gmail.com>
2014-11-04 22:44:47 +0100

    copyright notice

--- src/Base64.cpp
+++ src/Base64.cpp
@@ -5,6 +5,13 @@
 // file except in compliance with the License. You may obtain a copy of
 // the License at: http://opensource.org/licenses/MIT
 
+// This file is part of the "x0" project, http://xzero.io/
+//   (c) 2009-2014 Christian Parpart <trapni@gmail.com>
+//
+// Licensed under the MIT License (the "License"); you may not use this
+// file except in compliance with the License. You may obtain a copy of
+// the License at: http://opensource.org/licenses/MIT
+
 #include <xzero/Base64.h>
 
 namespace xzero {
