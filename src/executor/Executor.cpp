f25eb9bad931c0b909b7e3bb580902d93d2d6db8
Christian Parpart <trapni@gmail.com>
2015-02-07 01:08:23 +0100

    make use of new SafeCall API in Executor base class

--- src/executor/Executor.cpp
+++ src/executor/Executor.cpp
@@ -1,40 +1,12 @@
 #include <xzero/executor/Executor.h>
-#include <xzero/RuntimeError.h>
 
 namespace xzero {
 
-Executor::Executor(
-    std::function<void(const std::exception&)>&& eh)
-    : exceptionHandler_(std::bind(&consoleLogger, std::placeholders::_1)) {
+Executor::Executor(std::function<void(const std::exception&)>&& eh)
+    : SafeCall(eh) {
 }
 
 Executor::~Executor() {
-}
-
-void Executor::setExceptionHandler(
-    std::function<void(const std::exception&)>&& eh) {
-
-  exceptionHandler_ = std::move(eh);
-}
-
-void Executor::safeCall(const Task& task) XZERO_NOEXCEPT {
-  try {
-    if (task) {
-      task();
-    }
-  } catch (const std::exception& e) {
-    handleException(e);
-  } catch (...) {
-  }
-}
-
-void Executor::handleException(const std::exception& e) XZERO_NOEXCEPT {
-  if (exceptionHandler_) {
-    try {
-      exceptionHandler_(e);
-    } catch (...) {
-    }
-  }
 }
 
 } // namespace xzero
