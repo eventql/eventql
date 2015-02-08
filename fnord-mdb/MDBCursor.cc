520a006f84d12e9deb5fc3e4fd1db0a6085ab70c
Paul Asmuth <paul@paulasmuth.com>
2015-02-08 19:24:52 +0000

    activity log fetch

--- fnord-mdb/MDBCursor.cc
+++ fnord-mdb/MDBCursor.cc
@@ -30,6 +30,54 @@ void MDBCursor::close() {
   decRef();
 }
 
+bool MDBCursor::get(const String& key, Buffer* value) {
+  void* val;
+  size_t val_size;
+
+  if (get(key.c_str(), key.length(), &val, &val_size)) {
+    *value = Buffer(val, val_size);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bool MDBCursor::get(const Buffer& key, Buffer* value) {
+  void* val;
+  size_t val_size;
+
+  if (get(key.data(), key.size(), &val, &val_size)) {
+    *value = Buffer(val, val_size);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bool MDBCursor::get(
+    const void* key,
+    size_t key_size,
+    void** value,
+    size_t* value_size) {
+  MDB_val mkey, mval;
+  mkey.mv_data = const_cast<void*>(key);
+  mkey.mv_size = key_size;
+
+  auto rc = mdb_cursor_get(mdb_cur_, &mkey, &mval, MDB_SET);
+  if (rc == MDB_NOTFOUND) {
+    return false;
+  }
+
+  if (rc != 0) {
+    auto err = String(mdb_strerror(rc));
+    RAISEF(kRuntimeError, "mdb_cursor_get(FIRST) failed: $0", err);
+  }
+
+  *value = mval.mv_data;
+  *value_size = mval.mv_size;
+  return true;
+}
+
 bool MDBCursor::set(const void* key, size_t key_size) {
   MDB_val mkey;
   mkey.mv_data = const_cast<void*>(key);
@@ -127,6 +175,44 @@ bool MDBCursor::getNext(
     size_t* value_size) {
   MDB_val mkey, mval;
   auto rc = mdb_cursor_get(mdb_cur_, &mkey, &mval, MDB_NEXT);
+  if (rc == MDB_NOTFOUND) {
+    return false;
+  }
+
+  if (rc != 0) {
+    auto err = String(mdb_strerror(rc));
+    RAISEF(kRuntimeError, "mdb_cursor_get(NEXT) failed: $0", err);
+  }
+
+  *key = mkey.mv_data;
+  *key_size = mkey.mv_size;
+  *value = mval.mv_data;
+  *value_size = mval.mv_size;
+  return true;
+}
+
+bool MDBCursor::getPrev(Buffer* rkey, Buffer* rvalue) {
+  void* key;
+  size_t key_size;
+  void* val;
+  size_t val_size;
+
+  if (getPrev(&key, &key_size, &val, &val_size)) {
+    *rkey = Buffer(key, key_size);
+    *rvalue = Buffer(val, val_size);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bool MDBCursor::getPrev(
+    void** key,
+    size_t* key_size,
+    void** value,
+    size_t* value_size) {
+  MDB_val mkey, mval;
+  auto rc = mdb_cursor_get(mdb_cur_, &mkey, &mval, MDB_PREV);
   if (rc == MDB_NOTFOUND) {
     return false;
   }
