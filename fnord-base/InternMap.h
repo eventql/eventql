7d6102d3f7b506b6df0817f92e9a2ab7d3317e84
Paul Asmuth <paul@paulasmuth.com>
2015-03-06 21:08:52 +0100

    better InternMap

--- fnord-base/InternMap.h
+++ fnord-base/InternMap.h
@@ -16,11 +16,17 @@ namespace fnord {
 
 class InternMap {
 public:
+  static const constexpr uint32_t kMagic = 0x17234205;
 
   void* internString(const String& str);
   String getString(const void* interned);
 
 protected:
+  struct InternedStringHeader {
+    uint32_t magic;
+    size_t size;
+  };
+
   std::mutex intern_map_mutex_;
   HashMap<String, void*> intern_map_;
 };
