5403a233c33b01ec4a623a2264cd673d11046c5b
Paul Asmuth <paul@paulasmuth.com>
2015-03-08 22:32:22 +0100

    include fixes

--- fnord-sstable/SSTableScan.cc
+++ fnord-sstable/SSTableScan.cc
@@ -7,6 +7,7 @@
  * copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
+#include <algorithm>
 #include <fnord-sstable/SSTableScan.h>
 #include <fnord-sstable/SSTableColumnReader.h>
 
