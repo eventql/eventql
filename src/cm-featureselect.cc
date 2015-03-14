61f131f11c08542f8a553e1df12509dd9921bcc9
Paul Asmuth <paul@paulasmuth.com>
2015-03-14 16:31:26 +0100

    write output sstable in cm-featureselect

--- src/cm-featureselect.cc
+++ src/cm-featureselect.cc
@@ -28,7 +28,11 @@
 #include "fnord-json/jsonrpc.h"
 #include "fnord-http/httprouter.h"
 #include "fnord-http/httpserver.h"
-#include "fnord-sstable/SSTableReader.h"
+#include "fnord-sstable/sstablereader.h"
+#include "fnord-sstable/sstablewriter.h"
+#include "fnord-sstable/SSTableColumnSchema.h"
+#include "fnord-sstable/SSTableColumnReader.h"
+#include "fnord-sstable/SSTableColumnWriter.h"
 #include "fnord-feeds/FeedService.h"
 #include "fnord-feeds/RemoteFeedFactory.h"
 #include "fnord-feeds/RemoteFeedReader.h"
@@ -99,25 +103,21 @@ int main(int argc, const char** argv) {
   feature_schema.registerFeature("category3", 4, 1);
 
   /* open featuredb */
+  auto input_file = flags.getString("input_file");
   auto featuredb_path = flags.getString("featuredb_path");
   auto featuredb = mdb::MDB::open(featuredb_path, true);
   cm::FeatureIndex feature_index(featuredb, &feature_schema);
 
   /* feature selector */
   cm::FeatureSelector feature_select(&feature_index);
-
   HashMap<String, uint64_t> feature_counts;
 
-  /* open output file */
-  //auto outfile = File::openFile(outputfile_path, File::O_READ | File::O_WRITE);
-
   /* read input table */
-  auto input_file = flags.getString("input_file");
-  fnord::logInfo("cm.ctrstats", "Importing sstable: $0", input_file);
+  fnord::logInfo("cm.featureselect", "Importing sstable: $0", input_file);
   sstable::SSTableReader reader(File::openFile(input_file, File::O_READ));
 
   if (reader.bodySize() == 0) {
-    fnord::logCritical("cm.ctrstats", "unfinished sstable: $0", input_file);
+    fnord::logCritical("cm.featureselect", "unfinished table: $0", input_file);
     return 1;
   }
 
@@ -129,7 +129,7 @@ int main(int argc, const char** argv) {
   /* status line */
   util::SimpleRateLimitedFn status_line(kMicrosPerSecond, [&] () {
     fnord::logInfo(
-        "cm.ctrstats",
+        "cm.featureselect",
         "[$0%] Reading sstable... rows=$1",
         (size_t) ((cursor->position() / (double) body_size) * 100),
         row_idx);
@@ -147,11 +147,13 @@ int main(int argc, const char** argv) {
       for (const auto& item : q.items) {
         Set<String> features;
         feature_select.featuresFor(q, item, &features);
-        fnord::iputs("features: $0", features);
+        for (const auto& f : features) {
+          ++feature_counts[f];
+        }
       }
     } catch (const Exception& e) {
       fnord::logWarning(
-          "cm.ctrstats",
+          "cm.featureselect",
           e,
           "error while indexing query: $0",
           val.toString());
@@ -164,13 +166,27 @@ int main(int argc, const char** argv) {
 
   status_line.runForce();
 
-  /* clean up */
+  /* write output sstable */
+  sstable::SSTableColumnSchema sstable_schema;
+  sstable_schema.addColumn("count", 1, sstable::SSTableColumnType::UINT64);
 
-  //for (const auto& f : feature_counts) {
-  //  if (f.second < 100) continue;
-  //  outfile.write(StringUtil::format("$0\n", f.first));
-  //}
+  auto output_file = flags.getString("output_file");
+  fnord::logInfo("cm.featureselect", "Writing results to: $0", output_file);
+  auto sstable_writer = sstable::SSTableWriter::create(
+      output_file,
+      sstable::IndexProvider{},
+      NULL,
+      0);
 
-  exit(0);
+  for (const auto& f : feature_counts) {
+    sstable::SSTableColumnWriter cols(&sstable_schema);
+    cols.addUInt64Column(1, f.second);
+    sstable_writer->appendRow(f.first, cols);
+  }
+
+  sstable_schema.writeIndex(sstable_writer.get());
+  sstable_writer->finalize();
+
+  return 0;
 }
 
