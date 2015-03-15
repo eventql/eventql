074d45b65106dc16975b6ccc546ffe5b9805ac93
Paul Asmuth <paul@paulasmuth.com>
2015-03-15 21:12:39 +0100

    export training data in lightsvm format

--- src/PackedExample.cc
+++ src/PackedExample.cc
@@ -13,18 +13,12 @@ using namespace fnord;
 
 namespace cm {
 
-String exampleToSVMLight(const Example& ex, cm::FeatureSchema* schema) {
+String exampleToSVMLight(const Example& ex) {
   auto str = StringUtil::toString(ex.label);
 
   int n = 0;
   for (const auto& f : ex.features) {
-    auto fid = schema->featureID(f.first);
-
-    if (fid.isEmpty()) {
-      continue;
-    }
-
-    str += StringUtil::format(" $0:$1", fid.get().feature, f.second);
+    str += StringUtil::format(" $0:$1", f.first, f.second);
     ++n;
   }
 
