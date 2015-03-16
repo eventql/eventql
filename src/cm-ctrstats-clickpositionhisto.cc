9a80d847afb1a979af24f611672b389ec9c5059e
Paul Asmuth <paul@paulasmuth.com>
2015-03-16 21:56:06 +0100

    cm-ctrstats-clickpositionhisto.cc

---
+++ src/cm-ctrstats-clickpositionhisto.cc
@@ -0,0 +1,162 @@
+/**
+ * Copyright (c) 2015 - The CM Authors <legal@clickmatcher.com>
+ *   All Rights Reserved.
+ *
+ * This file is CONFIDENTIAL -- Distribution or duplication of this material or
+ * the information contained herein is strictly forbidden unless prior written
+ * permission is obtained.
+ */
+#include <algorithm>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include "fnord-base/io/fileutil.h"
+#include "fnord-base/application.h"
+#include "fnord-base/logging.h"
+#include "fnord-base/cli/flagparser.h"
+#include "fnord-base/util/SimpleRateLimit.h"
+#include "fnord-base/InternMap.h"
+#include "fnord-json/json.h"
+#include "fnord-mdb/MDB.h"
+#include "fnord-mdb/MDBUtil.h"
+#include "fnord-sstable/sstablereader.h"
+#include "fnord-sstable/sstablewriter.h"
+#include "fnord-sstable/SSTableColumnSchema.h"
+#include "fnord-sstable/SSTableColumnReader.h"
+#include "fnord-sstable/SSTableColumnWriter.h"
+#include "common.h"
+#include "CustomerNamespace.h"
+#include "FeatureSchema.h"
+#include "JoinedQuery.h"
+#include "CTRCounter.h"
+
+using namespace fnord;
+
+int main(int argc, const char** argv) {
+  fnord::Application::init();
+  fnord::Application::logToStderr();
+
+  fnord::cli::FlagParser flags;
+
+  flags.defineFlag(
+      "loglevel",
+      fnord::cli::FlagParser::T_STRING,
+      false,
+      NULL,
+      "INFO",
+      "loglevel",
+      "<level>");
+
+  flags.parseArgv(argc, argv);
+
+  Logger::get()->setMinimumLogLevel(
+      strToLogLevel(flags.getString("loglevel")));
+
+  HashMap<uint32_t, uint64_t> click_posis;
+
+  auto start_time = std::numeric_limits<uint64_t>::max();
+  auto end_time = std::numeric_limits<uint64_t>::min();
+
+  /* read input tables */
+  auto sstables = flags.getArgv();
+  for (int tbl_idx = 0; tbl_idx < sstables.size(); ++tbl_idx) {
+    const auto& sstable = sstables[tbl_idx];
+    fnord::logInfo("cm.ctrstats", "Importing sstable: $0", sstable);
+
+    /* read sstable header */
+    sstable::SSTableReader reader(File::openFile(sstable, File::O_READ));
+
+    if (reader.bodySize() == 0) {
+      fnord::logCritical("cm.ctrstats", "unfinished sstable: $0", sstable);
+      exit(1);
+    }
+
+    /* read report header */
+    auto hdr = json::parseJSON(reader.readHeader());
+
+    auto tbl_start_time = json::JSONUtil::objectGetUInt64(
+        hdr.begin(),
+        hdr.end(),
+        "start_time").get();
+
+    auto tbl_end_time = json::JSONUtil::objectGetUInt64(
+        hdr.begin(),
+        hdr.end(),
+        "end_time").get();
+
+    if (tbl_start_time < start_time) {
+      start_time = tbl_start_time;
+    }
+
+    if (tbl_end_time > end_time) {
+      end_time = tbl_end_time;
+    }
+
+    /* get sstable cursor */
+    auto cursor = reader.getCursor();
+    auto body_size = reader.bodySize();
+    int row_idx = 0;
+
+    /* status line */
+    util::SimpleRateLimitedFn status_line(kMicrosPerSecond, [&] () {
+      fnord::logInfo(
+          "cm.ctrstats",
+          "[$1/$2] [$0%] Reading sstable... rows=$3",
+          (size_t) ((cursor->position() / (double) body_size) * 100),
+          tbl_idx + 1, sstables.size(), row_idx);
+    });
+
+    /* read sstable rows */
+    for (; cursor->valid(); ++row_idx) {
+      status_line.runMaybe();
+
+      auto val = cursor->getDataBuffer();
+      Option<cm::JoinedQuery> q;
+
+      try {
+        q = Some(json::fromJSON<cm::JoinedQuery>(val));
+      } catch (const Exception& e) {
+        //fnord::logWarning("cm.ctrstats", e, "invalid json: $0", val.toString());
+      }
+
+      if (!q.isEmpty()) {
+        for (auto& item : q.get().items) {
+          if (!item.clicked || item.position < 0) {
+            continue;
+          }
+
+          click_posis[item.position]++;
+        }
+      }
+
+      if (!cursor->next()) {
+        break;
+      }
+    }
+
+    status_line.runForce();
+  }
+
+  uint64_t sum = 0;
+  Vector<Pair<uint64_t, uint64_t>> posis;
+  for (const auto& p : click_posis) {
+    sum += p.second;
+    posis.emplace_back(p);
+  }
+
+  std::sort(posis.begin(), posis.end(), [] (
+      const Pair<uint64_t, uint64_t>& a,
+      const Pair<uint64_t, uint64_t>& b) {
+    return a.first < b.first;
+  });
+
+  for (const auto& p : posis) {
+    fnord::iputs(" Clicks on item position $0 => $1 ($2)",
+        p.first,
+        p.second,
+        ((p.second / (double) sum) * 100));
+  }
+
+  return 0;
+}
+
