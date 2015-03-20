df25477f6ba749ebb121188d3ec779ac2cf99eba
Paul Asmuth <paul@paulasmuth.com>
2015-03-20 21:59:33 +0100

    cm-indexserver

---
+++ src/cm-indexserver.cc
@@ -0,0 +1,88 @@
+/**
+ * Copyright (c) 2015 - The CM Authors <legal@clickmatcher.com>
+ *   All Rights Reserved.
+ *
+ * This file is CONFIDENTIAL -- Distribution or duplication of this material or
+ * the information contained herein is strictly forbidden unless prior written
+ * permission is obtained.
+ */
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include "fnord-base/io/filerepository.h"
+#include "fnord-base/io/fileutil.h"
+#include "fnord-base/application.h"
+#include "fnord-base/logging.h"
+#include "fnord-base/random.h"
+#include "fnord-base/thread/eventloop.h"
+#include "fnord-base/thread/threadpool.h"
+#include "fnord-base/wallclock.h"
+#include "fnord-base/VFS.h"
+#include "fnord-rpc/ServerGroup.h"
+#include "fnord-rpc/RPC.h"
+#include "fnord-rpc/RPCClient.h"
+#include "fnord-base/cli/flagparser.h"
+#include "fnord-json/json.h"
+#include "fnord-json/jsonrpc.h"
+#include "fnord-http/httprouter.h"
+#include "fnord-http/httpserver.h"
+#include "fnord-http/VFSFileServlet.h"
+#include "fnord-feeds/FeedService.h"
+#include "fnord-feeds/RemoteFeedFactory.h"
+#include "fnord-feeds/RemoteFeedReader.h"
+#include "fnord-base/stats/statsdagent.h"
+#include "fnord-sstable/SSTableServlet.h"
+#include "fnord-mdb/MDB.h"
+#include "fnord-mdb/MDBUtil.h"
+#include "common.h"
+#include "CustomerNamespace.h"
+#include "FeatureSchema.h"
+#include "JoinedQuery.h"
+
+using namespace fnord;
+
+int main(int argc, const char** argv) {
+  fnord::Application::init();
+  fnord::Application::logToStderr();
+
+  fnord::cli::FlagParser flags;
+
+  flags.defineFlag(
+      "http_port",
+      fnord::cli::FlagParser::T_INTEGER,
+      false,
+      NULL,
+      "8000",
+      "Start the public http server on this port",
+      "<port>");
+
+  flags.defineFlag(
+      "loglevel",
+      fnord::cli::FlagParser::T_STRING,
+      false,
+      NULL,
+      "INFO",
+      "loglevel",
+      "<level>");
+
+  flags.parseArgv(argc, argv);
+
+  Logger::get()->setMinimumLogLevel(
+      strToLogLevel(flags.getString("loglevel")));
+
+  WhitelistVFS vfs;
+
+  /* start http server */
+  fnord::thread::EventLoop ev;
+  fnord::http::HTTPRouter http_router;
+  fnord::http::HTTPServer http_server(&http_router, &ev);
+  http_server.listen(flags.getInt("http_port"));
+
+  /* sstable servlet */
+  //sstable::SSTableServlet sstable_servlet("/sstable", &vfs);
+  //http_router.addRouteByPrefixMatch("/sstable", &sstable_servlet);
+
+  ev.run();
+  return 0;
+}
+
