eec823a66bd9e935faca859c760b3545ecf626eb
Paul Asmuth <paul@paulasmuth.com>
2015-03-20 21:37:40 +0100

    indexlookup via ftsquery

---
+++ src/IndexReader.h
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2015 - The CM Authors <legal@clickmatcher.com>
+ *   All Rights Reserved.
+ *
+ * This file is CONFIDENTIAL -- Distribution or duplication of this material or
+ * the information contained herein is strictly forbidden unless prior written
+ * permission is obtained.
+ */
+#ifndef _CM_INDEXREADER_H
+#define _CM_INDEXREADER_H
+#include <mutex>
+#include <stdlib.h>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <queue>
+#include "fnord-base/stdtypes.h"
+#include "fnord-base/thread/taskscheduler.h"
+#include <fnord-fts/fts.h>
+#include <fnord-fts/fts_common.h>
+#include "fnord-mdb/MDB.h"
+#include "fnord-base/stats/stats.h"
+#include "FeatureIndex.h"
+#include "DocStore.h"
+#include "IndexRequest.h"
+#include "FeatureIndexWriter.h"
+#include "ItemRef.h"
+
+using namespace fnord;
+
+namespace cm {
+
+class IndexReader : public RefCounted {
+public:
+
+  static RefPtr<IndexReader> openIndex(const String& path);
+
+  ~IndexReader();
+
+  std::shared_ptr<fts::IndexReader> fts_;
+protected:
+
+  IndexReader(
+      FeatureSchema schema,
+      RefPtr<mdb::MDB> db,
+      std::shared_ptr<fts::IndexReader> fts);
+
+  FeatureSchema schema_;
+  RefPtr<mdb::MDB> db_;
+};
+
+} // namespace cm
+
+#endif
