ca5cc5e908fc2ffd3c67485bb0a4293aa6c4e5f2
Paul Asmuth <paul@paulasmuth.com>
2015-03-22 14:54:52 +0100

    cm::IndexServlet...

--- src/IndexServlet.cc
+++ src/IndexServlet.cc
@@ -21,6 +21,38 @@ IndexServlet::IndexServlet(
 void IndexServlet::handleHTTPRequest(
     fnord::http::HTTPRequest* req,
     fnord::http::HTTPResponse* res) {
+  URI uri(req->uri());
+
+  res->addHeader("Access-Control-Allow-Origin", "*");
+
+  try {
+    if (StringUtil::endsWith(uri.path(), "/search")) {
+      return searchQuery(req, res, &uri);
+    }
+
+    res->setStatus(fnord::http::kStatusNotFound);
+    res->addBody("not found");
+  } catch (const Exception& e) {
+    res->setStatus(http::kStatusInternalServerError);
+    res->addBody(StringUtil::format("error: $0", e.getMessage()));
+  }
+}
+
+void IndexServlet::searchQuery(
+    http::HTTPRequest* req,
+    http::HTTPResponse* res,
+    URI* uri) {
+  const auto& params = uri->queryParams();
+  res->setStatus(fnord::http::kStatusOK);
+
+  String search_query;
+  if (!fnord::URI::getParam(params, "q", &search_query)) {
+    res->addBody("error: missing ?q=... parameter");
+    res->setStatus(http::kStatusBadRequest);
+    return;
+  }
+
+  res->addBody("search!!! " + search_query);
 }
 
 }
