83b8e1be1c695de9e30f63a1f82bfbbeecd8578a
Paul Asmuth <paul@paulasmuth.com>
2015-03-28 17:26:32 +0100

    AutoCompleteServlet

---
+++ src/AutoCompleteServlet.cc
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2015 - The CM Authors <legal@clickmatcher.com>
+ *   All Rights Reserved.
+ *
+ * This file is CONFIDENTIAL -- Distribution or duplication of this material or
+ * the information contained herein is strictly forbidden unless prior written
+ * permission is obtained.
+ */
+#include "AutoCompleteServlet.h"
+#include "CTRCounter.h"
+#include "fnord-base/Language.h"
+#include "fnord-base/wallclock.h"
+#include "fnord-base/io/fileutil.h"
+
+using namespace fnord;
+
+namespace cm {
+
+AutoCompleteServlet::AutoCompleteServlet() {}
+
+void AutoCompleteServlet::handleHTTPRequest(
+    http::HTTPRequest* req,
+    http::HTTPResponse* res) {
+  URI uri(req->uri());
+
+  const auto& params = uri.queryParams();
+
+  /* arguments */
+  String qstr;
+  if (!URI::getParam(params, "q", &qstr)) {
+    res->addBody("error: missing ?q=... parameter");
+    res->setStatus(http::kStatusBadRequest);
+    return;
+  }
+
+  /* write response */
+  res->setStatus(http::kStatusOK);
+  res->addHeader("Content-Type", "application/json; charset=utf-8");
+  json::JSONOutputStream json(res->getBodyOutputStream());
+
+  json.beginObject();
+  json.addObjectEntry("query");
+  json.addString(qstr);
+  json.endObject();
+}
+
+}
---
+++ src/AutoCompleteServlet.h
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2015 - The CM Authors <legal@clickmatcher.com>
+ *   All Rights Reserved.
+ *
+ * This file is CONFIDENTIAL -- Distribution or duplication of this material or
+ * the information contained herein is strictly forbidden unless prior written
+ * permission is obtained.
+ */
+#ifndef _CM_AUTOCOMPLETESERVLET_H
+#define _CM_AUTOCOMPLETESERVLET_H
+#include "fnord-http/httpservice.h"
+#include "fnord-json/json.h"
+
+using namespace fnord;
+
+namespace cm {
+
+/**
+ *  GET /autocomplete
+ *
+ */
+class AutoCompleteServlet : public fnord::http::HTTPService {
+public:
+
+  AutoCompleteServlet();
+
+  void handleHTTPRequest(
+      fnord::http::HTTPRequest* req,
+      fnord::http::HTTPResponse* res);
+
+protected:
+};
+
+}
+#endif
