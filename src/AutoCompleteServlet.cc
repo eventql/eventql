16cd34af7efd16ff2e5722c70cee3f9c4ff2087a
Paul Asmuth <paul@paulasmuth.com>
2015-03-28 19:43:04 +0100

    placeholder for multi term and spelling correction

--- src/AutoCompleteServlet.cc
+++ src/AutoCompleteServlet.cc
@@ -50,6 +50,7 @@ void AutoCompleteServlet::handleHTTPRequest(
   }
 
   Vector<String> terms;
+  Vector<String> valid_terms;
   analyzer_->tokenize(lang, qstr, &terms);
   if (terms.size() == 0) {
     res->addBody("error: invalid ?q=... parameter");
@@ -57,67 +58,82 @@ void AutoCompleteServlet::handleHTTPRequest(
     return;
   }
 
-  auto prefix = languageToString(lang) + "~" + terms.back();
-
-  Vector<Pair<String, double>> matches;
-  double best_match = 0;
-  for (auto iter = term_info_.lower_bound(prefix);
-      iter != term_info_.end() && StringUtil::beginsWith(iter->first, prefix);
-      ++iter) {
-    matches.emplace_back(iter->first, iter->second.score);
-    if (iter->second.score > best_match) {
-      best_match = iter->second.score;
+  for (const auto& t : terms) {
+    if (term_info_.count(lang_str + "~" + t) > 0) {
+      valid_terms.emplace_back(t);
     }
   }
 
-  std::sort(matches.begin(), matches.end(), [] (
-      const Pair<String, double>& a,
-      const Pair<String, double>& b) {
-    return b.second < a.second;
-  });
+  if (terms.size() == 1 || valid_terms.size() == 0) {
+    auto prefix = languageToString(lang) + "~" + terms.back();
+    terms.pop_back();
+    auto qstr_prefix = StringUtil::join(terms, " ");
+    Vector<Pair<String, double>> matches;
+    double best_match = 0;
 
-  if (matches.size() > 0) {
-    const auto& best_match_ti = term_info_.find(matches[0].first);
-    for (const auto& c : best_match_ti->second.top_categories) {
-      if ((c.second / best_match_ti->second.top_categories[0].second) < 0.2) {
+    for (auto iter = term_info_.lower_bound(prefix);
+        iter != term_info_.end() && StringUtil::beginsWith(iter->first, prefix);
+        ++iter) {
+      matches.emplace_back(iter->first, iter->second.score);
+      if (iter->second.score > best_match) {
+        best_match = iter->second.score;
+      }
+    }
+
+    std::sort(matches.begin(), matches.end(), [] (
+        const Pair<String, double>& a,
+        const Pair<String, double>& b) {
+      return b.second < a.second;
+    });
+
+    if (matches.size() > 0) {
+      const auto& best_match_ti = term_info_.find(matches[0].first);
+      for (const auto& c : best_match_ti->second.top_categories) {
+        if ((c.second / best_match_ti->second.top_categories[0].second) < 0.2) {
+          break;
+        }
+
+        auto label = StringUtil::format(
+            "$0 $1 in $2",
+            qstr_prefix,
+            matches[0].first,
+            c.first);
+
+        results.emplace_back(label, c.second, "");
+      }
+    }
+
+    int m = 0;
+    for (int m = 0; m < matches.size(); ++m) {
+      auto score = matches[m].second;
+
+      if ((score / best_match) < 0.1) {
         break;
       }
 
-      auto label = StringUtil::format(
-          "$0 in $1",
-          matches[0].first,
-          c.first);
-
-      results.emplace_back(label, c.second, "");
+      results.emplace_back(matches[m].first, score, "");
     }
+
+    if (m == 0 && matches.size() > 1) {
+      const auto& best_match_ti = term_info_.find(matches[0].first);
+
+      for (const auto& r : best_match_ti->second.related_terms) {
+        auto label = StringUtil::format(
+            "$0 $1 $2",
+            qstr_prefix,
+            matches[0].first,
+            r.first);
+
+        results.emplace_back(label, r.second, "");
+      }
+    }
+  } else {
+    results.emplace_back("here be dragons: multi term complete", 1.0, "");
   }
 
-  int m = 0;
-  for (int m = 0; m < matches.size(); ++m) {
-    auto score = matches[m].second;
-
-    if ((score / best_match) < 0.1) {
-      break;
-    }
-
-    results.emplace_back(matches[m].first, score, "");
-  }
-
-  if (m == 0 && matches.size() > 1) {
-    const auto& best_match_ti = term_info_.find(matches[0].first);
-
-    for (const auto& r : best_match_ti->second.related_terms) {
-      //if ((r.second / best_match_ti->second.related_terms[0].second) < 0.06) {
-      //  break;
-      //}
-
-      auto label = StringUtil::format(
-          "$0 $1",
-          matches[0].first,
-          r.first);
-
-      results.emplace_back(label, r.second, "");
-    }
+  fnord::iputs("terms: $0, valid: $1, results: $2", terms, valid_terms, results.size());
+  if (results.size() == 0 && qstr.length() > 2) {
+    results.emplace_back("here be dragons: spelling correction", 1.0, "");
   }
 
   /* write response */
