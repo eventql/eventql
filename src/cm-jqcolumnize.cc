c3de442736fc003df3e04783c63a36c609086489
Paul Asmuth <paul@paulasmuth.com>
2015-04-02 19:03:04 +0200

    jqcolumnize --{input,output}file flags

--- src/cm-jqcolumnize.cc
+++ src/cm-jqcolumnize.cc
@@ -46,6 +46,24 @@ int main(int argc, const char** argv) {
   fnord::cli::FlagParser flags;
 
   flags.defineFlag(
+      "input_file",
+      fnord::cli::FlagParser::T_STRING,
+      true,
+      "i",
+      NULL,
+      "file",
+      "<filename>");
+
+  flags.defineFlag(
+      "output_file",
+      fnord::cli::FlagParser::T_STRING,
+      true,
+      "o",
+      NULL,
+      "file",
+      "<filename>");
+
+  flags.defineFlag(
       "loglevel",
       fnord::cli::FlagParser::T_STRING,
       false,
@@ -106,85 +124,63 @@ int main(int argc, const char** argv) {
 
 
   /* read input tables */
-  auto sstables = flags.getArgv();
   int row_idx = 0;
-  for (int tbl_idx = 0; tbl_idx < sstables.size(); ++tbl_idx) {
-    const auto& sstable = sstables[tbl_idx];
-    fnord::logInfo("cm.jqcolumnize", "Importing sstable: $0", sstable);
 
-    /* read sstable header */
-    sstable::SSTableReader reader(File::openFile(sstable, File::O_READ));
+  const auto& sstable = flags.getString("input_file");
+  fnord::logInfo("cm.jqcolumnize", "Importing sstable: $0", sstable);
 
-    if (reader.bodySize() == 0) {
-      fnord::logCritical("cm.jqcolumnize", "unfinished sstable: $0", sstable);
-      exit(1);
+  /* read sstable header */
+  sstable::SSTableReader reader(File::openFile(sstable, File::O_READ));
+
+  if (reader.bodySize() == 0) {
+    fnord::logCritical("cm.jqcolumnize", "unfinished sstable: $0", sstable);
+    exit(1);
+  }
+
+  /* get sstable cursor */
+  auto cursor = reader.getCursor();
+  auto body_size = reader.bodySize();
+
+  /* status line */
+  util::SimpleRateLimitedFn status_line(kMicrosPerSecond, [&] () {
+    fnord::logInfo(
+        "cm.jqcolumnize",
+        "[$0%] Reading sstable... rows=$3",
+        (size_t) ((cursor->position() / (double) body_size) * 100),
+        row_idx);
+  });
+
+  /* read sstable rows */
+  for (; cursor->valid(); ++row_idx) {
+    status_line.runMaybe();
+
+    auto key = cursor->getKeyString();
+    auto val = cursor->getDataBuffer();
+    Option<cm::JoinedQuery> q;
+
+    try {
+      q = Some(json::fromJSON<cm::JoinedQuery>(val));
+    } catch (const Exception& e) {
+      fnord::logWarning("cm.jqcolumnize", e, "invalid json: $0", val.toString());
     }
 
-    /* get sstable cursor */
-    auto cursor = reader.getCursor();
-    auto body_size = reader.bodySize();
-
-    /* status line */
-    util::SimpleRateLimitedFn status_line(kMicrosPerSecond, [&] () {
-      fnord::logInfo(
-          "cm.jqcolumnize",
-          "[$1/$2] [$0%] Reading sstable... rows=$3",
-          (size_t) ((cursor->position() / (double) body_size) * 100),
-          tbl_idx + 1, sstables.size(), row_idx);
-    });
-
-    /* read sstable rows */
-    for (; cursor->valid(); ++row_idx) {
-      status_line.runMaybe();
-
-      auto key = cursor->getKeyString();
-      auto val = cursor->getDataBuffer();
-      Option<cm::JoinedQuery> q;
-
-      try {
-        q = Some(json::fromJSON<cm::JoinedQuery>(val));
-      } catch (const Exception& e) {
-        fnord::logWarning("cm.jqcolumnize", e, "invalid json: $0", val.toString());
-      }
-
-      if (!q.isEmpty()) {
-        cm::JoinedSession s;
-        s.queries.emplace_back(q.get());
-        add_session(s);
-      }
-
-      if (!cursor->next()) {
-        break;
-      }
+    if (!q.isEmpty()) {
+      cm::JoinedSession s;
+      s.queries.emplace_back(q.get());
+      add_session(s);
     }
 
-    status_line.runForce();
+    if (!cursor->next()) {
+      break;
+    }
   }
 
-  if (sstables.size() > 0) {
-    cstable::CSTableWriter writer("fnord.cstable", n);
-    writer.addColumn("queries.items.position", &position_col);
-    writer.addColumn("queries.items.clicked", &clicked_col);
-    writer.commit();
-  }
+  status_line.runForce();
 
-  cstable::CSTableReader reader("fnord.cstable");
-
-  auto t0 = WallClock::unixMicros();
-
-  cm::AnalyticsQuery aq;
-  cm::CTRByPositionQueryResult res;
-  cm::CTRByPositionQuery q(&aq, &res);
-  aq.scanTable(&reader);
-  auto t1 = WallClock::unixMicros();
-  fnord::iputs("scanned $0 rows in $1 ms", res.rows_scanned, (t1 - t0) / 1000.0f);
-  for (const auto& p : res.counters) {
-    fnord::iputs(
-       "pos: $0, views: $1, clicks: $2, ctr: $3", 
-        p.first, p.second.num_views,
-        p.second.num_clicks,
-        p.second.num_clicks / (double) p.second.num_views);
-  }
+  cstable::CSTableWriter writer(flags.getString("output_file"), n);
+  writer.addColumn("queries.items.position", &position_col);
+  writer.addColumn("queries.items.clicked", &clicked_col);
+  writer.commit();
 
   return 0;
 }
