88b727d33cad8f11fdf42d8f0a8f1c9b4c2a95ba
Paul Asmuth <paul@paulasmuth.com>
2015-04-02 18:58:37 +0200

    include fixes

--- fnord-cstable/CSTableWriter.cc
+++ fnord-cstable/CSTableWriter.cc
@@ -7,7 +7,7 @@
  * copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
-#include <fnord-base/Buffer.h>
+#include <fnord-base/buffer.h>
 #include <fnord-cstable/BinaryFormat.h>
 #include <fnord-cstable/CSTableWriter.h>
 
