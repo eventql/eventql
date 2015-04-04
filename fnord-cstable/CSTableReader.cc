00011eb2bc50cd9eadd27e02742dc4b6dac29758
Paul Asmuth <paul@paulasmuth.com>
2015-04-05 01:53:07 +0200

    UInt32Column{Reader,Writer}

--- fnord-cstable/CSTableReader.cc
+++ fnord-cstable/CSTableReader.cc
@@ -11,6 +11,7 @@
 #include <fnord-cstable/CSTableReader.h>
 #include <fnord-cstable/BooleanColumnReader.h>
 #include <fnord-cstable/BitPackedIntColumnReader.h>
+#include <fnord-cstable/UInt32ColumnReader.h>
 
 namespace fnord {
 namespace cstable {
@@ -71,6 +72,8 @@ RefPtr<ColumnReader> CSTableReader::getColumnReader(const String& column_name) {
       return new BooleanColumnReader(c.r_max, c.d_max, cdata, c.size);
     case ColumnType::UINT32_BITPACKED:
       return new BitPackedIntColumnReader(c.r_max, c.d_max, cdata, c.size);
+    case ColumnType::UINT32_PLAIN:
+      return new UInt32ColumnReader(c.r_max, c.d_max, cdata, c.size);
     default:
       RAISEF(kIllegalStateError, "invalid column type: $0", (uint32_t) c.type);
   }
