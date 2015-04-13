251222758447234e39a28ce32be3002d89867c16
Paul Asmuth <paul@paulasmuth.com>
2015-04-13 02:18:05 +0200

    TableJanitor

---
+++ fnord-eventdb/TableJanitor.cc
@@ -0,0 +1,63 @@
+/**
+ * This file is part of the "libfnord" project
+ *   Copyright (c) 2015 Paul Asmuth
+ *
+ * FnordMetric is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License v3.0. You should have received a
+ * copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+#include "unistd.h"
+#include <fnord-base/logging.h>
+#include <fnord-base/wallclock.h>
+#include <fnord-eventdb/TableJanitor.h>
+
+namespace fnord {
+namespace eventdb {
+
+TableJanitor::TableJanitor(
+    TableRepository* repo) :
+    repo_(repo),
+    interval_(10 * kMicrosPerSecond),
+    running_(false) {}
+
+void TableJanitor::start() {
+  running_ = true;
+  thread_ = std::thread(std::bind(&TableJanitor::run, this));
+}
+
+void TableJanitor::stop() {
+  if (!running_) {
+    return;
+  }
+
+  running_ = false;
+  thread_.join();
+}
+
+void TableJanitor::run() {
+  while (running_) {
+    auto begin = WallClock::unixMicros();
+
+    try {
+      auto tables = repo_->tables();
+
+      for (auto& tbl : tables) {
+        tbl->commit();
+        tbl->merge();
+        tbl->gc();
+      }
+    } catch (const Exception& e) {
+      fnord::logError("fnord.evdb", e, "TableJanitor error");
+    }
+
+    auto elapsed = WallClock::unixMicros() - begin;
+    if (elapsed < interval_) {
+      usleep(interval_ - elapsed);
+    }
+  }
+}
+
+} // namespace eventdb
+} // namespace fnord
+
