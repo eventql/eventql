58bb97b78e017a7755400fecf7e1b0e55fdce4c6
Paul Asmuth <paul@paulasmuth.com>
2015-04-15 18:50:38 +0200

    include fix

--- fnord-base/io/FileLock.cc
+++ fnord-base/io/FileLock.cc
@@ -7,6 +7,7 @@
  * copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
+#include <sys/file.h>
 #include <fnord-base/exception.h>
 #include <fnord-base/io/FileLock.h>
 
