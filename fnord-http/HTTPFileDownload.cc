f2a1e7a7f58c162b94b9ecaf124fa365c3a4d724
Paul Asmuth <paul@paulasmuth.com>
2015-04-16 19:56:40 +0200

    HTTPFileDownload impl

--- fnord-http/HTTPFileDownload.cc
+++ fnord-http/HTTPFileDownload.cc
@@ -16,14 +16,27 @@ HTTPFileDownload::HTTPFileDownload(
     const HTTPRequest& http_req,
     const String& output_file) :
     http_req_(http_req),
-    output_file_(output_file) {}
+    output_file_(output_file),
+    file_(File::openFile(output_file, File::O_CREATE | File::O_WRITE)) {}
 
 Future<HTTPResponse> HTTPFileDownload::download(HTTPConnectionPool* http) {
   return http->executeRequest(
       http_req_,
-      [] (const Promise<HTTPResponse> promise) {
-        return new HTTPResponseFuture(promise);
+      [this] (const Promise<HTTPResponse> promise) -> HTTPResponseFuture* {
+        return new HTTPFileDownload::ResponseFuture(promise, std::move(file_));
       });
+}
+
+HTTPFileDownload::ResponseFuture::ResponseFuture(
+    Promise<HTTPResponse> promise,
+    File&& file) :
+    HTTPResponseFuture(promise),
+    file_(std::move(file)) {}
+
+void HTTPFileDownload::ResponseFuture::onBodyChunk(
+    const char* data,
+    size_t size) {
+  file_.write(data, size);
 }
 
 }
