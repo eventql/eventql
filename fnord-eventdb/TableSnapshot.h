dcf42e08d6b3789bff78360f4e3219163a4f35bf
Paul Asmuth <paul@paulasmuth.com>
2015-04-17 19:58:51 +0200

    write TableChunkSummaries as .smr

--- fnord-eventdb/TableSnapshot.h
+++ fnord-eventdb/TableSnapshot.h
@@ -26,10 +26,10 @@ struct TableChunkRef {
   uint64_t num_records;
   uint64_t sstable_checksum;
   uint64_t cstable_checksum;
-  uint64_t index_checksum;
+  uint64_t summary_checksum;
   uint64_t sstable_size;
   uint64_t cstable_size;
-  uint64_t index_size;
+  uint64_t summary_size;
 };
 
 struct TableGeneration : public RefCounted {
