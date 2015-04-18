dc172e092a74e70ae594b994f969b6667dd47d6f
Paul Asmuth <paul@paulasmuth.com>
2015-04-18 17:36:52 +0200

    logjoin backfill tail logic

--- src/cm-logjoin-backfill.cc
+++ src/cm-logjoin-backfill.cc
@@ -17,6 +17,10 @@
 #include "fnord-base/thread/eventloop.h"
 #include "fnord-base/thread/threadpool.h"
 #include "fnord-base/wallclock.h"
+#include "fnord-base/cli/flagparser.h"
+#include "fnord-eventdb/TableRepository.h"
+#include "fnord-eventdb/LogTableTail.h"
+
 #include "common.h"
 #include "schemas.h"
 
@@ -24,10 +28,13 @@ using namespace cm;
 using namespace fnord;
 
 fnord::thread::EventLoop ev;
+std::atomic<bool> cm_logjoin_shutdown;
 
 int main(int argc, const char** argv) {
   fnord::Application::init();
   fnord::Application::logToStderr();
+
+  cm_logjoin_shutdown = false;
 
   fnord::cli::FlagParser flags;
 
@@ -68,6 +75,15 @@ int main(int argc, const char** argv) {
       "<bool>");
 
   flags.defineFlag(
+      "replica",
+      cli::FlagParser::T_STRING,
+      true,
+      NULL,
+      NULL,
+      "replica id",
+      "<id>");
+
+  flags.defineFlag(
       "loglevel",
       fnord::cli::FlagParser::T_STRING,
       false,
@@ -81,10 +97,8 @@ int main(int argc, const char** argv) {
   Logger::get()->setMinimumLogLevel(
       strToLogLevel(flags.getString("loglevel")));
 
-  /* schema... */
-  auto joined_sessions_schema = joinedSessionsSchema();
-
-  /* set up cmdata */
+  /* args */
+  auto batch_size = flags.getInt("batch_size");
   auto datadir = flags.getString("datadir");
   if (!FileUtil::isDirectory(datadir)) {
     RAISEF(kIOError, "no such directory: $0", datadir);
@@ -98,6 +112,42 @@ int main(int argc, const char** argv) {
   /* set up http */
   http::HTTPConnectionPool http(&ev);
 
+  /* open table */
+  auto joined_sessions_schema = joinedSessionsSchema();
+  auto table_reader = eventdb::TableReader::open(
+      "dawanda_joined_sessions",
+      flags.getString("replica"),
+      datadir,
+      joined_sessions_schema);
+
+  eventdb::LogTableTail tail(table_reader);
+
+  /* on record callback */
+  auto on_record = [] (const msg::MessageObject& record) -> bool {
+    fnord::iputs("on record...", 1);
+    return true;
+  };
+
+  /* read table */
+  fnord::logInfo(
+      "cm.logjoin",
+      "Starting LogJoin backfill with:\n    batch_size=$0",
+      batch_size);
+
+  bool running = true;
+  while (running) {
+    running = tail.fetchNext(on_record, batch_size);
+
+    //fnord::logInfo("cm.logjoin", "LogJoin backfill comitting");
+
+    if (cm_logjoin_shutdown.load()) {
+      break;
+    }
+  };
+
+  fnord::logInfo("cm.logjoin", "Draining LogJoin upload...");
+
+  fnord::logInfo("cm.logjoin", "LogJoin backfill finished succesfully :)");
 
   ev.shutdown();
   evloop_thread.join();
