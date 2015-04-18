85acc86303cc1b5814307f8e10b8417a3bfaf244
Paul Asmuth <paul@paulasmuth.com>
2015-04-18 16:46:49 +0200

    LogTableTail iface

---
+++ fnord-eventdb/LogTableTail.h
@@ -0,0 +1,51 @@
+/**
+ * This file is part of the "libfnord" project
+ *   Copyright (c) 2015 Paul Asmuth
+ *
+ * FnordMetric is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License v3.0. You should have received a
+ * copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+#ifndef _FNORD_EVENTDB_TABLEREADER_H
+#define _FNORD_EVENTDB_TABLEREADER_H
+#include <fnord-base/stdtypes.h>
+#include <fnord-base/autoref.h>
+#include <fnord-msg/MessageSchema.h>
+#include <fnord-msg/MessageObject.h>
+#include <fnord-eventdb/TableArena.h>
+#include <fnord-eventdb/TableSnapshot.h>
+#include "fnord-sstable/sstablereader.h"
+#include "fnord-cstable/CSTableReader.h"
+
+namespace fnord {
+namespace eventdb {
+
+struct LogTableTailOffset {
+  String replica_id;
+  uint64_t consumed_offset;
+};
+
+typedef Vector<LogTableTailOffset> LogTableTailCursor;
+
+class LogTableTail : public RefCounted {
+public:
+
+  LogTableTail(RefPtr<TableReader> reader);
+  LogTableTail(RefPtr<TableReader> reader, LogTableTailCursor cursor);
+
+  bool fetchNext(
+      Function<bool (const msg::MessageObject& record)> fn,
+      size_t limit = -1);
+
+  LogTableTailCursor getCursor() const;
+
+protected:
+  RefPtr<TableReader> reader_;
+  HashMap<String, uint64_t> offsets_;
+};
+
+} // namespace eventdb
+} // namespace fnord
+
+#endif
