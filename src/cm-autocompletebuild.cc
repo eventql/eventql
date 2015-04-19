a20d952b45876dd131d157c47d32157fc7665130
Paul Asmuth <paul@paulasmuth.com>
2015-04-19 17:06:37 +0200

    build autocomplete from new table

--- src/cm-autocompletebuild.cc
+++ src/cm-autocompletebuild.cc
@@ -69,14 +69,14 @@ int main(int argc, const char** argv) {
   //    "conf directory",
   //    "<path>");
 
-  flags.defineFlag(
-      "index",
-      cli::FlagParser::T_STRING,
-      false,
-      NULL,
-      NULL,
-      "index directory",
-      "<path>");
+  //flags.defineFlag(
+  //    "index",
+  //    cli::FlagParser::T_STRING,
+  //    false,
+  //    NULL,
+  //    NULL,
+  //    "index directory",
+  //    "<path>");
 
   flags.defineFlag(
       "replica",
@@ -119,13 +119,13 @@ int main(int argc, const char** argv) {
   Logger::get()->setMinimumLogLevel(
       strToLogLevel(flags.getString("loglevel")));
 
-  auto index_path = flags.getString("index");
+  //auto index_path = flags.getString("index");
   //auto conf_path = flags.getString("conf");
   auto tempdir = flags.getString("tempdir");
   auto datadir = flags.getString("datadir");
 
   /* open index */
-  auto index_reader = cm::IndexReader::openIndex(index_path);
+  //auto index_reader = cm::IndexReader::openIndex(index_path);
   //auto analyzer = RefPtr<fts::Analyzer>(new fts::Analyzer(conf_path));
 
   /* set up reportbuilder */
@@ -134,7 +134,7 @@ int main(int argc, const char** argv) {
   auto buildid = 0;
 
   auto table = eventdb::TableReader::open(
-      "dawanda_joined_sessions",
+      "joined_sessions-dawanda",
       flags.getString("replica"),
       flags.getString("datadir"),
       joinedSessionsSchema());
@@ -148,7 +148,7 @@ int main(int argc, const char** argv) {
     auto input_table = StringUtil::format(
         "$0/$1.$2.$3.cst",
         datadir,
-        "dawanda_joined_sessions",
+        "joined_sessions-dawanda",
         c.replica_id,
         c.chunk_id);
 
@@ -177,8 +177,7 @@ int main(int argc, const char** argv) {
         new CTRBySearchTermCrossCategoryMapper(
             new AnalyticsTableScanSource(input_table),
             new CTRCounterTableSink(0, 0, searchterm_x_e1_table),
-            "category1",
-            index_reader));
+            "category1"));
   }
 
   report_builder.addReport(
