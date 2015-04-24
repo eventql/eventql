e3162c7ad1dbfc319cfe093b30912376332041a6
Paul Asmuth <paul@paulasmuth.com>
2015-04-24 18:36:04 +0200

    fix

--- fnord-logtable/RemoteTableReader.cc
+++ fnord-logtable/RemoteTableReader.cc
@@ -8,6 +8,7 @@
  * <http://www.gnu.org/licenses/>.
  */
 #include <fnord-logtable/RemoteTableReader.h>
+#include <fnord-msg/MessageDecoder.h>
 
 namespace fnord {
 namespace logtable {
@@ -33,12 +34,42 @@ const msg::MessageSchema& RemoteTableReader::schema() const {
 //RefPtr<TableSnapshot> RemoteTableReader::getSnapshot() {
 //}
 
-//size_t RemoteTableReader::fetchRecords(
-//    const String& replica,
-//    uint64_t start_sequence,
-//    size_t limit,
-//    Function<bool (const msg::MessageObject& record)> fn) {
-//}
+size_t RemoteTableReader::fetchRecords(
+    const String& replica,
+    uint64_t start_sequence,
+    size_t limit,
+    Function<bool (const msg::MessageObject& record)> fn) {
+  auto path = StringUtil::format(
+      "/fetch?table=$0&replica=$1&seq=$2&limit=$3",
+      name_,
+      replica,
+      start_sequence,
+      limit);
+
+  URI uri(uri_.toString() + path);
+
+  http::HTTPRequest req(http::HTTPMessage::M_GET, uri.pathAndQuery());
+  req.addHeader("Host", uri.hostAndPort());
+  req.addHeader("Content-Type", "application/fnord-msg");
+
+  auto res = http_->executeRequest(req);
+  res.wait();
+
+  const auto& r = res.get();
+  if (r.statusCode() != 200) {
+    RAISEF(kRuntimeError, "received non-200 response: $0", r.body().toString());
+  }
+
+  const auto& buf = r.body();
+  for (size_t offset = 0; offset < buf.size(); ) {
+    msg::MessageObject msg;
+    msg::MessageDecoder::decode(buf, schema_, &msg, &offset);
+
+    if (!fn(msg)) {
+      break;
+    }
+  }
+}
 
 } // namespace logtable
 } // namespace fnord
