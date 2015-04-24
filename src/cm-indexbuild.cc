0060a3e773270b87872871acd7c31f29b3a0090d
Paul Asmuth <paul@paulasmuth.com>
2015-04-24 16:44:22 +0200

    cm-indexbuild...

--- src/cm-indexbuild.cc
+++ src/cm-indexbuild.cc
@@ -31,17 +31,19 @@
 #include "fnord-feeds/RemoteFeedReader.h"
 #include "fnord-base/stats/statsdagent.h"
 #include "fnord-sstable/sstablereader.h"
+#include "fnord-logtable/RemoteTableReader.h"
 #include "fnord-mdb/MDB.h"
 #include "CustomerNamespace.h"
 #include "FeatureSchema.h"
 #include "FeatureIndexWriter.h"
 #include "IndexChangeRequest.h"
 #include "IndexWriter.h"
+#include "schemas.h"
 
 using namespace cm;
 using namespace fnord;
 
-/* stats */
+fnord::thread::EventLoop ev;
 
 int main(int argc, const char** argv) {
   Application::init();
@@ -106,12 +108,32 @@ int main(int argc, const char** argv) {
 
   statsd_agent.start();
 
+  auto evloop_thread = std::thread([] {
+    ev.run();
+  });
+
+  http::HTTPConnectionPool http(&ev);
+
   /* open index */
   fnord::logInfo(
       "cm.indexbuild",
       "Opening index at $0",
       flags.getString("index"));
 
+  auto table = logtable::RemoteTableReader::open(
+      "index_feed-dawanda",
+      indexChangeRequestSchema(),
+      URI("http://nue03.prod.fnrd.net:7003/logtable"),
+      &http);
+
+  //auto table = logtable::TableReader::open(
+  //    "joined_sessions-dawanda",
+  //    flags.getString("replica"),
+  //    flags.getString("datadir"),
+  //    joinedSessionsSchema());
+
+  ev.shutdown();
+  evloop_thread.join();
 
   fnord::logInfo("cm.indexbuild", "Exiting...");
   return 0;
