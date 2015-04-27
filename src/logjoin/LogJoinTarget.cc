6fc13cadd0b31381af29c93074a2a1561a734b4d
Paul Asmuth <paul@paulasmuth.com>
2015-04-27 19:25:43 +0200

    logjoin/schema improvements

--- src/logjoin/LogJoinTarget.cc
+++ src/logjoin/LogJoinTarget.cc
@@ -109,97 +109,102 @@ void LogJoinTarget::onSession(
 
     /* queries.time */
     qry_obj.addChild(
-        schema.id("queries.time"),
+        schema.id("search_queries.time"),
         (uint32_t) (q.time.unixMicros() / kMicrosPerSecond));
 
     /* queries.language */
     auto lang = cm::extractLanguage(q.attrs);
-    qry_obj.addChild(schema.id("queries.language"), (uint32_t) lang);
+    qry_obj.addChild(schema.id("search_queries.language"), (uint32_t) lang);
 
     /* queries.query_string */
     auto qstr = cm::extractQueryString(q.attrs);
     if (!qstr.isEmpty()) {
       auto qstr_norm = analyzer_->normalize(lang, qstr.get());
-      qry_obj.addChild(schema.id("queries.query_string"), qstr.get());
-      qry_obj.addChild(schema.id("queries.query_string_normalized"), qstr_norm);
+      qry_obj.addChild(schema.id("search_queries.query_string"), qstr.get());
+      qry_obj.addChild(schema.id("search_queries.query_string_normalized"), qstr_norm);
     }
 
     /* queries.shopid */
     auto slrid = cm::extractAttr(q.attrs, "slrid");
     if (!slrid.isEmpty()) {
       uint32_t sid = std::stoul(slrid.get());
-      qry_obj.addChild(schema.id("queries.shop_id"), sid);
+      qry_obj.addChild(schema.id("search_queries.shop_id"), sid);
     }
 
-    qry_obj.addChild(schema.id("queries.num_items"), q.nitems);
-    qry_obj.addChild(schema.id("queries.num_items_clicked"), q.nclicks);
-    qry_obj.addChild(schema.id("queries.num_ad_impressions"), q.nads);
-    qry_obj.addChild(schema.id("queries.num_ad_clicks"), q.nadclicks);
-    qry_obj.addChild(schema.id("queries.num_cart_items"), q.num_cart_items);
-    qry_obj.addChild(schema.id("queries.cart_value_eurcents"), q.cart_value_eurcents);
-    qry_obj.addChild(schema.id("queries.num_order_items"), q.num_order_items);
-    qry_obj.addChild(schema.id("queries.gmv_eurcents"), q.gmv_eurcents);
+    qry_obj.addChild(schema.id("search_queries.num_result_items"), q.nitems);
+    qry_obj.addChild(schema.id("search_queries.num_result_items_clicked"), q.nclicks);
+    qry_obj.addChild(schema.id("search_queries.num_ad_impressions"), q.nads);
+    qry_obj.addChild(schema.id("search_queries.num_ad_clicks"), q.nadclicks);
+    qry_obj.addChild(schema.id("search_queries.num_cart_items"), q.num_cart_items);
+    qry_obj.addChild(schema.id("search_queries.cart_value_eurcents"), q.cart_value_eurcents);
+    qry_obj.addChild(schema.id("search_queries.num_order_items"), q.num_order_items);
+    qry_obj.addChild(schema.id("search_queries.gmv_eurcents"), q.gmv_eurcents);
 
     /* queries.page */
     auto pg_str = cm::extractAttr(q.attrs, "pg");
     if (!pg_str.isEmpty()) {
       uint32_t pg = std::stoul(pg_str.get());
-      qry_obj.addChild(schema.id("queries.page"), pg);
+      qry_obj.addChild(schema.id("search_queries.page"), pg);
     }
 
     /* queries.ab_test_group */
     auto abgrp = cm::extractABTestGroup(q.attrs);
     if (!abgrp.isEmpty()) {
       sess_abgrp = abgrp.get();
-      qry_obj.addChild(schema.id("queries.ab_test_group"), abgrp.get());
+      qry_obj.addChild(schema.id("search_queries.ab_test_group"), abgrp.get());
     }
 
     /* queries.category1 */
     auto qcat1 = cm::extractAttr(q.attrs, "q_cat1");
     if (!qcat1.isEmpty()) {
       uint32_t c = std::stoul(qcat1.get());
-      qry_obj.addChild(schema.id("queries.category1"), c);
+      qry_obj.addChild(schema.id("search_queries.category1"), c);
     }
 
     /* queries.category1 */
     auto qcat2 = cm::extractAttr(q.attrs, "q_cat2");
     if (!qcat2.isEmpty()) {
       uint32_t c = std::stoul(qcat2.get());
-      qry_obj.addChild(schema.id("queries.category2"), c);
+      qry_obj.addChild(schema.id("search_queries.category2"), c);
     }
 
     /* queries.category1 */
     auto qcat3 = cm::extractAttr(q.attrs, "q_cat3");
     if (!qcat3.isEmpty()) {
       uint32_t c = std::stoul(qcat3.get());
-      qry_obj.addChild(schema.id("queries.category3"), c);
+      qry_obj.addChild(schema.id("search_queries.category3"), c);
     }
 
     /* queries.device_type */
     qry_obj.addChild(
-        schema.id("queries.device_type"),
+        schema.id("search_queries.device_type"),
         (uint32_t) extractDeviceType(q.attrs));
 
     /* queries.page_type */
     qry_obj.addChild(
-        schema.id("queries.page_type"),
+        schema.id("search_queries.page_type"),
         (uint32_t) extractPageType(q.attrs));
 
     for (const auto& item : q.items) {
-      auto& item_obj = qry_obj.addChild(schema.id("queries.items"));
+      auto& item_obj = qry_obj.addChild(
+          schema.id("search_queries.result_items"));
 
       item_obj.addChild(
-          schema.id("queries.items.position"),
+          schema.id("search_queries.result_items.position"),
           (uint32_t) item.position);
 
       item_obj.addChild(
-          schema.id("queries.items.item_id"),
+          schema.id("search_queries.result_items.item_id"),
           item.item.docID().docid);
 
       if (item.clicked) {
-        item_obj.addChild(schema.id("queries.items.clicked"), msg::TRUE);
+        item_obj.addChild(
+            schema.id("search_queries.result_items.clicked"),
+            msg::TRUE);
       } else {
-        item_obj.addChild(schema.id("queries.items.clicked"), msg::FALSE);
+        item_obj.addChild(
+            schema.id("search_queries.result_items.clicked"),
+            msg::FALSE);
       }
 
       auto docid = item.item.docID();
@@ -212,28 +217,28 @@ void LogJoinTarget::onSession(
             docid.docid);
       } else {
         item_obj.addChild(
-            schema.id("queries.items.shop_id"),
+            schema.id("search_queries.result_items.shop_id"),
             (uint32_t) std::stoull(shopid.get()));
       }
 
       auto category1 = index_->getField(docid, "category1");
       if (!category1.isEmpty()) {
         item_obj.addChild(
-            schema.id("queries.items.category1"),
+            schema.id("search_queries.result_items.category1"),
             (uint32_t) std::stoull(category1.get()));
       }
 
       auto category2 = index_->getField(docid, "category2");
       if (!category2.isEmpty()) {
         item_obj.addChild(
-            schema.id("queries.items.category2"),
+            schema.id("search_queries.result_items.category2"),
             (uint32_t) std::stoull(category2.get()));
       }
 
       auto category3 = index_->getField(docid, "category3");
       if (!category3.isEmpty()) {
         item_obj.addChild(
-            schema.id("queries.items.category3"),
+            schema.id("search_queries.result_items.category3"),
             (uint32_t) std::stoull(category3.get()));
       }
     }
@@ -293,6 +298,20 @@ void LogJoinTarget::onSession(
   obj.addChild(schema.id("cart_value_eurcents"), session.cart_value_eurcents);
   obj.addChild(schema.id("num_order_items"), session.num_order_items);
   obj.addChild(schema.id("gmv_eurcents"), session.gmv_eurcents);
+
+  auto first_seen = session.firstSeenTime();
+  if (!first_seen.isEmpty()) {
+    obj.addChild(
+        schema.id("first_seen_time"),
+        first_seen.get().unixMicros() / kMicrosPerSecond);
+  }
+
+  auto last_seen = session.lastSeenTime();
+  if (!last_seen.isEmpty()) {
+    obj.addChild(
+        schema.id("last_seen_time"),
+        last_seen.get().unixMicros() / kMicrosPerSecond);
+  }
 
   if (dry_run_) {
     fnord::logInfo(
