6c58a6a4b734b6ea61ae2154d35f2d6b39c0d749
Paul Asmuth <paul@paulasmuth.com>
2015-04-27 22:07:26 +0200

    ArtifactIndexReplication stub

---
+++ fnord-logtable/ArtifactIndexReplication.h
@@ -0,0 +1,37 @@
+/**
+ * This file is part of the "libfnord" project
+ *   Copyright (c) 2015 Paul Asmuth
+ *
+ * FnordMetric is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License v3.0. You should have received a
+ * copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+#ifndef _FNORD_LOGTABLE_ARTIFACTINDEXREPLICATION_H
+#define _FNORD_LOGTABLE_ARTIFACTINDEXREPLICATION_H
+#include <thread>
+#include <fnord-base/stdtypes.h>
+#include <fnord-base/uri.h>
+#include <fnord-logtable/ArtifactIndex.h>
+#include "fnord-http/httprequest.h"
+#include "fnord-http/httpconnectionpool.h"
+
+namespace fnord {
+namespace logtable {
+
+class ArtifactIndexReplication {
+public:
+
+  ArtifactIndexReplication(ArtifactIndex* index);
+
+  void replicateFrom(const ArtifactIndex& other);
+  void replicateFrom(const URI& index_uri, http::HTTPConnectionPool* http);
+
+protected:
+  ArtifactIndex* index_;
+};
+
+} // namespace logtable
+} // namespace fnord
+
+#endif
