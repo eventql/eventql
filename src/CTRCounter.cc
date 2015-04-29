7037c0de1390aa11114d0ee6de18320b8c7a0b25
Paul Asmuth <paul@paulasmuth.com>
2015-04-29 15:24:54 +0200

    revenue stats in top seach queries

--- src/CTRCounter.cc
+++ src/CTRCounter.cc
@@ -36,27 +36,43 @@ CTRCounterData CTRCounterData::load(const String& str) {
   return c;
 }
 
+static double mkRate(double num, double div) {
+  if (div == 0 || num == 0) {
+    return 0;
+  }
+
+  return num / div;
+}
+
 CTRCounterData::CTRCounterData() :
     num_views(0),
     num_clicks(0),
-    num_clicked(0) {}
+    num_clicked(0),
+    gmv_eurcent(0),
+    cart_value_eurcent(0) {}
 
 void CTRCounterData::merge(const CTRCounterData& other) {
   num_views += other.num_views;
   num_clicks += other.num_clicks;
   num_clicked += other.num_clicked;
+  gmv_eurcent += other.gmv_eurcent;
+  cart_value_eurcent += other.cart_value_eurcent;
 }
 
 void CTRCounterData::encode(util::BinaryMessageWriter* writer) const {
-  writer->appendUInt64(num_views);
-  writer->appendUInt64(num_clicks);
-  writer->appendUInt64(num_clicked);
+  writer->appendVarUInt(num_views);
+  writer->appendVarUInt(num_clicks);
+  writer->appendVarUInt(num_clicked);
+  writer->appendVarUInt(gmv_eurcent);
+  writer->appendVarUInt(cart_value_eurcent);
 }
 
 void CTRCounterData::decode(util::BinaryMessageReader* reader) {
-  num_views = *reader->readUInt64();
-  num_clicks = *reader->readUInt64();
-  num_clicked = *reader->readUInt64();
+  num_views = reader->readVarUInt();
+  num_clicks = reader->readVarUInt();
+  num_clicked = reader->readVarUInt();
+  gmv_eurcent = reader->readVarUInt();
+  cart_value_eurcent = reader->readVarUInt();
 }
 
 void CTRCounterData::toJSON(json::JSONOutputStream* json) const {
@@ -75,6 +91,18 @@ void CTRCounterData::toJSON(json::JSONOutputStream* json) const {
   json->addComma();
   json->addObjectEntry("cpq");
   json->addFloat(num_clicked / (double) num_views);
+  json->addComma();
+  json->addObjectEntry("total_gmv_eurcent");
+  json->addInteger(gmv_eurcent);
+  json->addComma();
+  json->addObjectEntry("total_cart_value_eurcent");
+  json->addInteger(cart_value_eurcent);
+  json->addComma();
+  json->addObjectEntry("avg_gmv_eurcent");
+  json->addInteger(mkRate(gmv_eurcent, num_views));
+  json->addComma();
+  json->addObjectEntry("avg_cart_value_eurcent");
+  json->addInteger(mkRate(cart_value_eurcent, num_views));
   json->endObject();
 }
 
