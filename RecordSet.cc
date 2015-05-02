468a3f3596d6a842811c2d8b319161c83b7c8562
Paul Asmuth <paul@paulasmuth.com>
2015-05-02 17:21:08 +0200

    RecordSet stub

---
+++ RecordSet.cc
@@ -0,0 +1,24 @@
+/**
+ * This file is part of the "libfnord" project
+ *   Copyright (c) 2015 Paul Asmuth
+ *
+ * FnordMetric is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License v3.0. You should have received a
+ * copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+#include <fnord-tsdb/RecordSet.h>
+
+namespace fnord {
+namespace tsdb {
+
+RecordSet::RecordSet(
+    RefPtr<msg::MessageSchema> schema,
+    const String& filename_prefix) :
+    schema_(schema),
+    filename_prefix_(filename_prefix) {}
+
+
+} // namespace tsdb
+} // namespace fnord
+
