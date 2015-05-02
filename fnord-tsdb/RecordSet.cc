ca2a04edde77a095be1b281df1829c6b0f53aac2
Paul Asmuth <paul@paulasmuth.com>
2015-05-02 18:52:11 +0200

    RecordSet::loadCommitlog

--- fnord-tsdb/RecordSet.cc
+++ fnord-tsdb/RecordSet.cc
@@ -7,7 +7,10 @@
  * copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
-#include <fnord-base/io/FileUtil.h>
+#include <fnord-base/io/fileutil.h>
+#include <fnord-base/io/mmappedfile.h>
+#include <fnord-base/util/binarymessagereader.h>
+#include <fnord-base/util/binarymessagewriter.h>
 #include <fnord-tsdb/RecordSet.h>
 
 namespace fnord {
@@ -15,9 +18,23 @@ namespace tsdb {
 
 RecordSet::RecordSet(
     RefPtr<msg::MessageSchema> schema,
-    const String& filename_prefix) :
+    const String& filename_prefix,
+    RecordSetState state /* = RecordSetState{} */) :
     schema_(schema),
-    filename_prefix_(filename_prefix) {}
+    filename_prefix_(filename_prefix),
+    state_(state) {
+  auto id_index_fn = [this] (uint64_t id, const void* data, size_t size) {
+    commitlog_ids_.emplace(id);
+  };
+
+  for (const auto& o : state_.old_commitlogs) {
+    loadCommitlog(o, id_index_fn);
+  }
+
+  if (!state.commitlog.isEmpty()) {
+    loadCommitlog(state.commitlog.get(), id_index_fn);
+  }
+}
 
 RecordSet::RecordSetState RecordSet::getState() const {
   std::unique_lock<std::mutex> lk(mutex_);
@@ -69,6 +86,30 @@ void RecordSet::rollCommitlog() {
   state_.commitlog = None<String>();
   state_.commitlog_size = 0;
 }
+
+void RecordSet::compact() {
+  std::unique_lock<std::mutex> lk(mutex_);
+  auto snap = state_;
+  lk.unlock();
+
+  //auto new_datafile = commitlog = filename_prefix_ + rnd_.hex64() + ".log";
+}
+
+void RecordSet::loadCommitlog(
+    const String& filename,
+    Function<void (uint64_t, const void*, size_t)> fn) {
+  io::MmappedFile mmap(File::openFile(filename, File::O_READ));
+  util::BinaryMessageReader reader(mmap.data(), mmap.size());
+
+  while (reader.remaining() > 0) {
+    auto id = *reader.readUInt64();
+    auto len = reader.readVarUInt();
+    auto data = reader.read(len);
+
+    fn(id, data, len);
+  }
+}
+
 
 RecordSet::RecordSetState::RecordSetState() : commitlog_size(0) {}
 
