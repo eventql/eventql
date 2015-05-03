a3ccf4030c78c30a730f3e84dea407368f149217
Paul Asmuth <paul@paulasmuth.com>
2015-05-03 02:16:09 +0200

    RecordMaterializerTest::TestReMatWithNonRepeatedParent

--- fnord-cstable/RecordMaterializer_test.cc
+++ fnord-cstable/RecordMaterializer_test.cc
@@ -110,7 +110,73 @@ TEST_CASE(RecordMaterializerTest, TestSimpleReMaterializationWithNull, [] () {
   l1_c.addChild(2, "fnord3");
   l1_c.addChild(2, "fnord4");
 
-  fnord::iputs("msg: $0", msg::MessagePrinter::print(sobj, schema));
+  cstable::CSTableBuilder builder(&schema);
+  builder.addRecord(sobj);
+  builder.write(testfile);
+
+  cstable::CSTableReader reader(testfile);
+  cstable::RecordMaterializer materializer(&schema, &reader);
+
+  msg::MessageObject robj;
+  materializer.nextRecord(&robj);
+
+  EXPECT_EQ(robj.asObject().size(), 3);
+  EXPECT_EQ(robj.asObject()[0].asObject().size(), 2);
+  EXPECT_EQ(robj.asObject()[0].asObject()[0].asString(), "fnord1");
+  EXPECT_EQ(robj.asObject()[0].asObject()[1].asString(), "fnord2");
+  EXPECT_EQ(robj.asObject()[1].asObject().size(), 0);
+  EXPECT_EQ(robj.asObject()[2].asObject().size(), 2);
+  EXPECT_EQ(robj.asObject()[2].asObject()[0].asString(), "fnord3");
+  EXPECT_EQ(robj.asObject()[2].asObject()[1].asString(), "fnord4");
+});
+
+
+TEST_CASE(RecordMaterializerTest, TestReMatWithNonRepeatedParent, [] () {
+  String testfile = "/tmp/__fnord_testcstablematerialization.cst";
+
+  msg::MessageSchemaField level1(
+      1,
+      "level1",
+      msg::FieldType::OBJECT,
+      0,
+      true,
+      false);
+
+  msg::MessageSchemaField level2(
+      2,
+      "level2",
+      msg::FieldType::OBJECT,
+      0,
+      true,
+      false);
+
+  level2.fields.emplace_back(
+      3,
+      "str",
+      msg::FieldType::STRING,
+      1024,
+      true,
+      false);
+
+  level1.fields.emplace_back(level2);
+  msg::MessageSchema schema(
+      "TestSchema",
+      Vector<msg::MessageSchemaField> { level1 });
+
+  msg::MessageObject sobj;
+  auto& l1_a = sobj.addChild(1);
+  auto& l2_aa = l1_a.addChild(2);
+  l2_aa.addChild(3, "fnord1");
+  l2_aa.addChild(3, "fnord2");
+  auto& l2_ab = l1_a.addChild(2);
+  l2_ab.addChild(3, "fnord3");
+  l2_ab.addChild(3, "fnord4");
+  auto& l1_b = sobj.addChild(1);
+  auto& l1_c = sobj.addChild(1);
+  auto& l2_ca = l1_c.addChild(2);
+  auto& l2_cb = l1_c.addChild(2);
+  l2_cb.addChild(3, "fnord5");
+  l2_cb.addChild(3, "fnord6");
 
   cstable::CSTableBuilder builder(&schema);
   builder.addRecord(sobj);
@@ -122,13 +188,18 @@ TEST_CASE(RecordMaterializerTest, TestSimpleReMaterializationWithNull, [] () {
   msg::MessageObject robj;
   materializer.nextRecord(&robj);
 
-  fnord::iputs("msg: $0", msg::MessagePrinter::print(robj, schema));
   EXPECT_EQ(robj.asObject().size(), 3);
   EXPECT_EQ(robj.asObject()[0].asObject().size(), 2);
-  EXPECT_EQ(robj.asObject()[0].asObject()[0].asString(), "fnord1");
-  EXPECT_EQ(robj.asObject()[0].asObject()[1].asString(), "fnord2");
+  EXPECT_EQ(robj.asObject()[0].asObject()[0].asObject().size(), 2);
+  EXPECT_EQ(robj.asObject()[0].asObject()[0].asObject()[0].asString(), "fnord1");
+  EXPECT_EQ(robj.asObject()[0].asObject()[0].asObject()[1].asString(), "fnord2");
+  EXPECT_EQ(robj.asObject()[0].asObject()[1].asObject().size(), 2);
+  EXPECT_EQ(robj.asObject()[0].asObject()[1].asObject()[0].asString(), "fnord3");
+  EXPECT_EQ(robj.asObject()[0].asObject()[1].asObject()[1].asString(), "fnord4");
   EXPECT_EQ(robj.asObject()[1].asObject().size(), 0);
   EXPECT_EQ(robj.asObject()[2].asObject().size(), 2);
-  EXPECT_EQ(robj.asObject()[2].asObject()[0].asString(), "fnord3");
-  EXPECT_EQ(robj.asObject()[2].asObject()[1].asString(), "fnord4");
+  EXPECT_EQ(robj.asObject()[2].asObject()[0].asObject().size(), 0);
+  EXPECT_EQ(robj.asObject()[2].asObject()[1].asObject().size(), 2);
+  EXPECT_EQ(robj.asObject()[2].asObject()[1].asObject()[0].asString(), "fnord5");
+  EXPECT_EQ(robj.asObject()[2].asObject()[1].asObject()[1].asString(), "fnord6");
 });
