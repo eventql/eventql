1665ca669a176ed654408a92ff87bd600dd14514
Paul Asmuth <paul@paulasmuth.com>
2015-05-10 22:36:36 +0200

    30s commit interval

--- src/cm-chunkserver.cc
+++ src/cm-chunkserver.cc
@@ -137,7 +137,7 @@ int main(int argc, const char** argv) {
   tsdb::StreamProperties config(new msg::MessageSchema(joinedSessionsSchema()));
   config.max_datafile_size = 1024 * 1024 * 512;
   config.chunk_size = Duration(3600 * 4 * kMicrosPerSecond);
-  config.compaction_interval = Duration(3 * kMicrosPerSecond);
+  config.compaction_interval = Duration(30 * kMicrosPerSecond);
   tsdb_node.configurePrefix("joined_sessions.", config);
 
   tsdb::TSDBServlet tsdb_servlet(&tsdb_node);
