3a61e737dcd7e292f210b0774d73ae012e49dc31
Paul Asmuth <paul@paulasmuth.com>
2015-05-12 00:07:55 +0200

    build cstable index

--- CSTableIndex.cc
+++ CSTableIndex.cc
@@ -8,10 +8,18 @@
  * <http://www.gnu.org/licenses/>.
  */
 #include <fnord-base/stdtypes.h>
+#include <fnord-base/logging.h>
 #include <fnord-tsdb/CSTableIndex.h>
+#include <fnord-tsdb/RecordSet.h>
+#include <fnord-msg/MessageDecoder.h>
+#include <fnord-cstable/CSTableBuilder.h>
 
 namespace fnord {
 namespace tsdb {
+
+CSTableIndex::CSTableIndex(
+    RefPtr<msg::MessageSchema> schema) :
+    schema_(schema) {}
 
 String CSTableIndex::name() {
   return "cstable";
@@ -24,7 +32,34 @@ void CSTableIndex::update(
     const Buffer& last_state,
     Buffer* new_state,
     Set<String>* delete_after_commit) {
-  fnord::iputs("update cstable: $0 .. $1", last_offset, cur_offset);
+  auto filename = records->filenamePrefix() + rnd_.hex64() + ".cst";
+
+  fnord::logDebug(
+      "fnord.tsdb",
+      "Building cstable: $0",
+      filename);
+
+  cstable::CSTableBuilder cstable(schema_.get());
+  auto cur = records->firstOffset();
+  auto end = records->lastOffset();
+  while (cur < end) {
+    records->fetchRecords(cur, 0xffffffff, [this, &cstable, &cur] (
+        uint64_t msgid,
+        const void* data,
+        size_t size) {
+      ++cur;
+      msg::MessageObject obj;
+      msg::MessageDecoder::decode(data, size, *schema_, &obj);
+      cstable.addRecord(obj);
+    });
+  }
+
+  cstable.write(filename);
+  new_state->append(filename.data(), filename.size());
+
+  if (last_state.size() > 0) {
+    delete_after_commit->emplace(last_state.toString());
+  }
 }
 
 }
