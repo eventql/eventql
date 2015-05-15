0de364785815ddafdcb4148c7e426a5e72f182dc
Laura Schlimmer <laura@etat-gray.com>
2015-05-15 16:51:08 +0200

    load joined session via google protobuf

--- src/JoinedSessionViewer.cc
+++ src/JoinedSessionViewer.cc
@@ -8,12 +8,25 @@ void JoinedSessionViewer::handleHTTPRequest(
   fnord::http::HTTPResponse* res) {
 
   if (req->method() == http::HTTPMessage::M_GET) {
-    fnord::iputs("get $0", 1);
+    URI uri(req->uri());
+    fnord::URI::ParamList params = uri.queryParams();
+
+    std::string session;
+    if (!fnord::URI::getParam(params, "session", &session)) {
+      res->addBody("error: missing session parameter");
+      res->setStatus(fnord::http::kStatusBadRequest);
+      return;
+    }
+
+    fnord::iputs("session parameter: $0", session);
   }
 
   if (req->method() == http::HTTPMessage::M_POST) {
     res->setStatus(fnord::http::kStatusFound);
     res->addHeader("Location", "/view_session");
+
+    Buffer body = req->body();
+    res->addBody(body);
   }
 }
 
