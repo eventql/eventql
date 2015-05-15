afc84c9a09c209c5af13d5aac229f326996e4788
Paul Asmuth <paul@paulasmuth.com>
2015-05-15 15:22:16 +0200

    cm-itemboostbuild

---
+++ src/cm-itemboostbuild.cc
@@ -0,0 +1,127 @@
+/**
+ * Copyright (c) 2015 - The CM Authors <legal@clickmatcher.com>
+ *   All Rights Reserved.
+ *
+ * This file is CONFIDENTIAL -- Distribution or duplication of this material or
+ * the information contained herein is strictly forbidden unless prior written
+ * permission is obtained.
+ */
+#include <algorithm>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include "fnord-base/io/fileutil.h"
+#include "fnord-base/thread/eventloop.h"
+#include "fnord-base/application.h"
+#include "fnord-base/logging.h"
+#include "fnord-base/Language.h"
+#include "fnord-base/random.h"
+#include "fnord-base/fnv.h"
+#include "fnord-base/cli/flagparser.h"
+#include "fnord-base/util/SimpleRateLimit.h"
+#include "fnord-base/InternMap.h"
+#include "fnord-base/thread/threadpool.h"
+#include "fnord-json/json.h"
+#include "fnord-mdb/MDB.h"
+#include "fnord-mdb/MDBUtil.h"
+#include "fnord-sstable/sstablereader.h"
+#include "fnord-sstable/sstablewriter.h"
+#include "fnord-sstable/SSTableColumnSchema.h"
+#include "fnord-sstable/SSTableColumnReader.h"
+#include "fnord-sstable/SSTableColumnWriter.h"
+#include "fnord-http/httpconnectionpool.h"
+#include <fnord-fts/fts.h>
+#include <fnord-fts/fts_common.h>
+#include <fnord-tsdb/TSDBClient.h>
+#include "fnord-logtable/TableReader.h"
+#include "common.h"
+#include "schemas.h"
+#include "CustomerNamespace.h"
+#include "CTRCounter.h"
+#include "analytics/ReportBuilder.h"
+#include "analytics/AnalyticsTableScanSource.h"
+#include "analytics/CTRByShopMapper.h"
+#include "analytics/ECommerceStatsByShopMapper.h"
+#include "analytics/ProductStatsByShopMapper.h"
+#include "analytics/CTRCounterMergeReducer.h"
+#include "analytics/CTRCounterTableSink.h"
+#include "analytics/CTRCounterTableSource.h"
+#include "analytics/RelatedTermsMapper.h"
+#include "analytics/TopCategoriesByTermMapper.h"
+#include "analytics/TermInfoMergeReducer.h"
+
+using namespace fnord;
+using namespace cm;
+
+fnord::thread::EventLoop ev;
+
+int main(int argc, const char** argv) {
+  fnord::Application::init();
+  fnord::Application::logToStderr();
+
+  fnord::cli::FlagParser flags;
+
+  flags.defineFlag(
+      "tempdir",
+      cli::FlagParser::T_STRING,
+      false,
+      NULL,
+      NULL,
+      "artifact directory",
+      "<path>");
+
+  flags.defineFlag(
+      "loglevel",
+      fnord::cli::FlagParser::T_STRING,
+      false,
+      NULL,
+      "INFO",
+      "loglevel",
+      "<level>");
+
+  flags.parseArgv(argc, argv);
+
+  Logger::get()->setMinimumLogLevel(
+      strToLogLevel(flags.getString("loglevel")));
+
+  /* start event loop */
+  auto evloop_thread = std::thread([] {
+    ev.run();
+  });
+
+  auto tempdir = flags.getString("tempdir");
+
+  http::HTTPConnectionPool http(&ev);
+  tsdb::TSDBClient tsdb("http://nue03.prod.fnrd.net:7003/tsdb", &http);
+
+  auto input_partitions = tsdb.listPartitions(
+      "joined_sessions.dawanda",
+      WallClock::unixMicros() - 90 * kMicrosPerDay,
+      WallClock::unixMicros() - 6 * kMicrosPerHour);
+
+  fnord::iputs("partitions: $0", input_partitions);
+/*
+  for (const auto& input_table : input_table_files) {
+    FNV<uint64_t> fnv;
+    auto h = fnv.hash(input_table);
+
+    auto table = StringUtil::format(
+        "$0/shopstats-ctr-dawanda.$1.sst",
+        tempdir,
+        StringUtil::hexPrint(&h, sizeof(h), false));
+
+    tables.emplace(table);
+    report_builder.addReport(
+        new CTRByShopMapper(
+            new AnalyticsTableScanSource(input_table),
+            new ShopStatsTableSink(table)));
+  }
+
+*/
+
+  ev.shutdown();
+  evloop_thread.join();
+
+  return 0;
+}
+
