755588dcb17caef48e73cd0a3fbf39f91c8d0045
Laura Schlimmer <laura@etat-gray.com>
2015-05-17 18:54:54 +0200

    SSETestServlet

--- src/SSETestServlet.cc
+++ src/SSETestServlet.cc
@@ -17,24 +17,18 @@ namespace cm {
 void SSETestServlet::handleHTTPRequest(
       http::HTTPRequest* req,
       http::HTTPResponseStream* res_stream) {
-  int i = 0;
-  auto write_next_chunk = [i, res_stream] () mutable {
-    String chunk = "blah blah blah\n\n";
-    fnord::iputs("write chunk... $0", i);
-
-    if (++i < 100) {
-      res_stream->writeBodyChunk(Buffer(chunk.data(), chunk.size()));
-      usleep(100000);
-    } else {
-      res_stream->finishResponse();
-    }
-  };
-
   http::HTTPResponse res;
   res.populateFromRequest(*req);
   res.setStatus(http::kStatusOK);
-  res_stream->onBodyWritten(write_next_chunk);
   res_stream->startResponse(res);
+
+  for (int i = 0; i < 100; ++i) {
+    String chunk = "blah blah blah\n\n";
+    res_stream->writeBodyChunk(Buffer(chunk.data(), chunk.size()));
+    usleep(100000);
+  }
+
+  res_stream->finishResponse();
 }
 
 }
