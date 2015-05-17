161d2e67d2f97b97d55972aee4dd8e1955344ed4
Laura Schlimmer <laura@etat-gray.com>
2015-05-17 18:39:42 +0200

    HTTPRespnseStream::onBodyWritten

--- fnord-http/HTTPResponseStream.h
+++ fnord-http/HTTPResponseStream.h
@@ -23,10 +23,43 @@ public:
 
   HTTPResponseStream(HTTPServerConnection* conn);
 
+  /**
+   * Write the provided http response (including headers and body) and then
+   * immediately finish the response. After calling this method you must not
+   * call any of startResponse, writeBodyChunk or finishResponse
+   */
+  void writeResponse(const HTTPResponse& resp);
+
+  /**
+   * Start writing the HTTP response (i.e. write the headers). After calling
+   * this method you may call writeBodyChunk zero or more times and then MUST
+   * call finishResponse
+   */
   void startResponse(const HTTPResponse& resp);
+
+  /**
+   * Write a HTTP response body chunk. Only legal if the HTTP response was 
+   * previously started by calling "startResponse". You may call this method
+   * zero or more times before calling finishResponse
+   */
   void writeBodyChunk(const Buffer& buf);
+
+  /**
+   * Finish the http response. Must be called iff the HTTP response was started
+   * by calling "startResponse"
+   */
   void finishResponse();
 
+  /**
+   * Register a callback that will be called after all pending body chunks
+   * have been written to the client.
+   */
+  void onBodyWritten(Function<void ()> callback);
+
+  /**
+   * Returns true if the HTTP response was started (i.e. we started writing the
+   * status line and some headers
+   */
   bool isOutputStarted() const;
 
 protected:
@@ -40,6 +73,7 @@ protected:
   bool headers_written_;
   bool response_finished_;
   Buffer buf_;
+  Function<void ()> on_body_written_;
 };
 
 }
