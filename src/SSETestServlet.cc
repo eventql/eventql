936e68fb47dc1c94c3df95cd550b0b30e1d4daa6
Laura Schlimmer <laura@etat-gray.com>
2015-05-18 13:37:14 +0200

    HTTPSSEStream test Servlet

--- src/SSETestServlet.cc
+++ src/SSETestServlet.cc
@@ -17,24 +17,16 @@ namespace cm {
 void SSETestServlet::handleHTTPRequest(
       RefPtr<http::HTTPRequestStream> req_stream,
       RefPtr<http::HTTPResponseStream> res_stream) {
-  req_stream->readBody();
 
-  Buffer pong;
-  pong.append("pong: ");
-  pong.append(req_stream->request().body());
-  pong.append("\n");
+  http::HTTPSSEStream sse(req_stream, res_stream);
+  sse.start();
 
-  http::HTTPResponse res;
-  res.populateFromRequest(req_stream->request());
-  res.setStatus(http::kStatusOK);
-  res_stream->startResponse(res);
-
-  for (int i = 0; i < 100; ++i) {
-    res_stream->writeBodyChunk(pong);
+  for (int i = 0; i < 10; ++i) {
+    sse.sendEvent(StringUtil::toString(i), "myid", "myevent");
     usleep(100000);
   }
 
-  res_stream->finishResponse();
+  sse.finish();
 }
 
 }
