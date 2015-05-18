cb00f48df6f4a895dc70197ec6a450fb2ce8c6ae
Laura Schlimmer <laura@etat-gray.com>
2015-05-18 14:54:43 +0200

    HTTPStreamingService Servlet Examples

---
+++ fnord-http/streaminghttpservice_example/HttpServlet.cc
@@ -0,0 +1,32 @@
+/**
+ * This file is part of the "libfnord" project
+ *   Copyright (c) 2015 Paul Asmuth
+ *   Copyright (c) 2015 Laura Schlimmer
+ *
+ * FnordMetric is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License v3.0. You should have received a
+ * copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+#include "HttpServlet.h"
+
+namespace fnord {
+namespace http {
+
+void HTTPServlet::handleHTTPRequest(
+      RefPtr<http::HTTPRequestStream> req,
+      RefPtr<http::HTTPResponseStream> res) {
+
+  HTTPResponse resp;
+  Buffer body;
+
+  body.append("Ping Pong");
+  req->readBody();
+  resp->populateFromRequest(req->request());
+  resp.setStatus(kStatusOK);
+  resp.addBody(body.data(), body.size());
+  res->writeResponse(resp);
+}
+
+}
+}
