0e094bea89bf0c0fe4adec10e21926113d2e10f2
Paul Asmuth <paul@paulasmuth.com>
2015-05-19 20:47:34 +0200

    implement caching in localscheduler

--- fnord-dproc/Task.h
+++ fnord-dproc/Task.h
@@ -12,6 +12,7 @@
 #include <fnord-base/stdtypes.h>
 #include <fnord-base/autoref.h>
 #include <fnord-base/buffer.h>
+#include <fnord-base/option.h>
 #include <fnord-base/exception.h>
 #include <fnord-base/VFSFile.h>
 #include <fnord-msg/msg.h>
@@ -54,6 +55,10 @@ public:
 
   virtual Vector<String> preferredLocations() {
     return Vector<String>{};
+  }
+
+  virtual Option<String> cacheKey() {
+    return None<String>();
   }
 
 };
