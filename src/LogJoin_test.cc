03f11569932969e47056c4baa66d3d0496f8d4c2
Laura Schlimmer <laura@etat-gray.com>
2015-05-22 01:57:37 +0200

    WIP

--- src/LogJoin_test.cc
+++ src/LogJoin_test.cc
@@ -18,10 +18,59 @@ using namespace cm;
 UNIT_TEST(LogJoinTest);
 
 TEST_CASE(LogJoinTest, Test1, [] () {
-  fnord::iputs("test logjoin", 1);
-  Buffer loglines_txt = FileUtil::read("src/test_loglines.txt");
-  Vector<String> loglines = StringUtil::split(loglines_txt.toString(), "/n");
-  TrackedSession session;
+  TrackedSession tracked_session;
+  auto loglines_txt = FileUtil::read("src/test_loglines.txt");
+  auto loglines = StringUtil::split(loglines_txt.toString(), "\n");
 
+  for (const auto& logline : loglines) {
+    if (logline.length() == 0) {
+      continue;
+    }
+
+    URI::ParamList params;
+    URI::parseQueryString(logline, &params);
+
+    String time;
+    String evtype;
+    String evid;
+    for (auto p = params.begin(); p != params.end(); ) {
+      switch (p->first[0]) {
+
+        default:
+          ++p;
+          continue;
+
+        case 't':
+          time = p->second;
+          break;
+
+        case 'e':
+          evtype = p->second;
+          break;
+
+        case 'c':
+          Vector<String> cid = StringUtil::split(p->second, "~");
+          evid = cid.at(1);
+          break;
+      }
+
+      p = params.erase(p);
+    }
+
+
+
+    //FIXME String -> uint_64
+    tracked_session.insertLogline(
+      DateTime(atoi(time.c_str())),
+      evtype,
+      evid,
+      params);
+  }
+
+  CurrencyConverter::ConversionTable tbl;
+  tbl.emplace_back(Currency::PLN, Currency::EUR, 0.25);
+  CurrencyConverter cconv(tbl);
+  tracked_session.joinEvents(cconv);
+  fnord::iputs("num queries: $0", tracked_session.queries.size());
   //fnord::iputs("loglines: $0", loglines.toString());
 });
