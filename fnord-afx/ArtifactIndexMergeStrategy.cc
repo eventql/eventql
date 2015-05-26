e422e2343560d0ea041ffa2c5e969bd4daf197eb
Paul Asmuth <paul@paulasmuth.com>
2015-05-26 20:06:22 +0200

    afx module

--- fnord-afx/ArtifactIndexMergeStrategy.cc
+++ fnord-afx/ArtifactIndexMergeStrategy.cc
@@ -7,7 +7,7 @@
  * copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
-#include <fnord-logtable/ArtifactIndexMergeStrategy.h>
+#include <fnord-afx/ArtifactIndexMergeStrategy.h>
 
 namespace fnord {
 namespace logtable {
--- fnord-afx/ArtifactIndexMergeStrategy.h
+++ fnord-afx/ArtifactIndexMergeStrategy.h
@@ -10,7 +10,7 @@
 #ifndef _FNORD_LOGTABLE_ARTIFACTINDEXMERGESTRATEGY_H
 #define _FNORD_LOGTABLE_ARTIFACTINDEXMERGESTRATEGY_H
 #include <fnord-base/stdtypes.h>
-#include <fnord-logtable/ArtifactIndex.h>
+#include <fnord-afx/ArtifactIndex.h>
 
 namespace fnord {
 namespace logtable {
