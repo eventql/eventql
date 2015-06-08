b8276631148cab41ce45213f9d14ee0792dc7401
Paul Asmuth <paul@paulasmuth.com>
2015-06-08 18:29:38 +0200

    IndexChangeRequest via PB

--- src/IndexChangeRequest.h
+++ src/IndexChangeRequest.h
@@ -18,21 +18,24 @@
 #include <fnord-base/uri.h>
 #include <fnord-base/reflect/reflect.h>
 #include "ItemRef.h"
+#include "IndexChangeRequest.pb.h"
 
 using namespace fnord;
 
 namespace cm {
 
-struct IndexChangeRequest {
+struct IndexChangeRequestStruct {
   String customer;
   ItemRef item;
   HashMap<String, String> attrs;
 
+  IndexChangeRequest toIndexChangeRequest() const;
+
   template <typename T>
   static void reflect(T* meta) {
-    meta->prop(&IndexChangeRequest::customer, 1, "customer", false);
-    meta->prop(&IndexChangeRequest::item, 2, "docid", false);
-    meta->prop(&IndexChangeRequest::attrs, 3, "attributes", false);
+    meta->prop(&IndexChangeRequestStruct::customer, 1, "customer", false);
+    meta->prop(&IndexChangeRequestStruct::item, 2, "docid", false);
+    meta->prop(&IndexChangeRequestStruct::attrs, 3, "attributes", false);
   };
 };
 
