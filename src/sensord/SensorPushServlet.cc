bb6aaba375429007666a226a7ae558c60e320ac8
Paul Asmuth <paul@paulasmuth.com>
2015-06-09 16:23:40 +0200

    Squashed '3rdparty/sensord/' changes from 67ddf8b..8848b01
    8848b01 remove bogus include
    c2d79f9 SensorSampleFeed impl
    9365595 SensorServlet -> SensorPushServlet
    ea3062e fix includes
    7611157 move SensorPushServlet into sensord, cmake
    88dce95 SensorServlet
    68fcde9 fix dir structure
    8d12649 SensorSampleFeed
    2e1cdd3 NO_BUNDLED_LIBFNORD
    
    git-subtree-dir: 3rdparty/sensord
    git-subtree-split: 8848b0177a2e520ea121528c3fa0a46269c8369e

---
+++ src/sensord/SensorPushServlet.cc
@@ -0,0 +1,59 @@
+/**
+ * This file is part of the "libfnord" project
+ *   Copyright (c) 2015 Paul Asmuth
+ *
+ * FnordMetric is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License v3.0. You should have received a
+ * copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+#include <fnord-base/wallclock.h>
+#include <fnord-msg/msg.h>
+#include <sensord/SensorPushServlet.h>
+
+namespace fnord {
+namespace metricdb {
+
+SensorPushServlet::SensorPushServlet(
+    sensord::SensorSampleFeed* sensor_feed) :
+    sensor_feed_(sensor_feed) {}
+
+void SensorPushServlet::handleHTTPRequest(
+    fnord::http::HTTPRequest* req,
+    fnord::http::HTTPResponse* res) {
+  URI uri(req->uri());
+
+  res->addHeader("Access-Control-Allow-Origin", "*");
+
+  try {
+    if (StringUtil::endsWith(uri.path(), "/push")) {
+      return pushSample(req, res, &uri);
+    }
+
+    res->setStatus(fnord::http::kStatusNotFound);
+    res->addBody("not found");
+  } catch (const Exception& e) {
+    res->setStatus(http::kStatusInternalServerError);
+    res->addBody(StringUtil::format("error: $0: $1", e.getTypeName(), e.getMessage()));
+  }
+}
+
+void SensorPushServlet::pushSample(
+    http::HTTPRequest* req,
+    http::HTTPResponse* res,
+    URI* uri) {
+  const auto& params = uri->queryParams();
+
+  sensord::SampleList samples;
+  msg::decode(req->body(), &samples);
+
+  for (const auto& sample : samples.samples()) {
+    sensor_feed_->publish(sample);
+  }
+
+  res->setStatus(http::kStatusCreated);
+}
+
+}
+}
+
