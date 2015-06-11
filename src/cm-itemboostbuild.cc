26f303979adefbaa3c3ec40feb358ffa7cd16f2d
Paul Asmuth <paul@paulasmuth.com>
2015-06-12 00:34:52 +0200

    fnord-tsdb -> tsdb

--- src/cm-itemboostbuild.cc
+++ src/cm-itemboostbuild.cc
@@ -17,7 +17,7 @@
 #include <fnord/http/httpconnectionpool.h>
 #include <dproc/Application.h>
 #include <dproc/LocalScheduler.h>
-#include <fnord-tsdb/TSDBClient.h>
+#include <tsdb/TSDBClient.h>
 #include "analytics/ItemBoostMapper.h"
 #include "analytics/ItemBoostMerge.h"
 #include "analytics/ItemBoostExport.h"
