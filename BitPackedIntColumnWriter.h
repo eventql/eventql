58e215158d9ebec26d0539c76696d0c1655e69b3
Paul Asmuth <paul@paulasmuth.com>
2015-06-12 03:15:09 +0200

    Merge commit 'ba28ec463be6efb232aebb12070d34a2223f65a8'
    Conflicts:
    	lib/libcstable/CMakeLists.txt

---
+++ BitPackedIntColumnWriter.h
@@ -0,0 +1,53 @@
+/**
+ * This file is part of the "libfnord" project
+ *   Copyright (c) 2014 Paul Asmuth, Google Inc.
+ *
+ * FnordMetric is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License v3.0. You should have received a
+ * copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+#ifndef _FNORD_CSTABLE_UINT16COLUMNWRITER_H
+#define _FNORD_CSTABLE_UINT16COLUMNWRITER_H
+#include <fnord-base/stdtypes.h>
+#include <fnord-base/util/binarymessagewriter.h>
+#include <fnord-base/util/BitPackEncoder.h>
+#include <fnord-cstable/BinaryFormat.h>
+#include <fnord-cstable/ColumnWriter.h>
+
+namespace fnord {
+namespace cstable {
+
+class BitPackedIntColumnWriter : public ColumnWriter {
+public:
+
+  BitPackedIntColumnWriter(
+      uint64_t r_max,
+      uint64_t d_max,
+      uint32_t max_value = 0xffffffff);
+
+  void addDatum(
+      uint64_t rep_level,
+      uint64_t def_level,
+      const void* data,
+      size_t size) override;
+
+  void addDatum(uint64_t rep_level, uint64_t def_level, uint32_t value);
+  void commit();
+
+  ColumnType type() const override {
+    return ColumnType::UINT32_BITPACKED;
+  }
+
+protected:
+  size_t size() const override;
+  void write(util::BinaryMessageWriter* writer) override;
+
+  uint32_t max_value_;
+  util::BitPackEncoder data_writer_;
+};
+
+} // namespace cstable
+} // namespace fnord
+
+#endif
---
+++ BooleanColumnWriter.h
@@ -0,0 +1,49 @@
+/**
+ * This file is part of the "libfnord" project
+ *   Copyright (c) 2014 Paul Asmuth, Google Inc.
+ *
+ * FnordMetric is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License v3.0. You should have received a
+ * copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+#ifndef _FNORD_CSTABLE_BOOLEANCOLUMNWRITER_H
+#define _FNORD_CSTABLE_BOOLEANCOLUMNWRITER_H
+#include <fnord-base/stdtypes.h>
+#include <fnord-base/util/binarymessagewriter.h>
+#include <fnord-base/util/BitPackEncoder.h>
+#include <fnord-cstable/ColumnWriter.h>
+
+namespace fnord {
+namespace cstable {
+
+class BooleanColumnWriter : public ColumnWriter {
+public:
+
+  BooleanColumnWriter(uint64_t r_max, uint64_t d_max);
+
+  void addDatum(
+      uint64_t rep_level,
+      uint64_t def_level,
+      const void* data,
+      size_t size) override;
+
+  void addDatum(uint64_t rep_level, uint64_t def_level, bool value);
+
+  void commit();
+
+  ColumnType type() const override {
+    return ColumnType::BOOLEAN;
+  }
+
+protected:
+  size_t size() const override;
+  void write(util::BinaryMessageWriter* writer) override;
+
+  util::BitPackEncoder data_writer_;
+};
+
+} // namespace cstable
+} // namespace fnord
+
+#endif
