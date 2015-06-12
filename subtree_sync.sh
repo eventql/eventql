53402871876892726a3abc7cf253cf635237135c
Paul Asmuth <paul@paulasmuth.com>
2015-06-12 02:36:30 +0200

    subtree_sync

--- subtree_sync.sh
+++ subtree_sync.sh
@@ -6,8 +6,14 @@ git subtree push --prefix app/metricd git@github.com:fnordcorp/metricd.git maste
 git subtree pull --prefix app/sensord git@github.com:fnordcorp/sensord.git master
 git subtree push --prefix app/sensord git@github.com:fnordcorp/sensord.git master
 
+git subtree pull --prefix app/brokerd git@github.com:fnordcorp/brokerd.git master
+git subtree push --prefix app/brokerd git@github.com:fnordcorp/brokerd.git master
+
 git subtree pull --prefix app/tsdb git@github.com:fnordcorp/tsdb.git master
 git subtree push --prefix app/tsdb git@github.com:fnordcorp/tsdb.git master
+
+git subtree pull --prefix app/chartsql git@github.com:fnordcorp/chartsql.git master
+git subtree push --prefix app/chartsql git@github.com:fnordcorp/chartsql.git master
 
 git subtree pull --prefix lib/libsstable git@github.com:fnordcorp/libsstable.git master
 git subtree push --prefix lib/libsstable git@github.com:fnordcorp/libsstable.git master
@@ -21,5 +27,3 @@ git subtree push --prefix lib/libdproc git@github.com:fnordcorp/libdproc.git mas
 git subtree pull --prefix lib/libfnord git@github.com:fnordcorp/libfnord.git master
 git subtree push --prefix lib/libfnord git@github.com:fnordcorp/libfnord.git master
 
-git subtree pull --prefix app/brokerd git@github.com:fnordcorp/brokerd.git master
-git subtree push --prefix app/brokerd git@github.com:fnordcorp/brokerd.git master
