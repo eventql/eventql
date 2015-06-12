b047601c323c84e5bbced92b02b2b7fe16e1521a
Paul Asmuth <paul@paulasmuth.com>
2015-06-12 20:20:59 +0200

    ...

--- subtree_sync.sh
+++ subtree_sync.sh
@@ -9,7 +9,7 @@ for i in $(seq 1 2); do
   git subtree pull --prefix lib/libsstable git@github.com:fnordcorp/libsstable.git master
   git subtree pull --prefix lib/libcstable git@github.com:fnordcorp/libcstable.git master
   git subtree pull --prefix lib/libfnord git@github.com:fnordcorp/libfnord.git master
-  git subtree pull --prefix lib/libdproc git@github.com:fnordcorp/libdproc.git master
+  #git subtree pull --prefix lib/libdproc git@github.com:fnordcorp/libdproc.git master
 done;
 
 git subtree push --prefix app/sensord git@github.com:fnordcorp/sensord.git master
@@ -19,6 +19,6 @@ git subtree push --prefix app/tsdb git@github.com:fnordcorp/tsdb.git master
 git subtree push --prefix app/chartsql git@github.com:fnordcorp/chartsql.git master
 git subtree push --prefix lib/libsstable git@github.com:fnordcorp/libsstable.git master
 git subtree push --prefix lib/libcstable git@github.com:fnordcorp/libcstable.git master
-git subtree push --prefix lib/libdproc git@github.com:fnordcorp/libdproc.git master
+#git subtree push --prefix lib/libdproc git@github.com:fnordcorp/libdproc.git master
 git subtree push --prefix lib/libfnord git@github.com:fnordcorp/libfnord.git master
 
