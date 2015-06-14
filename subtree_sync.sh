cb90e8e4204ecf082784ead795363812343e20a5
Paul Asmuth <paul@paulasmuth.com>
2015-06-14 17:11:05 +0200

    par sync

--- subtree_sync.sh
+++ subtree_sync.sh
@@ -13,12 +13,13 @@ for i in $(seq 1 2); do
 done;
 
 #git subtree push --prefix app/sensord git@github.com:fnordcorp/sensord.git master
-git subtree push --prefix app/metricd git@github.com:fnordcorp/metricd.git master
-git subtree push --prefix app/brokerd git@github.com:fnordcorp/brokerd.git master
-git subtree push --prefix app/tsdb git@github.com:fnordcorp/tsdb.git master
-git subtree push --prefix app/chartsql git@github.com:fnordcorp/chartsql.git master
-git subtree push --prefix lib/libsstable git@github.com:fnordcorp/libsstable.git master
-git subtree push --prefix lib/libcstable git@github.com:fnordcorp/libcstable.git master
+git subtree push --prefix app/metricd git@github.com:fnordcorp/metricd.git master&
+git subtree push --prefix app/brokerd git@github.com:fnordcorp/brokerd.git master&
+git subtree push --prefix app/tsdb git@github.com:fnordcorp/tsdb.git master&
+git subtree push --prefix app/chartsql git@github.com:fnordcorp/chartsql.git master&
+git subtree push --prefix lib/libsstable git@github.com:fnordcorp/libsstable.git master&
+git subtree push --prefix lib/libcstable git@github.com:fnordcorp/libcstable.git master&
 #git subtree push --prefix lib/libdproc git@github.com:fnordcorp/libdproc.git master
-git subtree push --prefix lib/libfnord git@github.com:fnordcorp/libfnord.git master
+git subtree push --prefix lib/libfnord git@github.com:fnordcorp/libfnord.git master&
 
+wait
