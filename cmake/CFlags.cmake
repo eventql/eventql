ac82397ab1fc52fdc8f5bbce01b0218cc3112579
Paul Asmuth <paul@paulasmuth.com>
2015-06-21 18:03:30 +0200

    cflags

---
+++ cmake/CFlags.cmake
@@ -0,0 +1,25 @@
+set(CMAKE_CXX_FLAGS "-std=c++0x -ftemplate-depth=500 -mno-omit-leaf-frame-pointer -fno-omit-frame-pointer -Wall -pedantic ${CMAKE_CXX_FLAGS}")
+set(CMAKE_EXE_LINKER_FLAGS "-lprotobuf ${CMAKE_EXE_LINKER_FLAGS}")
+
+set(CMAKE_CXX_FLAGS_DEBUG "-g -fsanitize=address -fsanitize=memory -Qunused-arguments ${CMAKE_CXX_FLAGS_DEBUG}")
+set(CMAKE_EXE_LINKER_FLAGS_DEBUG "-fsanitize=address -fsanitize=memory ${CMAKE_EXE_LINKER_FLAGS_DEBUG}")
+
+# OSX FLAGS
+if(APPLE)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++ ")
+
+# LINUX FLAGS
+else()
+  set(CMAKE_EXE_LINKER_FLAGS "-ldl -static-libstdc++ -static-libgcc ${CMAKE_EXE_LINKER_FLAGS}")
+  set(CMAKE_FIND_LIBRARY_SUFFIXES ".a")
+  set(BUILD_SHARED_LIBRARIES OFF)
+  set(CMAKE_EXE_LINK_DYNAMIC_C_FLAGS)       # remove -Wl,-Bdynamic
+  set(CMAKE_EXE_LINK_DYNAMIC_CXX_FLAGS)
+  set(CMAKE_SHARED_LIBRARY_C_FLAGS)         # remove -fPIC
+  set(CMAKE_SHARED_LIBRARY_CXX_FLAGS)
+  set(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS)    # remove -rdynamic
+  set(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS)
+endif()
+
+# strip binaries
+#  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -s")
