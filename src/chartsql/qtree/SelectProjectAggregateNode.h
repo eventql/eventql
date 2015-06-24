74e393b7cbf1e89a1c8c40ba5f1058ffd9b49d06
Paul Asmuth <paul@paulasmuth.com>
2015-06-24 23:47:32 +0200

    qtree iface

---
+++ src/chartsql/qtree/SelectProjectAggregateNode.h
@@ -0,0 +1,31 @@
+/**
+ * This file is part of the "libfnord" project
+ *   Copyright (c) 2015 Paul Asmuth
+ *
+ * FnordMetric is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License v3.0. You should have received a
+ * copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+#include <fnord/stdtypes.h>
+#include <chartsql/qtree/QueryTreeNode.h>
+#include <chartsql/qtree/ScalarExpressionNode.h>
+
+using namespace fnord;
+
+namespace csql {
+
+class SelectProjectAggregateNode : public QueryTreeNode {
+public:
+
+  SelectProjectAggregateNode(
+      Vector<RefPtr<ScalarExpressionNode>> select_list,
+      RefPtr<ScalarExpressionNode> where_expr);
+
+protected:
+  Vector<RefPtr<ScalarExpressionNode>> select_list_;
+  RefPtr<ScalarExpressionNode> where_expr_;
+};
+
+} // namespace csql
