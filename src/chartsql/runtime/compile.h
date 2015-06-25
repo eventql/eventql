d93a16822ea63585c3c42b185b6f78d88f49c50a
Paul Asmuth <paul@paulasmuth.com>
2015-06-25 17:42:23 +0200

    {CompiledProgram,ScratchMemory}.h

--- src/chartsql/runtime/compile.h
+++ src/chartsql/runtime/compile.h
@@ -13,6 +13,7 @@
 #include <vector>
 #include <string>
 #include <chartsql/runtime/symboltable.h>
+#include <chartsql/runtime/CompiledProgram.h>
 #include <chartsql/qtree/ScalarExpressionNode.h>
 #include <chartsql/qtree/FieldReferenceNode.h>
 #include <chartsql/qtree/BuiltinExpressionNode.h>
@@ -22,59 +23,6 @@ using namespace fnord;
 namespace csql {
 class ASTNode;
 class SValue;
-
-enum kCompiledExpressionType {
-  X_CALL,
-  X_LITERAL,
-  X_INPUT,
-  X_MULTI
-};
-
-struct CompiledExpression {
-  kCompiledExpressionType type;
-  void (*call)(void*, int, SValue*, SValue*);
-  void* arg0;
-  CompiledExpression* next;
-  CompiledExpression* child;
-  ScalarExpression* expr;
-};
-
-class ScratchMemory {
-public:
-
-  void* alloc(size_t size);
-
-protected:
-  Buffer buf_;
-};
-
-class CompiledProgram {
-public:
-
-  CompiledProgram(CompiledExpression* expr, size_t scratchpad_size);
-
-  void* alloc(ScratchMemory* scratch) const;
-
-  void init(void* scratchpad);
-  void free(void* scratchpad);
-
-  void accumulate(
-      void* scratchpad,
-      int argc,
-      const SValue* argv);
-
-  void evaluate(
-      void* scratchpad,
-      int argc,
-      const SValue* argv,
-      SValue* out);
-
-  void reset(void* scratchpad);
-
-protected:
-  CompiledExpression* expr_;
-  size_t scratchpad_size_;
-};
 
 class Compiler {
 public:
