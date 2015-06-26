632cacefec9ef6fb2ee8af17daa6062a746c2f62
Paul Asmuth <paul@paulasmuth.com>
2015-06-26 02:52:39 +0200

    RuntimeTest, LiteralExpressionNode

---
+++ src/chartsql/qtree/LiteralExpressionNode.cc
@@ -0,0 +1,26 @@
+/**
+ * This file is part of the "libfnord" project
+ *   Copyright (c) 2015 Paul Asmuth
+ *
+ * FnordMetric is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License v3.0. You should have received a
+ * copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+#include <chartsql/qtree/LiteralExpressionNode.h>
+
+using namespace fnord;
+
+namespace csql {
+
+LiteralExpressionNode::LiteralExpressionNode(SValue value) : value_(value) {}
+
+const SValue& LiteralExpressionNode::value() const {
+  return value_;
+}
+
+Vector<RefPtr<ScalarExpressionNode>> LiteralExpressionNode::arguments() const {
+  return Vector<RefPtr<ScalarExpressionNode>>{};
+}
+
+} // namespace csql
---
+++ src/chartsql/runtime/Runtime_test.cc
@@ -0,0 +1,44 @@
+/**
+ * This file is part of the "libfnord" project
+ *   Copyright (c) 2015 Paul Asmuth
+ *
+ * FnordMetric is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License v3.0. You should have received a
+ * copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+#include <fnord/stdtypes.h>
+#include <fnord/exception.h>
+#include <fnord/test/unittest.h>
+#include "chartsql/runtime/DefaultRuntime.h"
+#include "chartsql/qtree/SelectProjectAggregateNode.h"
+#include "chartsql/qtree/FieldReferenceNode.h"
+#include "chartsql/qtree/BuiltinExpressionNode.h"
+#include "chartsql/qtree/LiteralExpressionNode.h"
+
+using namespace fnord;
+using namespace csql;
+
+UNIT_TEST(RuntimeTest);
+
+TEST_CASE(RuntimeTest, TestStaticExpression, [] () {
+  DefaultRuntime runtime;
+
+  auto expr = mkRef(
+      new csql::BuiltinExpressionNode(
+          "add",
+          {
+            new csql::LiteralExpressionNode(SValue(SValue::IntegerType(1))),
+            new csql::LiteralExpressionNode(SValue(SValue::IntegerType(2))),
+          }));
+
+
+  auto compiled = runtime.compiler()->compile(expr.get());
+
+  SValue out;
+  compiled->evaluateStatic(0, nullptr, &out);
+
+  fnord::iputs("result: $0", out.toString());
+  EXPECT_EQ(out.getInteger(), 3);
+});
+
