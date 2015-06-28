cdb6b915472bc02a45c99ce44e06f12ab9b43a4b
Paul Asmuth <paul@paulasmuth.com>
2015-06-28 20:43:53 +0200

    make SymbolTable available in QueryPlanBuilder

--- src/chartsql/runtime/defaultruntime.cc
+++ src/chartsql/runtime/defaultruntime.cc
@@ -8,74 +8,12 @@
  * <http://www.gnu.org/licenses/>.
  */
 #include <chartsql/runtime/defaultruntime.h>
-#include <chartsql/expressions/aggregate.h>
-#include <chartsql/expressions/boolean.h>
-#include <chartsql/expressions/datetime.h>
-#include <chartsql/expressions/math.h>
 
 namespace csql {
 
-DefaultRuntime::DefaultRuntime() :
-  scalar_exp_builder_(&symbol_table_) {
-
-  /* expressions/aggregate.h */
-  symbol_table_.registerFunction("count", expressions::kCountExpr);
-  symbol_table_.registerFunction("sum", expressions::kSumExpr);
-
-  symbol_table_.registerSymbol(
-      "mean",
-      &expressions::meanExpr,
-      expressions::meanExprScratchpadSize(),
-      &expressions::meanExprFree);
-
-  symbol_table_.registerSymbol(
-      "avg",
-      &expressions::meanExpr,
-      expressions::meanExprScratchpadSize(),
-      &expressions::meanExprFree);
-
-  symbol_table_.registerSymbol(
-      "average",
-      &expressions::meanExpr,
-      expressions::meanExprScratchpadSize(),
-      &expressions::meanExprFree);
-
-  symbol_table_.registerSymbol(
-      "min",
-      &expressions::minExpr,
-      expressions::minExprScratchpadSize(),
-      &expressions::minExprFree);
-
-  symbol_table_.registerSymbol(
-      "max",
-      &expressions::maxExpr,
-      expressions::maxExprScratchpadSize(),
-      &expressions::maxExprFree);
-
-  /* expressions/boolean.h */
-  symbol_table_.registerSymbol("eq", &expressions::eqExpr);
-  symbol_table_.registerSymbol("neq", &expressions::neqExpr);
-  symbol_table_.registerSymbol("and", &expressions::andExpr);
-  symbol_table_.registerSymbol("or", &expressions::orExpr);
-  symbol_table_.registerSymbol("neg", &expressions::negExpr);
-  symbol_table_.registerSymbol("lt", &expressions::ltExpr);
-  symbol_table_.registerSymbol("lte", &expressions::lteExpr);
-  symbol_table_.registerSymbol("gt", &expressions::gtExpr);
-  symbol_table_.registerSymbol("gte", &expressions::gteExpr);
-
-  /* expressions/datetime.h */
-  symbol_table_.registerSymbol("FROM_TIMESTAMP", &expressions::fromTimestamp);
-
-  /* expressions/math.h */
-  //symbol_table_.registerSymbol("add", &expressions::addExpr);
-  symbol_table_.registerFunction("add", &expressions::addExpr);
-
-  symbol_table_.registerSymbol("sub", &expressions::subExpr);
-  symbol_table_.registerSymbol("mul", &expressions::mulExpr);
-  symbol_table_.registerSymbol("div", &expressions::divExpr);
-  symbol_table_.registerSymbol("mod", &expressions::modExpr);
-  symbol_table_.registerSymbol("pow", &expressions::powExpr);
-}
+DefaultRuntime::DefaultRuntime(
+    SymbolTable* symbol_table) :
+    scalar_exp_builder_(symbol_table) {}
 
 RefPtr<ExecutionPlan> DefaultRuntime::buildExecutionPlan(
     RefPtr<QueryTreeNode> qtree,
