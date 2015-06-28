9c1af65edc0f808f2e166b984db263f1cecbbb99
Paul Asmuth <paul@paulasmuth.com>
2015-06-28 20:13:19 +0200

    hack out old queryplanbuilder

--- src/chartsql/runtime/runtime.cc
+++ src/chartsql/runtime/runtime.cc
@@ -14,11 +14,10 @@
 namespace csql {
 
 Runtime::Runtime() :
-    compiler_(&symbol_table_),
-    query_plan_builder_(&compiler_, backends_) {}
+    compiler_(&symbol_table_) {}
 
 QueryPlanBuilder* Runtime::queryPlanBuilder() {
-  return &query_plan_builder_;
+  RAISE(kNotImplementedError);
 }
 
 void Runtime::addBackend(std::unique_ptr<Backend> backend) {
