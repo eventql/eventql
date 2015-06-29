fb053e7770da22f1523f68c43b275565071ef99d
Paul Asmuth <paul@paulasmuth.com>
2015-06-29 16:14:17 +0200

    SelectExpression impl

---
+++ src/chartsql/runtime/SelectExpression.cc
@@ -0,0 +1,30 @@
+/**
+ * This file is part of the "FnordMetric" project
+ *   Copyright (c) 2014 Paul Asmuth, Google Inc.
+ *
+ * FnordMetric is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License v3.0. You should have received a
+ * copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+#include <chartsql/runtime/SelectExpression.h>
+
+namespace csql {
+
+SelectExpression::SelectExpression(
+    Vector<ScopedPtr<ValueExpression>> select_expressions) :
+    select_exprs_(std::move(select_expressions)) {}
+
+void SelectExpression::execute(
+    ExecutionContext* context,
+    Function<bool (int argc, const SValue* argv)> fn) {
+  Vector<SValue> out_row(select_exprs_.size(), SValue{});
+
+  for (int i = 0; i < select_exprs_.size(); ++i) {
+    select_exprs_[i]->evaluate(0, nullptr,  &out_row[i]);
+  }
+
+  fn(out_row.size(), out_row.data());
+}
+
+}
