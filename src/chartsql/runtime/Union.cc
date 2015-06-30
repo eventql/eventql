53f63752118406ecf33f2e4960b291dee1aa3522
Paul Asmuth <paul@paulasmuth.com>
2015-06-30 22:16:02 +0200

    Union column names

--- src/chartsql/runtime/Union.cc
+++ src/chartsql/runtime/Union.cc
@@ -14,7 +14,17 @@ namespace csql {
 
 Union::Union(
     Vector<ScopedPtr<TableExpression>> sources) :
-    sources_(std::move(sources)) {}
+    sources_(std::move(sources)) {
+  if (sources_.size() == 0) {
+    RAISE(kRuntimeError, "UNION must have at least one source table");
+  }
+
+  for (const auto& s : sources_) {
+    if (s->numColunns() != sources_[0]->numColunns()) {
+      RAISE(kRuntimeError, "UNION tables return different number of columns");
+    }
+  }
+}
 
 void Union::execute(
     ExecutionContext* context,
@@ -27,6 +37,14 @@ void Union::execute(
       return fn(sargc, sargv);
     });
   }
+}
+
+Vector<String> Union::columnNames() const {
+  sources_[0]->columnNames();
+}
+
+size_t Union::numColunns() const {
+  sources_[0]->numColunns();
 }
 
 }
