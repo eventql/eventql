0bd900e2c4e83e823cce92a6af3e4b5455d836d3
Paul Asmuth <paul@paulasmuth.com>
2015-06-30 19:10:06 +0200

    JSONSSEStreamFormat

---
+++ src/chartsql/runtime/JSONSSEStreamFormat.cc
@@ -0,0 +1,59 @@
+/**
+ * This file is part of the "libfnord" project
+ *   Copyright (c) 2015 Paul Asmuth
+ *
+ * FnordMetric is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License v3.0. You should have received a
+ * copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+#include <fnord/logging.h>
+#include <chartsql/runtime/ASCIITableFormat.h>
+#include <chartsql/runtime/JSONSSEStreamFormat.h>
+
+namespace csql {
+
+JSONSSEStreamFormat::JSONSSEStreamFormat(
+    http::HTTPSSEStream* output) :
+    output_(output) {}
+
+void JSONSSEStreamFormat::formatResults(
+    RefPtr<QueryPlan> query,
+    ExecutionContext* context) {
+  try {
+    context->onStatusChange([this] (const csql::ExecutionStatus& status) {
+      auto progress = status.progress();
+
+      Buffer buf;
+      json::JSONOutputStream json(BufferOutputStream::fromBuffer(&buf));
+      json.beginObject();
+      json.addObjectEntry("status");
+      json.addString("running");
+      json.addComma();
+      json.addObjectEntry("progress");
+      json.addFloat(progress);
+      json.addComma();
+      json.addObjectEntry("message");
+      if (progress == 0.0f) {
+        json.addString("Waiting...");
+      } else if (progress == 1.0f) {
+        json.addString("Downloading...");
+      } else {
+        json.addString("Running...");
+      }
+      json.endObject();
+
+      output_->sendEvent(buf, Some(String("status")));
+    });
+
+    Buffer result;
+    ASCIITableFormat format(BufferOutputStream::fromBuffer(&result));
+    format.formatResults(query, context);
+    output_->sendEvent(result, Some(String("result")));
+  } catch (const StandardException& e) {
+    fnord::logError("sql", e, "SQL execution failed");
+    output_->sendEvent(e.what(), Some(String("error")));
+  }
+}
+
+}
