4310677f3b2aff8d24ef4e8deabeff5eba890749
Paul Asmuth <paul@paulasmuth.com>
2015-07-25 18:06:39 +0200

    join pageviews

--- todo.c
+++ todo.c
@@ -53,3 +53,38 @@
 //            msg::TRUE);
 //      }
 //      /* EOF DAWANDA HACK */
+
+
+    auto docid = iv.item.docID();
+    auto shopid = get_field_(docid, "shop_id");
+    if (shopid.isEmpty()) {
+      //stx::logWarning(
+      //    "cm.logjoin",
+      //    "item not found in featureindex: $0",
+      //    docid.docid);
+    } else {
+      iv_obj.addChild(
+          item_visit_schema->fieldId("shop_id"),
+          (uint32_t) std::stoull(shopid.get()));
+    }
+
+    auto category1 = get_field_(docid, "category1");
+    if (!category1.isEmpty()) {
+      iv_obj.addChild(
+          item_visit_schema->fieldId("category1"),
+          (uint32_t) std::stoull(category1.get()));
+    }
+
+    auto category2 = get_field_(docid, "category2");
+    if (!category2.isEmpty()) {
+      iv_obj.addChild(
+          item_visit_schema->fieldId("category2"),
+          (uint32_t) std::stoull(category2.get()));
+    }
+
+    auto category3 = get_field_(docid, "category3");
+    if (!category3.isEmpty()) {
+      iv_obj.addChild(
+          item_visit_schema->fieldId("category3"),
+          (uint32_t) std::stoull(category3.get()));
+    }
