f3e2e9946814b2e9a00cea7dc6c83c78f87cc27c
Paul Asmuth <paul@paulasmuth.com>
2015-07-25 04:51:51 +0200

    not part of stdlib

--- src/cortex-flow/cortex-flow.pc.cmake
+++
@@ -1,9 +0,0 @@
-# cortex-flow.pc.cmake
-
-Name: cortex-flow
-Description: Flow Configuration Language Framework
-Version: @CORTEX_FLOW_VERSION@
-Requires: cortex-base >= 0.10.0
-# Conflicts: 
-Libs: -L@CMAKE_INSTALL_PREFIX@/lib -lcortex-flow
-Cflags: -I@CMAKE_INSTALL_PREFIX@/include
