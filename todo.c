a022256ccad496f09f5c31ad4a6f245c3e3e1701
Paul Asmuth <paul@paulasmuth.com>
2015-07-25 18:10:59 +0200

    join cart items

--- todo.c
+++ todo.c
@@ -53,6 +53,40 @@
 //            msg::TRUE);
 //      }
 //      /* EOF DAWANDA HACK */
+    // FIXPAUL use getFields...
+    auto docid = ci.item.docID();
+    auto shopid = get_field_(docid, "shop_id");
+    if (shopid.isEmpty()) {
+      //stx::logWarning(
+      //    "cm.logjoin",
+      //    "item not found in featureindex: $0",
+      //    docid.docid);
+    } else {
+      ci_obj.addChild(
+          cart_item_schema->fieldId("shop_id"),
+          (uint32_t) std::stoull(shopid.get()));
+    }
+
+    auto category1 = get_field_(docid, "category1");
+    if (!category1.isEmpty()) {
+      ci_obj.addChild(
+          cart_item_schema->fieldId("category1"),
+          (uint32_t) std::stoull(category1.get()));
+    }
+
+    auto category2 = get_field_(docid, "category2");
+    if (!category2.isEmpty()) {
+      ci_obj.addChild(
+          cart_item_schema->fieldId("category2"),
+          (uint32_t) std::stoull(category2.get()));
+    }
+
+    auto category3 = get_field_(docid, "category3");
+    if (!category3.isEmpty()) {
+      ci_obj.addChild(
+          cart_item_schema->fieldId("category3"),
+          (uint32_t) std::stoull(category3.get()));
+    }
 
 
     auto docid = iv.item.docID();
