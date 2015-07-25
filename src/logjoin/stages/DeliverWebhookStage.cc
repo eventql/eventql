f1498e3c5ca73b9717d3a22e4f19ed114726fb22
Paul Asmuth <paul@paulasmuth.com>
2015-07-25 19:13:16 +0200

    DeliverWebhookStage stub

---
+++ src/logjoin/stages/DeliverWebhookStage.cc
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2015 - The CM Authors <legal@clickmatcher.com>
+ *   All Rights Reserved.
+ *
+ * This file is CONFIDENTIAL -- Distribution or duplication of this material or
+ * the information contained herein is strictly forbidden unless prior written
+ * permission is obtained.
+ */
+#include "logjoin/stages/DeliverWebhookStage.h"
+#include "logjoin/common.h"
+
+using namespace stx;
+
+namespace cm {
+
+void DeliverWebhookStage::process(RefPtr<TrackedSessionContext> ctx) {
+}
+
+} // namespace cm
+
