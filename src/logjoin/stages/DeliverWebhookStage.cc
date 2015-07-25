779c4f44f27edeeb0cfae940f2b151da45cef628
Paul Asmuth <paul@paulasmuth.com>
2015-07-25 19:57:57 +0200

    DeliverWebhookStage::proces stub

--- src/logjoin/stages/DeliverWebhookStage.cc
+++ src/logjoin/stages/DeliverWebhookStage.cc
@@ -6,6 +6,8 @@
  * the information contained herein is strictly forbidden unless prior written
  * permission is obtained.
  */
+#include "stx/logging.h"
+#include "stx/http/httprequest.h"
 #include "logjoin/stages/DeliverWebhookStage.h"
 #include "logjoin/common.h"
 
@@ -14,6 +16,32 @@ using namespace stx;
 namespace cm {
 
 void DeliverWebhookStage::process(RefPtr<TrackedSessionContext> ctx) {
+  const auto& logjoin_config = ctx->customer_config->config.logjoin_config();
+
+  for (const auto hook : logjoin_config.webhooks()) {
+    stx::logDebug("logjoind", "Delivering webhook: $0", hook.target_url());
+
+    Buffer json_buf;
+    json::JSONOutputStream json(BufferOutputStream::fromBuffer(&json_buf));
+
+    json.beginObject();
+    json.addObjectEntry("session");
+    //msg::JSONEncoder::encode(session, &json);
+    json.endObject();
+
+    stx::iputs("session json: $0", json_buf.toString());
+
+    // FIXPAUL: security risk ahead, make sure url is actually external
+    http::HTTPMessage::HeaderList headers;
+    headers.emplace_back(
+        "X-DeepAnalytics-SessionID",
+        ctx->joined_session.session_id());
+
+    auto request = http::HTTPRequest::mkPost(
+        hook.target_url(),
+        json_buf,
+        headers);
+  }
 }
 
 } // namespace cm
