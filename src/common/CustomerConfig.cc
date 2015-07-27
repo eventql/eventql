7aef9bfbc9ebb35666c98d409bf126ea0c4959da
Paul Asmuth <paul@paulasmuth.com>
2015-07-27 19:10:52 +0200

    createCustomerConfig

---
+++ src/common/CustomerConfig.cc
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2015 - The CM Authors <legal@clickmatcher.com>
+ *   All Rights Reserved.
+ *
+ * This file is CONFIDENTIAL -- Distribution or duplication of this material or
+ * the information contained herein is strictly forbidden unless prior written
+ * permission is obtained.
+ */
+#include "stx/protobuf/MessageSchema.h"
+#include "common/CustomerConfig.h"
+#include "common/JoinedSession.pb.h"
+
+using namespace stx;
+
+namespace cm {
+
+CustomerConfig createCustomerConfig(const String& customer) {
+  CustomerConfig dwn;
+
+  dwn.set_customer(customer);
+
+  auto hook = dwn.mutable_logjoin_config()->add_webhooks();
+  hook->set_target_url("http://localhost:8080/mywebhook");
+
+  {
+    auto ev = dwn.mutable_logjoin_config()->add_session_event_schemas();
+    ev->set_evtype("search_query");
+    ev->set_evid(16);
+    ev->set_schema(msg::MessageSchema::fromProtobuf(cm::JoinedSearchQuery::descriptor())->encode().toString());
+  }
+
+  {
+    auto ev = dwn.mutable_logjoin_config()->add_session_event_schemas();
+    ev->set_evtype("page_view");
+    ev->set_evid(25);
+    ev->set_schema(msg::MessageSchema::fromProtobuf(cm::JoinedPageView::descriptor())->encode().toString());
+  }
+
+  {
+    auto ev = dwn.mutable_logjoin_config()->add_session_event_schemas();
+    ev->set_evtype("cart_items");
+    ev->set_evid(52);
+    ev->set_schema(msg::MessageSchema::fromProtobuf(cm::JoinedCartItem::descriptor())->encode().toString());
+  }
+
+  dwn.mutable_logjoin_config()->set_session_attributes_schema(
+      msg::MessageSchema::fromProtobuf(
+          cm::DefaultSessionAttributes::descriptor())->encode().toString());
+
+  dwn.mutable_logjoin_config()->set_session_schema_next_field_id(70);
+}
+
+} // namespace cm
+
