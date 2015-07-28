c8cd27507b5a64de4a6067746e6889ab9dcc7986
Paul Asmuth <paul@paulasmuth.com>
2015-07-28 18:45:19 +0200

    open csb in CustomerDirectory

--- src/common/CustomerDirectory.cc
+++ src/common/CustomerDirectory.cc
@@ -7,11 +7,22 @@
  * permission is obtained.
  */
 #include <stx/exception.h>
+#include <stx/protobuf/msg.h>
 #include <common/CustomerDirectory.h>
 
 using namespace stx;
 
 namespace cm {
+
+
+CustomerDirectory::CustomerDirectory(const String& path) {
+  mdb::MDBOptions mdb_opts;
+  mdb_opts.data_filename = "cdb.db",
+  mdb_opts.lock_filename = "cdb.db.lck";
+  mdb_opts.duplicate_keys = false;
+
+  db_ = mdb::MDB::open(path, mdb_opts);
+}
 
 RefPtr<CustomerConfigRef> CustomerDirectory::configFor(
     const String& customer_key) {
@@ -28,6 +39,92 @@ RefPtr<CustomerConfigRef> CustomerDirectory::configFor(
 void CustomerDirectory::updateCustomerConfig(CustomerConfig config) {
   std::unique_lock<std::mutex> lk(mutex_);
   customers_.emplace(config.customer(), new CustomerConfigRef(config));
+}
+
+void CustomerDirectory::addTableDefinition(const TableDefinition& table) const {
+  if (!StringUtil::isShellSafe(table.table_name())) {
+    RAISEF(
+        kIllegalArgumentError,
+        "invalid table name: '$0'",
+        table.table_name());
+  }
+
+  if (!table.has_schema_name() && !table.has_schema_inline()) {
+    RAISEF(
+        kIllegalArgumentError,
+        "can't add table without a schema: '$0'",
+        table.table_name());
+  }
+
+  auto db_key = StringUtil::format(
+      "tbl~$0~$1",
+      table.customer(),
+      table.table_name());
+
+  auto buf = msg::encode(table);
+
+  auto txn = db_->startTransaction(false);
+  txn->autoAbort();
+
+  txn->insert(db_key.data(), db_key.size(), buf->data(), buf->size());
+  txn->commit();
+}
+
+void CustomerDirectory::updateTableDefinition(const TableDefinition& table) const {
+  if (!StringUtil::isShellSafe(table.table_name())) {
+    RAISEF(
+        kIllegalArgumentError,
+        "invalid table name: '$0'",
+        table.table_name());
+  }
+
+  if (!table.has_schema_name() && !table.has_schema_inline()) {
+    RAISEF(
+        kIllegalArgumentError,
+        "can't add table without a schema: '$0'",
+        table.table_name());
+  }
+
+  auto db_key = StringUtil::format(
+      "tbl~$0~$1",
+      table.customer(),
+      table.table_name());
+
+  auto buf = msg::encode(table);
+
+  auto txn = db_->startTransaction(false);
+  txn->autoAbort();
+
+  txn->update(db_key.data(), db_key.size(), buf->data(), buf->size());
+  txn->commit();
+}
+
+void CustomerDirectory::listTableDefinitions(
+    Function<void (const TableDefinition& table)> fn) const {
+  auto prefix = "tbl~";
+
+  Buffer key;
+  Buffer value;
+
+  auto txn = db_->startTransaction(true);
+  txn->autoAbort();
+
+  auto cursor = txn->getCursor();
+  key.append(prefix);
+
+  if (!cursor->getFirstOrGreater(&key, &value)) {
+    return;
+  }
+
+  do {
+    if (!StringUtil::beginsWith(key.toString(), prefix)) {
+      break;
+    }
+
+    fn(msg::decode<TableDefinition>(value));
+  } while (cursor->getNext(&key, &value));
+
+  cursor->close();
 }
 
 } // namespace cm
