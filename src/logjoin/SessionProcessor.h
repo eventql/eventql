b989472202074cc56850ae68104161e6fb6a0c87
Paul Asmuth <paul@paulasmuth.com>
2015-07-29 16:34:18 +0200

    session processor queue logic

--- src/logjoin/SessionProcessor.h
+++ src/logjoin/SessionProcessor.h
@@ -10,6 +10,7 @@
 #include "stx/stdtypes.h"
 #include "stx/random.h"
 #include "stx/thread/FixedSizeThreadPool.h"
+#include "stx/thread/DelayedQueue.h"
 #include "logjoin/TrackedSession.h"
 #include "logjoin/SessionContext.h"
 #include "common/CustomerDirectory.h"
@@ -33,11 +34,16 @@ public:
 
 protected:
 
+  void work();
+
+  void processSession(const SHA1Hash& skey);
   void processSession(const TrackedSession& session);
 
   Vector<PipelineStageFn> stages_;
   CustomerDirectory* customer_dir_;
-  thread::FixedSizeThreadPool tpool_;
+  thread::DelayedQueue<SHA1Hash> queue_;
+  Vector<std::thread> threads_;
+  bool running_;
 };
 
 } // namespace cm
