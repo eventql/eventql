9a1559394978889ba1bec7ab07e8e9e32f30e4e1
Paul Asmuth <paul@paulasmuth.com>
2015-07-31 19:09:56 +0200

    CSTableIndex...

--- src/tsdb/CSTableIndex.cc
+++ src/tsdb/CSTableIndex.cc
@@ -32,7 +32,7 @@ Option<RefPtr<VFSFile>> CSTableIndex::fetchCSTable(
       StringUtil::format(
           "$0/$1/$2/_cstable",
           tsdb_namespace,
-          table,
+          SHA1::compute(table).toString(),
           partition.toString()));
 
   if (FileUtil::exists(filepath)) {
@@ -44,8 +44,9 @@ Option<RefPtr<VFSFile>> CSTableIndex::fetchCSTable(
 }
 
 void CSTableIndex::buildCSTable(RefPtr<Partition> partition) {
-  auto table = partition->getTable();
   auto snap = partition->getSnapshot();
+  auto table = partition->getTable();
+  auto schema = table->schema();
 
   logDebug(
       "tsdb",
@@ -57,41 +58,15 @@ void CSTableIndex::buildCSTable(RefPtr<Partition> partition) {
   auto filepath = FileUtil::joinPaths(snap->base_path, "_cstable");
   auto tmppath = filepath + "." + Random::singleton()->hex128();
 
-  auto schema = table->schema();
   cstable::CSTableBuilder cstable(schema.get());
 
   auto reader = partition->getReader();
-
-  reader->fetchRecords([] (const Buffer& record) {
-//
-//      msg::MessageObject obj;
-//      msg::MessageDecoder::decode(data, data_size, *schema, &obj);
-//      cstable.addRecord(obj);
-//
+  reader->fetchRecords([&schema, &cstable] (const Buffer& record) {
+      msg::MessageObject obj;
+      msg::MessageDecoder::decode(record.data(), record.size(), *schema, &obj);
+      cstable.addRecord(obj);
   });
-//
-//  for (const auto& f : input_files) {
-//    auto fpath = FileUtil::joinPaths(node_->db_path, f);
-//    sstable::SSTableReader reader(fpath);
-//    auto cursor = reader.getCursor();
-//
-//    while (cursor->valid()) {
-//      uint64_t* key;
-//      size_t key_size;
-//      cursor->getKey((void**) &key, &key_size);
-//      if (key_size != SHA1Hash::kSize) {
-//        RAISE(kRuntimeError, "invalid row");
-//      }
-//
-//      void* data;
-//      size_t data_size;
-//      cursor->getData(&data, &data_size);
-//      if (!cursor->next()) {
-//        break;
-//      }
-//    }
-//  }
-//
+
   cstable.write(tmppath);
   FileUtil::mv(tmppath, filepath);
 }
