c7f9cfa96838aaf1bacb6d73e24eaa25069ec2bb
Paul Asmuth <paul@paulasmuth.com>
2015-08-04 18:55:03 +0200

    CustomerDirectoryMaster::updateCustomerConfig impl

--- src/master/CustomerDirectoryMaster.cc
+++ src/master/CustomerDirectoryMaster.cc
@@ -7,6 +7,11 @@
  * permission is obtained.
  */
 #include <master/CustomerDirectoryMaster.h>
+#include <stx/io/file.h>
+#include <stx/io/fileutil.h>
+#include <stx/random.h>
+#include <stx/protobuf/msg.h>
+#include <stx/logging.h>
 
 using namespace stx;
 
@@ -17,7 +22,54 @@ CustomerDirectoryMaster::CustomerDirectoryMaster(
     db_path_(path) {}
 
 void CustomerDirectoryMaster::updateCustomerConfig(CustomerConfig config) {
-  RAISE(kNotYetImplementedError);
+  std::unique_lock<std::mutex> lk(mutex_);
+  uint64_t head_version = 0;
+
+  auto cpath = FileUtil::joinPaths(db_path_, config.customer());
+  auto hpath = FileUtil::joinPaths(cpath, "config.HEAD");
+  if (FileUtil::exists(cpath)) {
+    if (FileUtil::exists(hpath)) {
+      auto head_version_str = FileUtil::read(hpath);
+      head_version = std::stoull(head_version_str.toString());
+    }
+  } else {
+    FileUtil::mkdir(cpath);
+  }
+
+  if (config.version() != head_version) {
+    RAISE(
+        kRuntimeError,
+        "VERSION MISMATCH: can't update customer config because the update is" \
+        " out of date (i.e. it is not based on the latest head version)");
+  }
+
+  auto config_buf = msg::encode(config);
+  ++head_version;
+
+  logInfo(
+      "dxa-master",
+      "Updating customer config; customer=$0 head=$1",
+      config.customer(),
+      head_version);
+
+  auto vpath = FileUtil::joinPaths(
+      cpath,
+      StringUtil::format("config.$0", head_version));
+
+  auto vtmppath = vpath + "~tmp." + Random::singleton()->hex64();
+  {
+    auto tmpfile = File::openFile(vtmppath, File::O_CREATE | File::O_WRITE);
+    tmpfile.write(config_buf->data(), config_buf->size());
+  }
+
+  auto htmppath = hpath + "~tmp." + Random::singleton()->hex64();
+  {
+    auto tmpfile = File::openFile(htmppath, File::O_CREATE | File::O_WRITE);
+    tmpfile.write(StringUtil::toString(head_version));
+  }
+
+  FileUtil::mv(vtmppath, vpath);
+  FileUtil::mv(htmppath, hpath);
 }
 
 } // namespace cm
