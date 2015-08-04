666ceda619f5a1d3f68ccaf1dbe3b0a54c9b071d
Paul Asmuth <paul@paulasmuth.com>
2015-08-04 18:34:59 +0200

    CustomerDirectoryMaster stub

---
+++ src/master/CustomerDirectoryMaster.h
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2015 - The CM Authors <legal@clickmatcher.com>
+ *   All Rights Reserved.
+ *
+ * This file is CONFIDENTIAL -- Distribution or duplication of this material or
+ * the information contained herein is strictly forbidden unless prior written
+ * permission is obtained.
+ */
+#pragma once
+#include <stx/stdtypes.h>
+#include <stx/SHA1.h>
+#include <common/CustomerConfig.h>
+#include <common/TableDefinition.h>
+
+using namespace stx;
+
+namespace cm {
+
+class CustomerDirectoryMaster {
+public:
+
+  CustomerDirectoryMaster(const String& path);
+
+  void updateCustomerConfig(CustomerConfig config);
+
+protected:
+  mutable std::mutex mutex_;
+  String db_path_;
+};
+
+} // namespace cm
