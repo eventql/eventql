11f27d5900c563602a0ca34248ce51b69d29d265
Paul Asmuth <paul@paulasmuth.com>
2015-08-05 20:20:55 +0200

    FixedShardPartitioner

---
+++ src/tsdb/FixedShardPartitioner.cc
@@ -0,0 +1,29 @@
+/**
+ * This file is part of the "libfnord" project
+ *   Copyright (c) 2015 Paul Asmuth
+ *
+ * FnordMetric is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License v3.0. You should have received a
+ * copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+#include <stx/util/binarymessagewriter.h>
+#include <tsdb/FixedShardPartitioner.h>
+
+using namespace stx;
+
+namespace tsdb {
+
+SHA1Hash FixedShardPartitioner::partitionKeyFor(
+    const String& stream_key,
+    size_t shard) {
+  util::BinaryMessageWriter buf(stream_key.size() + 32);
+
+  buf.append(stream_key.data(), stream_key.size());
+  buf.appendUInt8(27);
+  buf.appendVarUInt(shard);
+
+  return SHA1::compute(buf.data(), buf.size());
+}
+
+}
---
+++ src/tsdb/FixedShardPartitioner.h
@@ -0,0 +1,30 @@
+/**
+ * This file is part of the "libfnord" project
+ *   Copyright (c) 2015 Paul Asmuth
+ *
+ * FnordMetric is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License v3.0. You should have received a
+ * copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+#include <stx/stdtypes.h>
+#include <stx/option.h>
+#include <stx/UnixTime.h>
+#include <stx/duration.h>
+#include <stx/SHA1.h>
+
+using namespace stx;
+
+namespace tsdb {
+
+class FixedShardPartitioner : public RefCounted {
+public:
+
+  static SHA1Hash partitionKeyFor(
+      const String& stream_key,
+      size_t nshard);
+
+};
+
+}
