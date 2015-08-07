b3c0fc3593f91e3d50b25ddbd06947b3a82e82a8
Laura Schlimmer <laura@etat-gray.com>
2015-08-07 18:46:23 +0200

    test BooleanColumnWriter

--- src/cstable/cstable_test.cc
+++ src/cstable/cstable_test.cc
@@ -14,6 +14,7 @@
 #include <cstable/CSTableWriter.h>
 #include <cstable/CSTableReader.h>
 #include <cstable/BitPackedIntColumnWriter.h>
+#include <cstable/BooleanColumnWriter.h>
 
 using namespace stx;
 
@@ -39,14 +40,11 @@ TEST_CASE(CSTableTest, TestCSTableContainer, [] () {
   EXPECT_EQ(tbl_reader.numRecords(), num_records);
   EXPECT_EQ(tbl_reader.hasColumn("key1"), true);
   EXPECT_EQ(tbl_reader.hasColumn("key2"), true);
-
-
-  RefPtr<cstable::ColumnReader> column_reader = tbl_reader.getColumnReader("key1");
 });
 
-TEST_CASE(CSTableTest, TestCSTable, [] () {
+TEST_CASE(CSTableTest, TestCSTableBitPacked, [] () {
   const String& filename = "/tmp/__fnord__cstabletest2.cstable";
-  const uint64_t num_records = 10;
+  const uint64_t num_records = 1000;
 
   FileUtil::rm(filename);
 
@@ -57,9 +55,59 @@ TEST_CASE(CSTableTest, TestCSTable, [] () {
     num_records);
 
   tbl_writer.addColumn("key1", column_writer.get());
+  tbl_writer.commit();
 
-  for (uint32_t i = 0; i < 1000; i++) {
+  /*for (uint32_t i = 0; i < 10; i++) {
     column_writer->addDatum(1, 1, i);
+  }*/
+
+  column_writer->addDatum(1, 1, 10);
+
+  cstable::CSTableReader tbl_reader(filename);
+  RefPtr<cstable::ColumnReader> column_reader = tbl_reader.getColumnReader("key1");
+
+  /*uint64_t rep_level;
+  uint64_t def_level;
+  void* data;
+  size_t size;*/
+
+  //column_reader->next(&rep_level, &def_level, &data, &size);
+});
+
+TEST_CASE(CSTableTest, TestBooleanColumnWriter, [] () {
+  const String& filename = "/tmp/__fnord__cstabletest3.cstable";
+  const uint64_t num_records = 1000;
+
+  FileUtil::rm(filename);
+
+  RefPtr<cstable::BooleanColumnWriter> column_writer = mkRef(
+    new cstable::BooleanColumnWriter(1, 1));
+
+  uint8_t v = 1;
+  for (uint32_t i = 0; i < 10; i++) {
+    column_writer->addDatum(1, 1, &v, sizeof(v));
+  }
+
+
+  auto tbl_writer = cstable::CSTableWriter(
+    filename,
+    num_records);
+  tbl_writer.addColumn("key1", column_writer.get());
+  tbl_writer.commit();
+
+
+  cstable::CSTableReader tbl_reader(filename);
+  RefPtr<cstable::ColumnReader> column_reader = tbl_reader.getColumnReader("key1");
+
+  EXPECT_EQ(column_reader->type() == msg::FieldType::BOOLEAN, true);
+
+  uint64_t rep_level;
+  uint64_t def_level;
+  void* data;
+  size_t size;
+
+  for (uint32_t i = 0; i < 10; i++) {
+    EXPECT_EQ(column_reader->next(&rep_level, &def_level, &data, &size), true);
   }
 });
 
