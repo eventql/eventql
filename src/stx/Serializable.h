b0b0babc3daf993ea07cc57fef719408bef81f1d
Paul Asmuth <paul@paulasmuth.com>
2015-08-07 15:43:45 +0200

    Serializable stub

---
+++ src/stx/Serializable.h
@@ -0,0 +1,23 @@
+/**
+ * This file is part of the "libstx" project
+ *   Copyright (c) 2015 Paul Asmuth, FnordCorp B.V.
+ *
+ * FnordMetric is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License v3.0. You should have received a
+ * copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+#include "stx/stdtypes.h"
+
+namespace stx {
+
+class Serializable {
+public:
+
+  virtual ~Serializable() {}
+
+};
+
+} // namespace stx
+
