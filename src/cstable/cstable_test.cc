f876f6b658bad2b4cf45d506ed3ad895aa693357
Laura Schlimmer <laura@etat-gray.com>
2015-08-08 14:04:38 +0200

    test StringColumnWriter / Reader

--- src/cstable/cstable_test.cc
+++ src/cstable/cstable_test.cc
@@ -17,6 +17,7 @@
 #include <cstable/BooleanColumnWriter.h>
 #include <cstable/DoubleColumnWriter.h>
 #include <cstable/LEB128ColumnWriter.h>
+#include <cstable/StringColumnWriter.h>
 
 
 using namespace stx;
@@ -205,6 +206,48 @@ TEST_CASE(CSTableTest, TestLEB128ColumnWriterReader, [] () {
   for (uint32_t i = 0; i < 10; i++) {
     EXPECT_EQ(column_reader->next(&rep_level, &def_level, &data, &size), true);
     val = static_cast<uint64_t*>(data);
+    EXPECT_EQ(size, sizeof(v));
+    EXPECT_EQ(*val, v);
+  }
+});
+
+
+TEST_CASE(CSTableTest, TestStringColumnWriterReader, [] () {
+  const String& filename = "/tmp/__fnord__cstabletest6.cstable";
+  const uint64_t num_records = 100;
+
+  FileUtil::rm(filename);
+
+  RefPtr<cstable::StringColumnWriter> column_writer = mkRef(
+    new cstable::StringColumnWriter(1, 1));
+
+  const String& v = "value";
+  for (uint32_t i = 0; i < 10; i++) {
+    column_writer->addDatum(1, 1, &v, sizeof(v));
+  }
+
+
+  auto tbl_writer = cstable::CSTableWriter(
+    filename,
+    num_records);
+  tbl_writer.addColumn("key1", column_writer.get());
+  tbl_writer.commit();
+
+
+  cstable::CSTableReader tbl_reader(filename);
+  RefPtr<cstable::ColumnReader> column_reader = tbl_reader.getColumnReader("key1");
+
+  EXPECT_EQ(column_reader->type() == msg::FieldType::STRING, true);
+
+  uint64_t rep_level;
+  uint64_t def_level;
+  void* data;
+  String* val;
+  size_t size;
+
+  for (uint32_t i = 0; i < 10; i++) {
+    EXPECT_EQ(column_reader->next(&rep_level, &def_level, &data, &size), true);
+    val = static_cast<String*>(data);
     EXPECT_EQ(size, sizeof(v));
     EXPECT_EQ(*val, v);
   }
