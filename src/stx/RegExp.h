32bc9642fa744f7f4a733ae8d6b841064934388a
Paul Asmuth <paul@paulasmuth.com>
2015-08-08 17:24:19 +0200

    merge RegExp into libstx

--- src/stx/RegExp.h
+++ src/stx/RegExp.h
@@ -1,33 +1,24 @@
-// This file is part of the "libcortex" project
+// This file is part of the "libstx" project
 //   (c) 2009-2015 Christian Parpart <https://github.com/christianparpart>
 //   (c) 2014-2015 Paul Asmuth <https://github.com/paulasmuth>
 //
-// libcortex is free software: you can redistribute it and/or modify it under
-// the terms of the GNU Affero General Public License v3.0.
+// libstx is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License v3.0.
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
-
-#ifndef sw_flow_RegExp_h
-#define sw_flow_RegExp_h
-
-#include <cortex-base/Api.h>
+#pragma once
+#include <stx/stdtypes.h>
+#include <stx/buffer.h>
 #include <pcre.h>
 #include <string>
 #include <vector>
 
-namespace cortex {
+namespace stx {
 
-class BufferRef;
-
-class CORTEX_API RegExp {
- private:
-  std::string pattern_;
-  pcre* re_;
-
- public:
+class RegExp {
+public:
   typedef std::vector<std::pair<const char*, size_t>> Result;
 
- public:
   explicit RegExp(const std::string& pattern);
   RegExp();
   RegExp(const RegExp& v);
@@ -36,8 +27,9 @@ class CORTEX_API RegExp {
   RegExp(RegExp&& v);
   RegExp& operator=(RegExp&& v);
 
+  bool match(const Buffer& subject, Result* result = nullptr) const;
+  bool match(const String& subject, Result* result = nullptr) const;
   bool match(const char* buffer, size_t size, Result* result = nullptr) const;
-  bool match(const BufferRef& buffer, Result* result = nullptr) const;
   bool match(const char* cstring, Result* result = nullptr) const;
 
   const std::string& pattern() const { return pattern_; }
@@ -48,45 +40,38 @@ class CORTEX_API RegExp {
   friend bool operator==(const RegExp& a, const RegExp& b) {
     return a.pattern() == b.pattern();
   }
+
   friend bool operator!=(const RegExp& a, const RegExp& b) {
     return a.pattern() != b.pattern();
   }
+
   friend bool operator<=(const RegExp& a, const RegExp& b) {
     return a.pattern() <= b.pattern();
   }
+
   friend bool operator>=(const RegExp& a, const RegExp& b) {
     return a.pattern() >= b.pattern();
   }
+
   friend bool operator<(const RegExp& a, const RegExp& b) {
     return a.pattern() < b.pattern();
   }
+
   friend bool operator>(const RegExp& a, const RegExp& b) {
     return a.pattern() > b.pattern();
   }
+
+private:
+  std::string pattern_;
+  pcre* re_;
 };
 
-class CORTEX_API RegExpContext {
- public:
-  RegExpContext();
-  virtual ~RegExpContext();
-
-  RegExp::Result* regexMatch() {
-    if (!regexMatch_) regexMatch_ = new RegExp::Result();
-
-    return regexMatch_;
-  }
-
- private:
-  RegExp::Result* regexMatch_;
-};
-
-}  // namespace cortex
+}  // namespace stx
 
 namespace std {
-inline std::ostream& operator<<(std::ostream& os, const cortex::RegExp& re) {
+inline std::ostream& operator<<(std::ostream& os, const stx::RegExp& re) {
   os << re.pattern();
   return os;
 }
 }
 
-#endif
