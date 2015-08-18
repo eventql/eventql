9c1f04930fe3d79a8be6fb3ce0554355132a0cba
Paul Asmuth <paul@paulasmuth.com>
2015-08-18 23:51:52 +0200

    move all compile deps into src/stx

---
+++ src/stx/3rdparty/simdcomp/simdcomp.h
@@ -0,0 +1,20 @@
+/**
+ * This code is released under a BSD License.
+ */
+
+#ifndef SIMDCOMP_H_
+#define SIMDCOMP_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "simdbitpacking.h"
+#include "simdcomputil.h"
+#include "simdintegratedbitpacking.h"
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif 
