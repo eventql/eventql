ba43177b8ac92d898386792fb786fac695211bf9
Paul Asmuth <paul@paulasmuth.com>
2015-08-21 22:31:19 +0200

    logout...

--- src/zbase/webui/modules/login/logout.js
+++ src/zbase/webui/modules/login/logout.js
@@ -2,7 +2,15 @@ ZBase.registerView((function() {
 
   var render = function(path) {
     ZBase.util.httpPost("/analytics/api/v1/auth/logout", "", function() {
-      ZBase.navigateTo("/a/");
+      ZBase.util.httpGet("/a/_/c", function(http) {
+        if (http.status != 200) {
+          ZBase.fatalError("logout failed");
+          return;
+        }
+
+        ZBase.updateConfig(JSON.parse(http.responseText));
+        ZBase.navigateTo("/a/");
+      });
     });
   };
 
