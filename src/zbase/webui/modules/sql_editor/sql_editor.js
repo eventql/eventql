53e26bbc5c92fbb3f74df81da80414f310fd085f
Laura Schlimmer <laura@etat-gray.com>
2015-08-21 22:09:18 +0200

    setQueryContent / observeQueryExecution

--- src/zbase/webui/modules/sql_editor/sql_editor.js
+++ src/zbase/webui/modules/sql_editor/sql_editor.js
@@ -14,31 +14,99 @@ ZBase.registerView((function() {
 
     Editor.validateAndFetchDocument();
     Editor.doc_sync = new DocSync(function() {
-      return "content=" + encodeURIComponent(document.querySelector("[data-content='query_content']").getValue()) +
-      "&name=" + encodeURIComponent(document.querySelector("[data-content='query_name']").getValue())
+      return "content=" + encodeURIComponent(document.querySelector(".zbase_sql_editor_pane fn-codeeditor").getValue()) +
+      "&name=" + encodeURIComponent(document.querySelector(".zbase_sql_editor_pane input[name='doc_name']").value)
       },
-      "/analytics/api/v1/sql_queries/" + Editor.doc_id
+      "/analytics/api/v1/sql_queries/" + Editor.doc_id,
+      document.querySelector(".zbase_sql_editor_pane .zbase_sql_editor_infobar")
     );
-
-    document.querySelector(".zbase_sql_editor_pane .zbase_loader")
-      .classList.add("hidden");
   };
 
   Editor.validateAndFetchDocument = function() {
     Editor.doc_id = path_parts[3];
+    // invalid docid
     if (!/^[A-Za-z0-9]+$/.test(Editor.doc_id)) {
       //TODO redirect and display message
       alert("invalid docid");
       return;
     }
 
+    // fetch document
     ZBase.util.httpGet("/analytics/api/v1/documents/sql_queries/" + Editor.doc_id, function(r) {
       if (r.status == 200) {
-        Editor.doc = JSON.parse(r.response);
-        
-        console.log(Editor.doc);
+        var doc = JSON.parse(r.response);
+        Editor.handleNameEditing(doc.name);
+        Editor.setQueryContent(doc.sql_query);
+        Editor.observeQueryExecution();
+
+        document.querySelector(".zbase_sql_editor_pane .zbase_loader")
+          .classList.add("hidden");
       }
     });
+  };
+
+  Editor.setQueryContent = function(query) {
+    console.log(query);
+    document.querySelector(".zbase_sql_editor_pane fn-codeeditor").setValue(query);
+    if (query != "-- SELECT ... FROM ...;" && query.length > 0) {
+      Editor.executeQuery(query);
+    }
+  };
+
+  Editor.observeQueryExecution = function() {
+    var editor = document.querySelector(".zbase_sql_editor_pane fn-codeeditor");
+    var exec_btn = document.querySelector(
+      ".zbase_sql_editor_pane button[data-action='execute-query']");
+
+    /*document.querySelector("fn-tooltip[data-target='query_exec_btn']").init(
+      exec_btn);*/
+
+    exec_btn.addEventListener('click', function() {
+      Editor.executeQuery(editor.getValue());
+    }, false);
+
+    var cmd_pressed = false;
+    editor.addEventListener('keydown', function(e) {
+      if (e.keyCode == 17 || e.keyCode == 91) {
+        cmd_pressed = true;
+      }
+    }, false);
+
+    editor.addEventListener('keyup', function(e) {
+      if (e.keyCode == 17 || e.keyCode == 91) {
+        cmd_pressed = false;
+      }
+    }, false);
+
+    editor.addEventListener('keydown', function(e) {
+      if (e.keyCode == 13 && cmd_pressed) {
+        e.preventDefault();
+        Editor.executeQuery(editor.getValue());
+      }
+    }, false);
+
+    var value = editor.getValue();
+    editor.addEventListener('keyup', function() {
+      var new_value = editor.getValue();
+      if (value == new_value) {return;}
+      value = new_value;
+      Editor.doc_sync.cur_version++;
+      Editor.doc_sync.documentChanged("content_changed");
+    }, false);
+
+  };
+
+  Editor.executeQuery = function(query) {
+    console.log("execute", query);
+  };
+
+  Editor.handleNameEditing = function(doc_name) {
+    var name_elem = document.querySelector(".zbase_sql_editor_pane .pagetitle");
+    name_elem.innerHTML = doc_name;
+
+    name_elem.addEventListener("click", function() {
+      console.log("open update name modal");
+    }, false);
   };
 
   Overview.render = function() {
