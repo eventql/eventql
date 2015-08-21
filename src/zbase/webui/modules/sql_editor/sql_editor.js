7865f549bb94041a434a38550fde1a94b43e4bd6
Laura Schlimmer <laura@etat-gray.com>
2015-08-21 16:06:17 +0200

    moved sql_overview to sql_editor

--- src/zbase/webui/modules/sql_editor/sql_editor.js
+++ src/zbase/webui/modules/sql_editor/sql_editor.js
@@ -2,7 +2,7 @@ ZBase.registerView((function() {
 
   var render = function(path) {
     var viewport = document.getElementById("zbase_viewport");
-    var page = ZBase.getTemplate("sql_editor", "zbase_sql_editor_main_tpl");
+    var page = ZBase.getTemplate("sql_editor", "zbase_sql_editor_overview_main_tpl");
     console.log(page);
     ZBase.util.install_link_handlers(page);
 
@@ -15,13 +15,10 @@ ZBase.registerView((function() {
 
     loadView: function(params) {
       render(params.path);
-      console.log("load view sql editor", params);
     },
     unloadView: function() {
-      console.log("unload view sql editor");
     },
     handleNavigationChange: function(url){
-      console.log("handle nav change sql editor");
     }
   };
 
