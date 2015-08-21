02b29917e721eb90114d22c30f6a180bf12825ca
Laura Schlimmer <laura@etat-gray.com>
2015-08-21 16:46:40 +0200

    render sql editor view

--- src/zbase/webui/modules/sql_editor/sql_editor.js
+++ src/zbase/webui/modules/sql_editor/sql_editor.js
@@ -1,18 +1,23 @@
 ZBase.registerView((function() {
 
-  var render = function(path) {
-    var path_parts = path.split("/");
+  var renderOverview = function() {
     var viewport = document.getElementById("zbase_viewport");
-    var page;
-    if (path_parts.length == 4 && path_parts[3].length > 0) {
-      page = ZBase.getTemplate("sql_editor", "zbase_sql_editor_main_tpl");
-    } else {
-      page = ZBase.getTemplate("sql_editor", "zbase_sql_editor_overview_main_tpl");
-    }
+    var page = ZBase.getTemplate(
+      "sql_editor", "zbase_sql_editor_overview_main_tpl");
     ZBase.util.install_link_handlers(page);
 
     viewport.innerHTML = "";
     viewport.appendChild(page);
+  };
+
+  var render = function(path) {
+    var path_parts = path.split("/");
+    // render view
+    if (path_parts.length == 4 && path_parts[3].length > 0) {
+      renderSqlEditor();
+    } else {
+      renderOverview();
+    }
   };
 
   return {
