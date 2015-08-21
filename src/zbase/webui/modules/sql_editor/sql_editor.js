01a01d6b52815bcd586fff962292147fbc7050c8
Laura Schlimmer <laura@etat-gray.com>
2015-08-21 15:00:21 +0200

    sql editor module

--- src/zbase/webui/modules/sql_editor/sql_editor.js
+++ src/zbase/webui/modules/sql_editor/sql_editor.js
@@ -1,9 +1,20 @@
 ZBase.registerView((function() {
 
+  var render = function(path) {
+    var viewport = document.getElementById("zbase_viewport");
+    var page = ZBase.getTemplate("sql_editor", "zbase_sql_editor_main_tpl");
+    console.log(page);
+    ZBase.util.install_link_handlers(page);
+
+    viewport.innerHTML = "";
+    viewport.appendChild(page);
+  };
+
   return {
     name: "sql_editor",
 
     loadView: function(params) {
+      render(params.path);
       console.log("load view sql editor", params);
     },
     unloadView: function() {
