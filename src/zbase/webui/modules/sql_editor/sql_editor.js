3e05b709f74c9e87ad72d2b5b0fbc18ed30c900a
Laura Schlimmer <laura@etat-gray.com>
2015-08-22 18:46:05 +0200

    render table description modal

--- src/zbase/webui/modules/sql_editor/sql_editor.js
+++ src/zbase/webui/modules/sql_editor/sql_editor.js
@@ -14,16 +14,69 @@ ZBase.registerView((function() {
       source_handler.close(source_id);
 
       var data = JSON.parse(e.data);
-      data.results[0].rows.forEach(function(row) {
+      data.results[0].rows.forEach(function(table) {
         var li = document.createElement("li");
-        li.innerHTML = row[0];
+        li.innerHTML = table[0];
         ul.appendChild(li);
         li.addEventListener('click', function() {
-          renderTableDescrPopup(row);
+          renderTableDescriptionModal(source_handler, table[0]);
         }, false);
       });
       pane.querySelector(".sidebar_section .zbase_loader").classList.add("hidden");
       ul.style.display = "block";
+    });
+  };
+
+  function renderTableDescriptionModal(source_handler, table) {
+    var modal = document.querySelector(".zbase_sql_editor_modal.table_descr");
+    var loader = modal.querySelector(".zbase_loader");
+    var source_id = "table_descr";
+    var source = source_handler.get(
+      source_id,
+      "/api/v1/sql_stream?query=" + encodeURIComponent("DESCRIBE '" + table + "';"));
+
+    modal.querySelector(".zbase_modal_header").innerHTML = table;
+    loader.classList.remove("hidden");
+    modal.show();
+
+    source.addEventListener('result', function(e) {
+      source_handler.close(source_id);
+      var result = JSON.parse(e.data).results[0];
+      var header = modal.querySelector("thead tr");
+      var body = modal.querySelector("tbody");
+
+      header.innerHTML = "";
+      result.columns.forEach(function(column) {
+        header.innerHTML += "<th>" + column + "</th";
+      });
+
+      body.innerHTML = "";
+      result.rows.forEach(function(row) {
+        var html = "<tr>"
+        row.forEach(function(cell) {
+          html += "<td>" + cell + "</td>";
+        });
+        html += "</tr>";
+        body.innerHTML += html;
+      });
+
+      loader.classList.add("hidden");
+      modal.querySelector(".error_message").classList.add("hidden");
+      modal.querySelector(".zbase_table").classList.remove("hidden");
+    });
+
+    source.addEventListener("error", function(e) {
+      source_handler.close(source_id);
+      var error_message = modal.querySelector(".error_message");
+      try {
+        error_message.innerHTML = JSON.parse(e.data).error;
+      } catch (err) {
+        error_message.innerHTML = e.data;
+      }
+
+      loader.classList.add("hidden");
+      modal.querySelector(".zbase_table").classList.add("hidden");
+      error_message.classList.remove("hidden");
     });
   };
 
@@ -259,7 +312,8 @@ ZBase.registerView((function() {
 
   Editor.handleNameEditing = function(doc_name) {
     var name_elem = document.querySelector(".zbase_sql_editor_pane .pagetitle");
-    var modal = document.querySelector(".zbase_sql_editor_pane fn-modal");
+    var modal = document.querySelector(
+      ".zbase_sql_editor_pane fn-modal.rename_query");
     var input = modal.querySelector("input");
     var submit_btn = modal.querySelector("button[data-action='submit']");
     name_elem.innerHTML = doc_name;
