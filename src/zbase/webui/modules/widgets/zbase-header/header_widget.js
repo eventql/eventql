07df246e3fa482e6e0c1977a9ec67ab6decf5959
Paul Asmuth <paul@paulasmuth.com>
2015-08-24 17:49:46 +0200

    separate modules into views/widgets/util...

--- src/zbase/webui/modules/widgets/zbase-header/header_widget.js
+++ src/zbase/webui/modules/widgets/zbase-header/header_widget.js
@@ -2,10 +2,11 @@ var HeaderWidget = (function() {
 
   var render = function() {
     var conf = $.getConfig();
-    var elem = document.querySelector("#zbase_header");
+    var tpl = $.getTemplate("widgets/zbase-header", "zbase_header_tpl");
 
+    var elem = document.querySelector("#zbase_header");
     elem.innerHTML = "";
-    elem.appendChild($.getTemplate("header_widget", "zbase_header_tpl"))
+    elem.appendChild(tpl)
     elem.querySelector(".userid_info").innerHTML = conf.current_user.userid;
     elem.querySelector(".namespace_info").innerHTML = conf.current_user.namespace;
     elem.classList.remove("hidden");
