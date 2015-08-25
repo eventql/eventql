cbf98d69b0c553a802e8e935cc14ec7ef805cf9d
Paul Asmuth <paul@paulasmuth.com>
2015-08-25 22:37:13 +0200

    QueryProgressWidget

--- src/zbase/webui/widgets/zbase-query-progress/query_progress.js
+++ src/zbase/webui/widgets/zbase-query-progress/query_progress.js
@@ -1,9 +1,24 @@
 var QueryProgressWidget = (function() {
 
-  var render = function(elem, progress) {
+  var render = function(elem, data) {
     var tpl = $.getTemplate(
         "widgets/zbase-query-progress",
         "zbase_query_progress_main_tpl");
+
+    var pbar = $("z-progressbar", tpl);
+    if (data && data.status == "running") {
+      pbar.setAttribute("data-progress", (status_data.progress * 100));
+      pbar.setAttribute("data-label", status_data.message);
+    } else {
+      pbar.setAttribute("data-label", "Waiting...");
+    }
+
+    //if (waiting_for_long_time_already) {
+    //  var header = loader.querySelector("h5");
+    //  if (header) {
+    //    header.style.visibility = "visible";
+    //  }
+    //}
 
     elem.innerHTML = "";
     elem.appendChild(tpl)
