f1d9300762316f7b8b57700e8837ba17bc609399
Paul Asmuth <paul@paulasmuth.com>
2015-08-26 00:56:24 +0200

    datastore logfiles page, responsive menu

--- src/zbase/webui/views/datastore_logfiles/datastore_logfiles.js
+++ src/zbase/webui/views/datastore_logfiles/datastore_logfiles.js
@@ -1,6 +1,20 @@
 ZBase.registerView((function() {
 
-  var render = function(url) {
+  var load = function(url) {
+    $.showLoader();
+
+    $.httpGet("/api/v1/logfiles", function(r) {
+      if (r.status == 200) {
+        render(JSON.parse(r.response).logfile_definitions);
+      } else {
+        $.fatalError();
+      }
+
+      $.hideLoader();
+    });
+  }
+
+  var render = function(logfiles) {
     var layout = $.getTemplate("views/datastore", "zbase_datastore_main_tpl");
 
     var menu = DatastoreMenu();
@@ -10,6 +24,19 @@ ZBase.registerView((function() {
         "views/datastore_logfiles",
         "zbase_datastore_logfiles_list_tpl");
 
+    var tbody = $("tbody", page);
+    logfiles.forEach(function(def) {
+      var url = "/a/logviewer/" + def.name;
+      var tr = document.createElement("tr");
+      tr.innerHTML = 
+          "<td><a href='" + url + "'>" + def.name + "</a></td>" +
+          "<td><a href='" + url + "'>&mdash;</a></td>" +
+          "<td><a href='" + url + "'>&mdash;</a></td>";
+
+      $.onClick(tr, function() { $.navigateTo(url); });
+      tbody.appendChild(tr);
+    });
+
     $.replaceContent($(".datastore_viewport", layout), page);
     $.handleLinks(layout);
     $.replaceViewport(layout);
@@ -17,8 +44,8 @@ ZBase.registerView((function() {
 
   return {
     name: "datastore_logfiles",
-    loadView: function(params) { render(params.url); },
+    loadView: function(params) { load(params.url); },
     unloadView: function() {},
-    handleNavigationChange: render
+    handleNavigationChange: load
   };
 })());
