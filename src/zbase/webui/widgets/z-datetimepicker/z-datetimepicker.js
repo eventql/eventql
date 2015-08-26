918802ca200e7f19339b92ba79a27fe68efb4963
Laura Schlimmer <laura@etat-gray.com>
2015-08-26 14:24:27 +0200

    control time input values

--- src/zbase/webui/widgets/z-datetimepicker/z-datetimepicker.js
+++ src/zbase/webui/widgets/z-datetimepicker/z-datetimepicker.js
@@ -19,13 +19,19 @@ var DateTimePicker = function(input) {
   };
 
   /*************************** PRIVATE **********************************/
-  this.__render = function() {
+  this.__init = function() {
     // insert tpl after input
     input.parentNode.insertBefore(tpl, input.nextSibling);
     //FIXME
     proto = input.nextElementSibling;
+    var _this = this;
+    proto.querySelector("button").addEventListener("click", function() {
+      _this.__apply();
+    }, false);
 
+    this.__onTimeChange();
     this.__handleVisibility();
+    this.__controlTimeInput();
   };
 
   this.__handleVisibility = function() {
@@ -40,9 +46,110 @@ var DateTimePicker = function(input) {
     }, false);
   };
 
+  this.__controlTimeInput = function() {
+    var inputs = proto.querySelectorAll("input");
+    var _this = this;
+
+    for (var i = 0; i < inputs.length; i++) {
+      inputs[i].addEventListener("blur", function() {
+
+        switch (this.value.length) {
+          // if no value is given set it to 00
+          case 0:
+            this.value = "00";
+            break;
+
+          case 1:
+            var value = parseInt(this.value, 10);
+            // non-integer value
+            if (isNaN(value)) {
+              _this.__renderTimeControlError(this);
+              return;
+            }
+            this.value = Fnord.appendLeadingZero(value);
+            break;
+
+          case 2:
+            // non-integer value
+            if (isNaN(parseInt(this.value[0], 10)) ||
+                isNaN(parseInt(this.value[1], 10))) {
+              _this.__renderTimeControlError(this);
+              return;
+            }
+            break;
+        }
+
+        var value = parseInt(this.value, 10);
+        if (this.getAttribute("data-factor") == "3600") {
+          // hours value > 24
+          if (value > 23) {
+            _this.__renderTimeControlError(this);
+            return;
+          }
+        } else {
+          // minutes or seconds value > 59
+          if (value > 59) {
+            _this.__renderTimeControlError(this);
+            return;
+          }
+        }
+
+
+        // correct input value
+        _this.__removeTimeControlError(this);
+      }, false);
+    }
+  };
+
+  this.__removeTimeControlError = function(time_control) {
+    time_control.classList.remove("error");
+    // if only correct input values enable apply button
+    if (proto.querySelector("input.error") == null) {
+      proto.querySelector("button").removeAttribute("data-state");
+    }
+  };
+
+  this.__renderTimeControlError = function(time_control) {
+    time_control.classList.add("error");
+    // disable apply button
+    proto.querySelector("button").setAttribute("data-state", "disabled");
+  };
+
+  this.__onTimeChange = function() {
+    var timestamp = Math.floor(
+      parseInt(input.getAttribute("data-timestamp"), 10));
+
+    // set input value
+    input.value = DateUtil.printTimestamp(timestamp);
+
+    timestamp = Math.floor(timestamp / 1000);
+    var date = new Date(timestamp);
+
+    // set z-calendar selection
+    proto.querySelector("z-calendar").setAttribute("data-selected", timestamp);
+    // set hours input value
+    proto.querySelector("input[data-value='hours']").value =
+      DateUtil.appendLeadingZero(date.getHours());
+    // set minutes input value
+    proto.querySelector("input[data-value='minutes']").value =
+      DateUtil.appendLeadingZero(date.getMinutes());
+    // set seconds input value
+    proto.querySelector("input[data-value='seconds']").value =
+      DateUtil.appendLeadingZero(date.getSeconds());
+  };
+
+  this.__apply = function() {
+    
+
+    // fire change event on input
+    var evt = new CustomEvent("z-datetimepicker-change");
+    input.dispatchEvent(evt);
+    console.log("apply");
+  };
+
 
   // init
-  this.__render();
+  this.__init();
 };
 
 /*var proto = Object.create(HTMLElement.prototype);
