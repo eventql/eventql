741e1063eb58087c1032d8fba8327907ac671077
Laura Schlimmer <laura@etat-gray.com>
2015-08-26 21:28:59 +0200

    datepicker: submit

--- src/zbase/webui/widgets/z-datetimepicker/z-datetimepicker.js
+++ src/zbase/webui/widgets/z-datetimepicker/z-datetimepicker.js
@@ -11,7 +11,7 @@ function DateTimePicker(input) {
   }
 
   var getTime = function() {
-    return Math.floor(parseInt(input.getAttribute("data-timestamp"), 10) / 1000);
+    return parseInt(input.getAttribute("data-timestamp"), 10);
   }
 
   var setTime = function(new_time) {
@@ -21,7 +21,7 @@ function DateTimePicker(input) {
 
     input.setAttribute("data-timestamp", new_time);
     input.value = DateUtil.printTimestamp(new_time);
-  }
+  };
 
   var toggleFlyout = function() {
     if (flyout.hasAttribute("data-active")) {
@@ -54,55 +54,21 @@ function DateTimePicker(input) {
   };
 
   var submit = function() {
-    console.log("update value...");
-    //var new_time = DateUtil.fromCivilTime(
-        // year,
-        // month,
-        // day,
-        // hours,
-        // minutes,
-        // seconds);
+    var date = DateUtil.getStartOfDay(
+        $("z-calendar", flyout).getAttribute("data-selected"));
 
-    setTime(new_time);
+    var time = DateUtil.fromCivilTime(
+        $(".hours_control", flyout).value,
+        $(".minutes_control", flyout).value,
+        $(".seconds_control", flyout).value);
+
+    setTime(date + time);
     hideFlyout();
+
+    var evt = new CustomEvent("z-datetimepicker-change");
+    input.dispatchEvent(evt);
   };
 
-
-//
-//
-//  // get selected datetime as timestamp
-//  var getTimeValue = function() {
-//    var timestamp = DateUtil.parseTimestamp(
-//      parseInt(flyout.querySelector("z-calendar")
-//        .getAttribute("data-selected"), 10));
-//
-//    var inputs = flyout.querySelectorAll("input");
-//    for (var i = 0; i < inputs.length; i++) {
-//      var value = parseInt(inputs[i].value, 10);
-//      timestamp += value * parseInt(inputs[i].getAttribute("data-factor"), 10);
-//    }
-//
-//    timestamp = timestamp * 1000;
-//    return timestamp;
-//  };
-
-//
-//  var apply = function() {
-//    var timestamp = getTimeValue();
-//
-//    // invalid timestamp
-//    if (!isValidTimeValue(timestamp)) {
-//      return;
-//    }
-//
-//    input.setAttribute("data-timestamp", timestamp);
-//    onTimeChange();
-//    hide();
-//
-//    // fire change event on input
-//    var evt = new CustomEvent("z-datetimepicker-change");
-//    input.dispatchEvent(evt);
-//  };
 
   if (input.tagName == "Z-INPUT") {
     input = input.querySelector("input");
