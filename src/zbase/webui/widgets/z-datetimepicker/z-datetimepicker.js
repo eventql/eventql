92a9c96d91c1a9fe6557c9115a6a7d5b362627e7
Laura Schlimmer <laura@etat-gray.com>
2015-08-26 11:52:26 +0200

    splitted into different (js/html/css) files

---
+++ src/zbase/webui/widgets/z-datetimepicker/z-datetimepicker.js
@@ -0,0 +1,99 @@
+var DateTimePickerComponent = function() {
+  console.log("date time picker");
+  this.createdCallback = function() {
+    
+  };
+};
+
+var proto = Object.create(HTMLElement.prototype);
+DateTimePickerComponent.apply(proto);
+document.registerElement("z-datetimepicker", { prototype: proto });
+/*
+
+    this.handleTimeControl = function() {
+      var _this = this;
+      var control_input = document.getElementById("time_control");
+      var widget = document.getElementById("logviewer_date_widget");
+      var inputs = widget.querySelectorAll("input");
+      var apply_button = document.querySelector(
+        "fn-button[data-action='set-logviewer-date']");
+
+      control_input.addEventListener("click", function() {
+        if (widget.classList.contains("hidden")) {
+          widget.classList.remove("hidden");
+        } else {
+          widget.classList.add("hidden")
+        }
+      }, false);
+
+
+      for (var i = 0; i < inputs.length; i++) {
+        inputs[i].addEventListener("blur", function() {
+          switch (this.value.length) {
+            case 0:
+              this.value = "00";
+              break;
+
+            case 1:
+              var value = parseInt(this.value, 10);
+              if (isNaN(value)) {
+                this.classList.add("error");
+                apply_button.setAttribute("data-state", "disabled");
+                return;
+              }
+              this.value = Fnord.appendLeadingZero(value);
+              break;
+
+            case 2:
+              if (isNaN(parseInt(this.value[0], 10)) ||
+                  isNaN(parseInt(this.value[1], 10))) {
+                this.classList.add("error");
+                apply_button.setAttribute("data-state", "disabled");
+                return;
+              }
+          }
+
+          var value = parseInt(this.value, 10);
+          if (this.getAttribute("data-factor") == "3600") {
+            if (value > 23) {
+              this.classList.add("error");
+              apply_button.setAttribute("data-state", "disabled");
+              return;
+            }
+          } else {
+            if (value > 59) {
+              this.classList.add("error");
+              apply_button.setAttribute("data-state", "disabled");
+              return;
+            }
+          }
+
+          this.classList.remove("error");
+          if (widget.querySelector("input.error") == null) {
+            apply_button.removeAttribute("data-state");
+          }
+        }, false);
+      }
+
+      apply_button.addEventListener("click", function() {
+        var timestamp = DateUtil.parseTimestamp(
+          parseInt(widget.querySelector("fn-calendar")
+            .getAttribute("data-selected"), 10));
+        for (var i = 0; i < inputs.length; i++) {
+          var value = parseInt(inputs[i].value, 10);
+          timestamp += value * parseInt(inputs[i].getAttribute("data-factor"), 10);
+        }
+
+        timestamp = timestamp * 1000;
+        _this.initial_end_time = timestamp;
+        _this.updateParams();
+        widget.classList.add("hidden");
+        history.pushState(
+          {param: "until", value: timestamp},
+          "Until",
+          UrlUtil.addOrModifyUrlParam(
+            window.location.href, "until", timestamp));
+      }, false);
+    };
+
+  */
