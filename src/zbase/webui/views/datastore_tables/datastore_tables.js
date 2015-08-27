d0c160a07e72571f4f2b7e5462999d37ca33b13b
Laura Schlimmer <laura@etat-gray.com>
2015-08-27 15:23:22 +0200

    main page struct with home menu

--- src/zbase/webui/views/datastore_tables/datastore_tables.js
+++ src/zbase/webui/views/datastore_tables/datastore_tables.js
@@ -1,18 +1,16 @@
 ZBase.registerView((function() {
 
   var render = function(url) {
-    var layout = $.getTemplate("views/datastore", "zbase_datastore_main_tpl");
-
-    var menu = DatastoreMenu();
-    menu.render($(".datastore_sidebar", layout));
-
     var page = $.getTemplate(
         "views/datastore_tables",
         "zbase_datastore_tables_list_tpl");
 
-    $.replaceContent($(".datastore_viewport", layout), page);
-    $.handleLinks(layout);
-    $.replaceViewport(layout);
+    var menu = HomeMenu();
+    menu.render($(".zbase_home_menu_sidebar", page));
+
+
+    $.handleLinks(page);
+    $.replaceViewport(page);
   };
 
   return {
