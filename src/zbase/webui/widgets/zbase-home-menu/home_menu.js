1a8875a2eb710dbbbdda619078a1b4038e4c3579
Laura Schlimmer <laura@etat-gray.com>
2015-08-27 14:41:04 +0200

    set active menu item

--- src/zbase/webui/widgets/zbase-home-menu/home_menu.js
+++ src/zbase/webui/widgets/zbase-home-menu/home_menu.js
@@ -5,8 +5,24 @@ var HomeMenu = function() {
         "widgets/zbase-home-menu",
         "zbase_home_menu_main_tpl");
 
+    setActiveMenuItem(tpl);
+
     elem.innerHTML = "";
-    elem.appendChild(tpl)
+    elem.appendChild(tpl);
+  };
+
+  var setActiveMenuItem = function(tpl) {
+    var active_item = $("z-menu-item[data-active]", tpl);
+    if (active_item) {
+      active_item.removeAttribute("data-active");
+    }
+
+    var new_active_link = $(
+        "z-menu-item a[href='" + window.location.pathname + "']",
+        tpl);
+    if (new_active_link) {
+      new_active_link.parentNode.setAttribute("data-active", "active");
+    }
   };
 
   return {
