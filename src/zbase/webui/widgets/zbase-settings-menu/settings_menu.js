d4c71120c3fbd355411e32bd59fb04b1156ccee1
Laura Schlimmer <laura@etat-gray.com>
2015-08-28 14:42:40 +0200

    settings menu widget

---
+++ src/zbase/webui/widgets/zbase-settings-menu/settings_menu.js
@@ -0,0 +1,17 @@
+var SettingsMenu = function() {
+
+  var render = function(elem) {
+    var tpl = $.getTemplate(
+        "widgets/zbase-settings-menu",
+        "zbase_settings_menu_main_tpl");
+
+    elem.innerHTML = "";
+    elem.appendChild(tpl)
+  };
+
+  return {
+    render: render
+  };
+
+};
+
