79cd2f6835a10ac2f62ac59c5c22b07e15127bb4
Laura Schlimmer <laura@etat-gray.com>
2015-08-30 20:39:24 +0200

    displayLoader / loadTableSchema

--- src/zbase/webui/widgets/zbase-table-schema-modal/table_schema_modal.js
+++ src/zbase/webui/widgets/zbase-table-schema-modal/table_schema_modal.js
@@ -1,11 +1,55 @@
 var TableSchemaModalWidget = function(elem) {
   var renderModal = function(table) {
-    
+    $(".z-modal-header", modal).innerHTML = table;
+    showLoader();
+    modal.show();
+
+    $.httpGet("/api/v1/tables/" + table, function(r) {
+      if (r.status == 200) {
+        var response = JSON.parse(r.response);
+        var thead = $("thead tr", modal);
+        var tbody = $("tbody", modal);
+
+        thead.innerHTML = "";
+        response.columns.forEach(function(column) {
+          thead.innerHTML += "<th>" + column + "</th>";
+        });
+
+        tbody.innerHTML = "";
+        response.rows.forEach(function(row) {
+          var tr = document.createElement("tr");
+          row.forEach(function(cell) {
+            var td = document.createElement("td");
+            td.innerHTML = cell;
+            tr.appendChild(td);
+          });
+          tbody.appendChild(tr);
+        });
+      } else {
+        modal.close();
+        $.fatalError();
+      }
+
+      hideLoader();
+    });
+  };
+
+  var showLoader = function() {
+    $("table", modal).classList.add("hidden");
+    $(".zbase_loader", modal).classList.remove("hidden");
+  };
+
+  var hideLoader = function() {
+    $("table", modal).classList.remove("hidden");
+    $(".zbase_loader", modal).classList.add("hidden");
   };
 
 
-  var tpl
-  var modal = 
+  var tpl = $.getTemplate(
+    "widgets/zbase-table-schema-modal",
+    "zbase_table_schema_modal_tpl");
+  var modal = $("z-modal", tpl);
+  elem.appendChild(tpl);
 
   return {
     render: renderModal
