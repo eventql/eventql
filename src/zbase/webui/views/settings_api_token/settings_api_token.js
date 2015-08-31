5cafa50e439478e0d938102102846efae0f2c633
Laura Schlimmer <laura@etat-gray.com>
2015-08-31 16:47:29 +0200

    render api token

--- src/zbase/webui/views/settings_api_token/settings_api_token.js
+++ src/zbase/webui/views/settings_api_token/settings_api_token.js
@@ -1,12 +1,24 @@
 ZBase.registerView((function() {
   var render = function() {
-    console.log("render settings api token");
     var page = $.getTemplate(
         "views/settings_api_token",
         "zbase_settings_api_token_main_tpl");
 
     var menu = SettingsMenu();
     menu.render($(".zbase_settings_menu_sidebar", page));
+
+    $.handleLinks(page);
+    $.replaceViewport(page);
+
+    $.httpGet("/api/v1/auth/private_api_token?allow_write=1", function(r) {
+      if (r.status == 200) {
+        $("input.api_token").value = JSON.parse(r.response).api_token;
+      } else {
+        $.fatalError();
+      }
+    });
+
+    $.hideLoader();
   };
 
   return {
