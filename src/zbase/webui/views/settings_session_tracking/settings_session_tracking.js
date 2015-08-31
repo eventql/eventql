949bfa949bedea41e722ff1d24fc1302b8c86b4d
Laura Schlimmer <laura@etat-gray.com>
2015-08-31 20:15:52 +0200

    page strucuture

--- src/zbase/webui/views/settings_session_tracking/settings_session_tracking.js
+++ src/zbase/webui/views/settings_session_tracking/settings_session_tracking.js
@@ -1,13 +1,22 @@
 ZBase.registerView((function() {
-  var load = function() {
+  var render = function() {
+    var page = $.getTemplate(
+        "views/settings_session_tracking",
+        "zbase_settings_session_tracking_main_tpl");
 
+    var menu = SettingsMenu();
+    menu.render($(".zbase_settings_menu_sidebar", page));
+
+    $.handleLinks(page);
+    $.replaceViewport(page);
+    $.hideLoader();
   };
 
   return {
     name: "settings_session_tracking",
-    loadView: function(params) { load(); },
+    loadView: function(params) { render(); },
     unloadView: function() {},
-    handleNavigationChange: load
+    handleNavigationChange: render
   };
 
 })());
