7c2e62cff23d8b6caeeebc38fe03d8581aad5b6a
Laura Schlimmer <laura@etat-gray.com>
2015-09-01 21:22:23 +0200

    basic layout

--- src/zbase/webui/views/report/report.js
+++ src/zbase/webui/views/report/report.js
@@ -1,13 +1,70 @@
 ZBase.registerView((function() {
-    var load = function() {
-      console.log("load report view");
-    };
+  var kPathPrefix = "/a/reports/";
+  var doc_sync;
 
-    return {
+  var loadReport = function(params) {
+    var query_id = params.path.substr(kPathPrefix.length);
+
+    $.showLoader();
+    $.httpGet("/api/v1/documents/" + query_id, function(r) {
+      if (r.status == 200) {
+        var doc = JSON.parse(r.response);
+        renderReport(doc);
+        $.hideLoader();
+      } else {
+        $.fatalError();
+      }
+    });
+  };
+
+  var renderReport = function(doc) {
+    var readonly = !doc.is_writable;
+    var page = $.getTemplate(
+        "views/report",
+        "zbase_report_main_tpl");
+
+    if (!readonly) {
+      // setup docsync
+      docsync = DocSync(
+          getDocument,
+          "/api/v1/documents/" + doc.uuid,
+          $(".zbase_report_infobar", page));
+    }
+
+
+    //report name
+    $(".report_name", page).innerHTML = doc.name;
+    var report_title = $(".zbase_report_pane .report_name", page)
+    report_title.innerHTML = doc.name;
+
+    if (!readonly) {
+      report_title.classList.add("editable");
+      initNameEditModal();
+    }
+
+    //stub
+    doc.content = {
+      description: "Top On-Site Search Terms per Language that were referred by Google"
+    };
+    //report description
+    $(".zbase_report_pane .report_description", page).innerHTML =
+      doc.content.description;
+
+
+
+    $.handleLinks(page);
+    $.replaceViewport(page);
+  };
+
+  var initNameEditModal = function() {
+
+  };
+
+  return {
     name: "report",
-    loadView: function(params) { load(); },
+    loadView: loadReport,
     unloadView: function() {},
-    handleNavigationChange: load
+    handleNavigationChange: loadReport
   };
 
 })());
