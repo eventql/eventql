a803ab3e18dac69d30905a7aae5b90615780517d
Laura Schlimmer <laura@etat-gray.com>
2015-09-03 14:26:13 +0200

    WidgetList interface

---
+++ src/zbase/webui/views/report/widget_list.js
@@ -0,0 +1,26 @@
+var WidgetList = function() {
+  
+  var init = function(elem) {
+
+  };
+
+  var getJSON = function() {
+
+  };
+
+  var setJSON = function(json) {
+
+  };
+
+  var setEditable = function(is_editable) {
+
+  };
+
+
+  return {
+    init: init,
+    getJSON: getJSON,
+    setJSON: setJSON,
+    setEditable: setEditable
+  }
+};
