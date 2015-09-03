e899ad3122c4d428b7a89095e0020fe55b4f018f
Laura Schlimmer <laura@etat-gray.com>
2015-09-03 16:49:57 +0200

    render widgets / switch view mode

--- src/zbase/webui/views/report/widget_list.js
+++ src/zbase/webui/views/report/widget_list.js
@@ -1,7 +1,9 @@
 var WidgetList = function() {
+  var pane;
   var widgets = [];
 
   var init = function(elem) {
+    pane = elem;
     console.log("init");
     
   };
@@ -12,10 +14,32 @@ var WidgetList = function() {
 
   var setJSON = function(new_widgets) {
     widgets = new_widgets;
+    var tpl = $.getTemplate(
+          "views/report",
+          "zbase_report_widget_main_tpl");
+
+    widgets.forEach(function(widget) {
+      var elem = tpl.cloneNode(true);
+      //widget.render(elem)
+
+      $.onClick($("button.edit", elem), function() {
+        console.log("render widget edit view");
+        //widget.editView();
+      });
+      pane.appendChild(elem);
+    });
   };
 
   var setEditable = function(is_editable) {
+    var widget_elems = document.querySelectorAll(".zbase_report_widget");
 
+    for (var i = 0; i < widget_elems.length; i++) {
+      if (is_editable) {
+        widget_elems[i].classList.add("editable");
+      } else {
+        widget_elems[i].classList.remove("editable");
+      }
+    }
   };
 
 
