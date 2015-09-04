ae087c91219e5e43efb43c92cffec6a070f0edc2
Laura Schlimmer <laura@etat-gray.com>
2015-09-04 17:43:44 +0200

    load widgets

--- src/zbase/webui/views/report/report_widget_factory.js
+++ src/zbase/webui/views/report/report_widget_factory.js
@@ -1,9 +1,13 @@
 var ReportWidgetFactory = (function() {
   var widgets = {};
 
-  //[report_sql_widget]
   var loadWidgets = function(required_widgets, on_loaded) {
-    //widget name -> module name 
+    var widget_modules = [];
+    required_widgets.forEach(function(widget) {
+      widget_modules.push("report-widgets/report-" + widget);
+    });
+
+    ZBase.loadModules(widget_modules, on_loaded);
   };
 
   var register = function(widget) {
