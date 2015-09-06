9df57da0250066e454bdc79b9d37c219a6f31f42
Laura Schlimmer <laura@etat-gray.com>
2015-09-06 20:11:26 +0200

    update status setting

--- src/zbase/webui/widgets/zbase-document-settings/doc_settings.js
+++ src/zbase/webui/widgets/zbase-document-settings/doc_settings.js
@@ -1,16 +1,72 @@
-var DocumentSettingsWidget = function(docid) {
-  var render = function(elem) {
+var DocumentSettingsWidget = function(elem, docid) {
+  var init = function() {
     var tpl = $.getTemplate(
         "widgets/zbase-document-settings",
         "zbase_doc_settings_main_tpl");
-
     elem.appendChild(tpl);
-    //$.httpGet("/api/v1/documents/" + docid + "?no_content=true", function(r) {
-    console.log(docid);
-    console.log(elem);
+
+    $.httpGet("/api/v1/documents/" + docid + "?no_content=true", function(r) {
+      if (r.status == 200) {
+        var doc = JSON.parse(r.response);
+        setStatus(doc.status);
+        setAclPolicy(doc.acl_policy);
+        setCategory(doc.category);
+      } else {
+        $.fatalError();
+      }
+    });
   };
 
+  var setStatus = function(doc_status) {
+    //REMOVEME
+    if (doc_status == undefined) {
+      doc_status = "published";
+    }
+    //REMOVEME END
+
+    var tpl = $.getTemplate(
+        "widgets/zbase-document-settings",
+        "zbase_doc_settings_status_inner_tpl");
+
+    var dropdown = $("z-dropdown", tpl);
+    $("z-dropdown-item[data-value='" + doc_status + "']", tpl)
+        .setAttribute("data-selected", "selected");
+    dropdown.setAttribute("data-resolved", "resolved");
+
+    $.replaceContent($(".doc_setting_value.status", elem), tpl);
+
+    dropdown.addEventListener("change", function() {
+      updateSettings("status=" + encodeURIComponent(dropdown.getValue()));
+    }, false);
+  };
+
+  var setAclPolicy = function(acl_policy) {
+    var value = $(".doc_setting_value.acl_policy", elem);
+    if (acl_policy == "ACLPOLICY_PRIVATE") {
+      value.innerHTML = "Private"
+    } else {
+      var config = $.getConfig();
+      value.innerHTML = "Everybody at " + config.current_user.namespace;
+    }
+  };
+
+  var setCategory = function(category) {
+    $(".doc_setting_value.category", elem).innerHTML = category;
+  }
+
+  var updateSettings = function(postbody) {
+    //showInfobar
+    $.httpPost("/api/v1/documents/" + docid, postbody, function(r) {
+      if (r.status == 201) {
+        //showInfobar
+      } else {
+        //showInfobar with error
+      }
+    });
+  };
+
+
   return {
-    render: render
+    render: init
   }
 };
