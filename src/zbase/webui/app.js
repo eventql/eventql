6514232c9de3c5b641297ef7f6b9ac453dc9dad8
Laura Schlimmer <laura@etat-gray.com>
2015-09-06 12:01:42 +0200

    create uuid

--- src/zbase/webui/app.js
+++ src/zbase/webui/app.js
@@ -517,6 +517,16 @@ $.wrapText = function(str) {
   return new_str;
 }
 
+$.uuid = function() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+};
+
 document.getTemplateByID = function(template_name) {
   return $.getTemplate("", template_name);
 };
