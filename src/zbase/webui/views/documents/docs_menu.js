a9d16f9d930192924a0b67cc13accee028bb1e8a
Paul Asmuth <paul@paulasmuth.com>
2015-09-07 13:23:26 +0200

    document menu render logic

--- src/zbase/webui/views/documents/docs_menu.js
+++ src/zbase/webui/views/documents/docs_menu.js
@@ -5,7 +5,11 @@ var DocsMenu = function(categories) {
         "views/documents",
         "zbase_documents_menu_main_tpl");
 
-    console.log("categories:", categories);
+    var menu = $("z-menu", tpl);
+    categories.forEach(function(c) {
+      insertMenuItem(c.split("~"), menu);
+    });
+
     setActiveMenuItem(tpl);
 
     elem.innerHTML = "";
@@ -30,6 +34,45 @@ var DocsMenu = function(categories) {
 
     active_item.parentNode.setAttribute("data-active", "active");
   };
+
+  var insertMenuItem = function(path, elem) {
+    if (path.length == 0) {
+      return;
+    }
+
+    if (path.length == 1) {
+      var item_title = path[0];
+      var item = elem.querySelector(
+          "z-menu-item[data-key='" + item_title + "']"); // FIXME escaping
+
+      if (!item) {
+        item = document.createElement("z-menu-item");
+        item.setAttribute("data-key", item_title);
+        elem.appendChild(item);
+
+        var link = document.createElement("a");
+        link.innerHTML = $.escapeHTML(item_title);
+        link.href = "#";
+        item.appendChild(link);
+      }
+    } else {
+      var section_title = path.shift();
+      var section = elem.querySelector(
+          "z-menu-section[data-key='" + section_title + "']"); // FIXME escaping
+
+      if (!section) {
+        section = document.createElement("z-menu-section");
+        section.setAttribute("data-key", section_title);
+        elem.appendChild(section);
+
+        var title = document.createElement("z-menu-title");
+        title.innerHTML = $.escapeHTML(section_title);
+        section.appendChild(title);
+      }
+
+      insertMenuItem(path, section);
+    }
+  }
 
   return {
     render: render
