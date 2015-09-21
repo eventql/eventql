564a288f0fc9e1f0aa06d833bc2e670eb94d0c82
Christian Parpart <trapni@gmail.com>
2015-09-21 15:18:34 +0200

    minor comment typos

--- src/stx/TimeRange.h
+++ src/stx/TimeRange.h
@@ -1,8 +1,8 @@
-// This file is part of the "libcortex" project
+// This file is part of the "libstx" project
 //   (c) 2009-2015 Christian Parpart <https://github.com/christianparpart>
 //   (c) 2014-2015 Paul Asmuth <https://github.com/paulasmuth>
 //
-// libcortex is free software: you can redistribute it and/or modify it under
+// libstx is free software: you can redistribute it and/or modify it under
 // the terms of the GNU Affero General Public License v3.0.
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
@@ -43,7 +43,7 @@ private:
   const Duration duration_;
 };
 
-}  // namespace cortex
+}  // namespace stx
 
 #include <stx/TimeRange_impl.h>
 #endif
