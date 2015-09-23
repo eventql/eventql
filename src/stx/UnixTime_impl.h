37954073a88a5f41c7d0862ad8c5db9d221fc356
Paul Asmuth <paul@paulasmuth.com>
2015-09-23 17:28:17 +0200

    make old compilers happy

--- src/stx/UnixTime_impl.h
+++ src/stx/UnixTime_impl.h
@@ -1,60 +1,60 @@
 namespace stx {
 
-inline constexpr UnixTime::UnixTime(uint64_t utc_time) :
+constexpr UnixTime::UnixTime(uint64_t utc_time) :
     utc_micros_(utc_time) {
 }
 
-inline constexpr bool UnixTime::operator==(const UnixTime& other) const {
+constexpr bool UnixTime::operator==(const UnixTime& other) const {
   return utc_micros_ == other.utc_micros_;
 }
 
-inline constexpr bool UnixTime::operator!=(const UnixTime& other) const {
+constexpr bool UnixTime::operator!=(const UnixTime& other) const {
   return utc_micros_ != other.utc_micros_;
 }
 
-inline constexpr bool UnixTime::operator<(const UnixTime& other) const {
+constexpr bool UnixTime::operator<(const UnixTime& other) const {
   return utc_micros_ < other.utc_micros_;
 }
 
-inline constexpr bool UnixTime::operator>(const UnixTime& other) const {
+constexpr bool UnixTime::operator>(const UnixTime& other) const {
   return utc_micros_ > other.utc_micros_;
 }
 
-inline constexpr bool UnixTime::operator<=(const UnixTime& other) const {
+constexpr bool UnixTime::operator<=(const UnixTime& other) const {
   return utc_micros_ <= other.utc_micros_;
 }
 
-inline constexpr bool UnixTime::operator>=(const UnixTime& other) const {
+constexpr bool UnixTime::operator>=(const UnixTime& other) const {
   return utc_micros_ >= other.utc_micros_;
 }
 
-inline constexpr UnixTime::operator uint64_t() const {
+constexpr UnixTime::operator uint64_t() const {
   return utc_micros_;
 }
 
-inline constexpr UnixTime::operator double() const {
+constexpr UnixTime::operator double() const {
   return utc_micros_;
 }
 
-inline constexpr uint64_t UnixTime::unixMicros() const {
+constexpr uint64_t UnixTime::unixMicros() const {
   return utc_micros_;
 }
 
-inline constexpr UnixTime UnixTime::epoch() {
+UnixTime UnixTime::epoch() {
   return UnixTime(0);
 }
 
-inline constexpr Duration UnixTime::operator-(const UnixTime& other) const {
+constexpr Duration UnixTime::operator-(const UnixTime& other) const {
   return *this > other
-      ? Duration::fromMicroseconds(utc_micros_ - other.utc_micros_)
-      : Duration::fromMicroseconds(other.utc_micros_ - utc_micros_);
+      ? Duration(utc_micros_ - other.utc_micros_)
+      : Duration(other.utc_micros_ - utc_micros_);
 }
 
-inline constexpr UnixTime UnixTime::operator+(const Duration& duration) const {
+constexpr UnixTime UnixTime::operator+(const Duration& duration) const {
   return UnixTime(utc_micros_ + duration.microseconds());
 }
 
-inline constexpr UnixTime UnixTime::operator-(const Duration& duration) const {
+constexpr UnixTime UnixTime::operator-(const Duration& duration) const {
   return UnixTime(utc_micros_ - duration.microseconds());
 }
 
