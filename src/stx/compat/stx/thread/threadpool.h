0bc6efa8a8531a3af9db37e1d8cc16556b436395
Christian Parpart <trapni@gmail.com>
2015-09-24 11:11:37 +0200

    fix ThreadPool compat header

--- src/stx/compat/stx/thread/threadpool.h
+++ src/stx/compat/stx/thread/threadpool.h
@@ -1,3 +1,9 @@
 #pragma once
-#warn "Please include <stx/executor/ThreadPool.h> instead."
+#warning "Please include <stx/executor/ThreadPool.h> instead."
 #include <stx/executor/ThreadPool.h>
+
+namespace stx {
+  namespace thread {
+    using ThreadPool = stx::ThreadPool;
+  }
+}
