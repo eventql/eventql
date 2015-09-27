f79338f6f7135c3840f12f0242b95d716b7d1e2b
Paul Asmuth <paul@paulasmuth.com>
2015-09-27 16:52:11 +0200

    XOR trackids

--- src/zbase/tracker/ztrackid_test.cc
+++ src/zbase/tracker/ztrackid_test.cc
@@ -16,32 +16,32 @@ UNIT_TEST(ZTrackIDTest);
 
 TEST_CASE(ZTrackIDTest, TestDecode, [] () {
   {
-    auto v = ztrackid_decode("IPB0AA2");
+    auto v = ztrackid_decode("5C4RPVX58H04B");
     EXPECT_EQ(v, "dawanda");
   }
 
   {
-    auto v = ztrackid_decode("ipb0aa2");
+    auto v = ztrackid_decode("5c4rpvx58h04b");
     EXPECT_EQ(v, "dawanda");
   }
 
   {
-    auto v = ztrackid_decode("1TKHR0JNJIDJQ1TKHR0JMQPOJF16DII8V");
+    auto v = ztrackid_decode("48CF7E4RUKXUX48CF7E4QS7HI85C4RPW6WV8U98");
     EXPECT_EQ(v, "000000-production");
   }
 
   {
-    auto v = ztrackid_decode("1tkhr0jnjidjq1tkhr0jmqpojf16dii8v");
+    auto v = ztrackid_decode("48cf7e4rukxux48cf7e4qs7hi85c4rpw6wv8u98");
     EXPECT_EQ(v, "000000-production");
   }
 
   {
-    auto v = ztrackid_decode("1X3P9JHTF0CE116DII8V");
+    auto v = ztrackid_decode("78KJCXEC8Q8TU5C4RPW6WV8U98");
     EXPECT_EQ(v, "zscale-production");
   }
 
   {
-    auto v = ztrackid_decode("1x3p9jhtf0ce116dii8v");
+    auto v = ztrackid_decode("78kjcxec8q8tu5c4rpw6wv8u98");
     EXPECT_EQ(v, "zscale-production");
   }
 });
