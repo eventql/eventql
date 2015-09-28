1a63124fe573b7b21cd09e0eb04d1b34bb088db0
Paul Asmuth <paul@paulasmuth.com>
2015-09-28 17:34:45 +0200

    test nested dynamic messages

--- src/stx/protobuf/protobuf_test.cc
+++ src/stx/protobuf/protobuf_test.cc
@@ -39,7 +39,30 @@ RefPtr<msg::MessageSchema> testSchema() {
             msg::FieldType::STRING,
             0,
             true,
-            false)
+            false),
+        msg::MessageSchemaField::mkObjectField(
+            4,
+            "four",
+            true,
+            false,
+            new msg::MessageSchema(
+                  "NestedTestSchema",
+                  Vector<msg::MessageSchemaField> {
+                      msg::MessageSchemaField(
+                          1,
+                          "alpha",
+                          msg::FieldType::STRING,
+                          250,
+                          false,
+                          false),
+                      msg::MessageSchemaField(
+                          2,
+                          "beta",
+                          msg::FieldType::DOUBLE,
+                          0,
+                          false,
+                          false),
+                  }))
       });
 }
 
@@ -59,10 +82,50 @@ TEST_CASE(ProtobufTest, TestDynamicMessageToJSON, [] () {
       R"({"one": "fnord","two": 23.5,"three": ["blah","fubar"]})");
 });
 
+TEST_CASE(ProtobufTest, TestNestedDynamicMessageToJSON, [] () {
+  msg::DynamicMessage msg(testSchema());
+  msg.addField("one", "fnord");
+  msg.addObject("four", [] (msg::DynamicMessage* cld) {
+    cld->addField("alpha", "a");
+    cld->addField("beta", "123.5");
+  });
+  msg.addObject("four", [] (msg::DynamicMessage* cld) {
+    cld->addField("alpha", "b");
+    cld->addField("beta", "42");
+  });
+
+  Buffer json;
+  json::JSONOutputStream jsons(BufferOutputStream::fromBuffer(&json));
+  msg.toJSON(&jsons);
+
+  EXPECT_EQ(
+      json.toString(),
+      R"({"one": "fnord","four": [{"alpha": "a","beta": 123.5},{"alpha": "b","beta": 42.0}]})");
+});
+
+
 TEST_CASE(ProtobufTest, TestDynamicMessageFromJSON, [] () {
   msg::DynamicMessage msg(testSchema());
 
   auto orig_json = R"({"one": "fnord","two": 23.5,"three": ["blah","fubar"]})";
+
+  {
+    auto j = json::parseJSON(orig_json);
+    msg.fromJSON(j.begin(), j.end());
+  }
+
+  {
+    Buffer json;
+    json::JSONOutputStream jsons(BufferOutputStream::fromBuffer(&json));
+    msg.toJSON(&jsons);
+    EXPECT_EQ(json.toString(), orig_json);
+  }
+});
+
+TEST_CASE(ProtobufTest, TestNestedDynamicMessageFromJSON, [] () {
+  msg::DynamicMessage msg(testSchema());
+
+  auto orig_json = R"({"one": "fnord","four": [{"alpha": "a","beta": 123.5},{"alpha": "b","beta": 42.0}]})";
 
   {
     auto j = json::parseJSON(orig_json);
