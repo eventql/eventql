464f3ed58aeebecdbd4b3ab7d29c847e50392e86
Laura Schlimmer <laura@etat-gray.com>
2015-09-29 14:02:58 +0200

    set session tracking menu active item

--- src/zbase/webui/views/settings_session_tracking/session_tracking_menu.js
+++ src/zbase/webui/views/settings_session_tracking/session_tracking_menu.js
@@ -1,4 +1,4 @@
-var SessionTrackingMenu = function() {
+var SessionTrackingMenu = function(path) {
   var render = function(elem) {
     var tpl = $.getTemplate(
         "views/settings_session_tracking",
@@ -10,7 +10,20 @@ var SessionTrackingMenu = function() {
   };
 
   var setActiveMenuItem = function(menu) {
+    var items = menu.querySelectorAll("a");
+    var active_path_length = 0;
+    var active_item;
 
+    for (var i = 0; i < items.length; i++) {
+      var current_path = items[i].getAttribute("href");
+      if (path.indexOf(current_path) == 0 &&
+          current_path.length > active_path_length) {
+              active_item = items[i];
+              active_path_length = current_path.length;
+      }
+    }
+
+    active_item.setAttribute("data-active", "active");
   };
 
   return {
