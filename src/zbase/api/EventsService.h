84ae61dc54cc8a4fb183a0636c988ba6ef5acc02
Paul Asmuth <paul@paulasmuth.com>
2015-09-29 19:07:51 +0200

    events API stubs

---
+++ src/zbase/api/EventsService.h
@@ -0,0 +1,91 @@
+/**
+ * Copyright (c) 2015 - The CM Authors <legal@clickmatcher.com>
+ *   All Rights Reserved.
+ *
+ * This file is CONFIDENTIAL -- Distribution or duplication of this material or
+ * the information contained herein is strictly forbidden unless prior written
+ * permission is obtained.
+ */
+#pragma once
+#include "stx/protobuf/MessageSchema.h"
+#include "stx/io/inputstream.h"
+#include "zbase/core/TSDBService.h"
+#include "zbase/AnalyticsAuth.h"
+#include "zbase/CustomerConfig.h"
+#include "zbase/ConfigDirectory.h"
+#include "zbase/TableDefinition.h"
+#include "zbase/EventScanResult.h"
+#include "zbase/AnalyticsSession.pb.h"
+#include "zbase/EventScanParams.pb.h"
+#include "csql/runtime/runtime.h"
+
+using namespace stx;
+
+namespace zbase {
+
+class EventsService {
+public:
+
+  EventsService(
+      ConfigDirectory* cdir,
+      AnalyticsAuth* auth,
+      zbase::TSDBService* tsdb,
+      zbase::PartitionMap* pmap,
+      zbase::ReplicationScheme* repl,
+      csql::Runtime* sql);
+
+  /**
+   * Scan a logfile, returns the "limit" newest rows that match the condition
+   * and are older than end_time
+   */
+  void scanTable(
+      const AnalyticsSession& session,
+      const String& table_name,
+      const EventScanParams& params,
+      EventScanResult* result,
+      Function<void (bool done)> on_progress);
+
+  /**
+   * Scan a single logfile partition. This method must be executed on a host
+   * that actually stores this partition
+   */
+  void scanTablePartition(
+      const AnalyticsSession& session,
+      const String& table_name,
+      const SHA1Hash& partition,
+      const EventScanParams& params,
+      EventScanResult* result);
+
+  /**
+   * Scan a single remote logfile partition, try all hosts in order from first
+   * to last
+   */
+  void scanRemoteTablePartition(
+      const AnalyticsSession& session,
+      const String& table_name,
+      const SHA1Hash& partition,
+      const EventScanParams& params,
+      const Vector<InetAddr>& hosts,
+      EventScanResult* result);
+
+  /**
+   * Scan a single remote logfile partition on a specific host
+   */
+  bool scanRemoteTablePartition(
+      const AnalyticsSession& session,
+      const String& table_name,
+      const SHA1Hash& partition,
+      const EventScanParams& params,
+      const InetAddr& hosts,
+      EventScanResult* result);
+
+protected:
+  ConfigDirectory* cdir_;
+  AnalyticsAuth* auth_;
+  zbase::TSDBService* tsdb_;
+  zbase::PartitionMap* pmap_;
+  zbase::ReplicationScheme* repl_;
+  csql::Runtime* sql_;
+};
+
+} // namespace zbase
