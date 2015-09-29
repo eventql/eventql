675e02dd13602a968a7cb82e5c4f10e506cfa5b1
Paul Asmuth <paul@paulasmuth.com>
2015-09-29 18:43:54 +0200

    ...

--- src/zbase/webui/views/session_tracking/journey_viewer.js
+++ src/zbase/webui/views/session_tracking/journey_viewer.js
@@ -5,10 +5,14 @@ ZBase.registerView((function() {
         "views/session_tracking",
         "zbase_session_tracking_main_tpl");
 
+    var content = $.getTemplate(
+        "views/session_tracking",
+        "zbase_session_tracking_journey_viewer_tpl");
+
     var menu = SessionTrackingMenu(path);
     menu.render($(".zbase_content_pane .session_tracking_sidebar", page));
 
-    $(".zbase_content_pane .session_tracking_content", page).innerHTML  = "journey_viewer";
+    $(".zbase_content_pane .session_tracking_content", page).appendChild(content);
 
     $.handleLinks(page);
     $.replaceViewport(page);
