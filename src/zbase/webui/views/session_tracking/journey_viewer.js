b5f9a76df79d940e8a02f20b425415368171808a
Paul Asmuth <paul@paulasmuth.com>
2015-09-30 17:43:13 +0200

    journey viewer css

--- src/zbase/webui/views/session_tracking/journey_viewer.js
+++ src/zbase/webui/views/session_tracking/journey_viewer.js
@@ -1,5 +1,7 @@
 ZBase.registerView((function() {
 
+  var query_mgr = EventSourceHandler();
+
   var load = function(path) {
     var page = $.getTemplate(
         "views/session_tracking",
@@ -16,12 +18,25 @@ ZBase.registerView((function() {
 
     $.handleLinks(page);
     $.replaceViewport(page);
+
+
+    //var query = query_mgr.get(
+    //    "journey_fetch",
+    //    "/api/v1/events/scan?table=sessions&limit=10");
+
+    //query.addEventListener('result', function(e) {
+    //  console.log(e);
+    //});
+  };
+
+  var destroy = function() {
+    query_mgr.closeAll();
   };
 
   return {
     name: "session_tracking_journey_viewer",
     loadView: function(params) { load(params.path); },
-    unloadView: function() {},
+    unloadView: destroy,
     handleNavigationChange: load
   };
 
