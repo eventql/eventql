bb22a039c83ace01c8e4c48dc8a3b1312eb7317c
Paul Asmuth <paul@paulasmuth.com>
2015-10-01 22:29:58 +0200

    remote aggregate caching

--- src/csql/runtime/ExecutionContext.h
+++ src/csql/runtime/ExecutionContext.h
@@ -34,7 +34,7 @@ public:
 
   ExecutionContext(
       TaskScheduler* sched,
-      size_t max_concurrent_tasks = 8);
+      size_t max_concurrent_tasks = 16);
 
   void onStatusChange(Function<void (const ExecutionStatus& status)> fn);
 
