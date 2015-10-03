8619fc0cb7f68dc3d5d1598d90b3fae83288b340
Paul Asmuth <paul@paulasmuth.com>
2015-10-03 17:29:06 +0200

    RemoteTSDBScan::executeOnHost...

--- src/zbase/core/RemoteTSDBScan.cc
+++ src/zbase/core/RemoteTSDBScan.cc
@@ -8,6 +8,9 @@
  * <http://www.gnu.org/licenses/>.
  */
 #include <stx/logging.h>
+#include <stx/thread/future.h>
+#include <stx/http/httpclient.h>
+#include <stx/protobuf/msg.h>
 #include <zbase/core/RemoteTSDBScan.h>
 
 using namespace stx;
@@ -78,7 +81,70 @@ void RemoteTSDBScan::executeOnHost(
     const RemoteTSDBScanParams& params,
     const InetAddr& host) {
   iputs("execute scan on $0 => $1", host.hostAndPort(), params.DebugString());
-  RAISE(kNotYetImplementedError, "not yet implemented");
+
+  Buffer buffer;
+  bool eos = false;
+  auto handler = [&buffer, &eos] (const void* data, size_t size) {
+    buffer.append(data, size);
+    size_t consumed = 0;
+
+    util::BinaryMessageReader reader(buffer.data(), buffer.size());
+    while (reader.remaining() >= sizeof(uint64_t)) {
+      auto rec_len = *reader.readUInt64();
+
+      if (rec_len > reader.remaining()) {
+        break;
+      }
+
+      if (rec_len == 0) {
+        eos = true;
+      } else {
+        Buffer row(reader.read(rec_len), rec_len);
+        iputs("got row: $0", rec_len);
+
+        consumed = reader.position();
+      }
+    }
+
+    Buffer remaining((char*) buffer.data() + consumed, buffer.size() - consumed);
+    buffer.clear();
+    buffer.append(remaining);
+  };
+
+  auto handler_factory = [&handler] (const Promise<http::HTTPResponse> promise)
+      -> http::HTTPResponseFuture* {
+    return new http::StreamingResponseFuture(promise, handler);
+  };
+
+  auto url = StringUtil::format(
+      "http://$0/api/v1/sql/scan_partition",
+      host.ipAndPort());
+
+  //AnalyticsPrivileges privileges;
+  //privileges.set_allow_private_api_read_access(true);
+  //auto api_token = auth->getPrivateAPIToken(customer, privileges);
+
+  http::HTTPMessage::HeaderList auth_headers;
+  //auth_headers.emplace_back(
+  //    "Authorization",
+  //    StringUtil::format("Token $0", api_token));
+
+  http::HTTPClient http_client;
+  auto req_body = msg::encode(params);
+  auto req = http::HTTPRequest::mkPost(url, *req_body, auth_headers);
+  auto res = http_client.executeRequest(req, handler_factory);
+  handler(nullptr, 0);
+
+  if (!eos) {
+    RAISE(kRuntimeError, "unexpected EOF");
+  }
+
+  if (res.statusCode() != 200) {
+    RAISEF(
+        kRuntimeError,
+        "received non-200 response: $0",
+        res.body().toString());
+  }
 }
 
 size_t RemoteTSDBScan::rowsScanned() const {
