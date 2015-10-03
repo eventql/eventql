27e706ae20076f0e31abea18bbc622203abffb64
Paul Asmuth <paul@paulasmuth.com>
2015-10-03 22:23:35 +0200

    ReplicationSchemeTest...

--- src/zbase/core/ReplicationScheme_test.cc
+++ src/zbase/core/ReplicationScheme_test.cc
@@ -85,4 +85,114 @@ TEST_CASE(ReplicationSchemeTest, TestHostAssignmentWithTwoNodes, [] () {
   }
 });
 
+TEST_CASE(ReplicationSchemeTest, TestHostAssignmentWithThreeNodes, [] () {
+  ClusterConfig cc;
+  cc.set_dht_num_copies(3);
+
+  {
+    auto node = cc.add_dht_nodes();
+    node->set_status(DHTNODE_LIVE);
+    node->set_name("nodeA");
+    node->set_addr("1.1.1.1:1234");
+    *node->add_sha1_tokens() = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    *node->add_sha1_tokens() = "6666666666666666666666666666666666666666";
+  }
+
+  {
+    auto node = cc.add_dht_nodes();
+    node->set_status(DHTNODE_LIVE);
+    node->set_name("nodeB");
+    node->set_addr("2.2.2.2:1234");
+    *node->add_sha1_tokens() = "3333333333333333333333333333333333333333";
+  }
+
+  {
+    auto node = cc.add_dht_nodes();
+    node->set_status(DHTNODE_LIVE);
+    node->set_name("nodeC");
+    node->set_addr("3.3.3.3:1234");
+    *node->add_sha1_tokens() = "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb";
+    *node->add_sha1_tokens() = "fcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfc";
+  }
+
+
+  DHTReplicationScheme rscheme(cc);
+
+  {
+    auto replicas = rscheme.replicasFor(
+        SHA1Hash::fromHexString("1111111111111111111111111111111111111111"));
+
+    EXPECT_EQ(replicas.size(), 3);
+    EXPECT_EQ(
+        replicas[0].unique_id.toString(),
+        "3333333333333333333333333333333333333333");
+    EXPECT_EQ(replicas[0].addr.ipAndPort(), "2.2.2.2:1234");
+    EXPECT_EQ(
+        replicas[1].unique_id.toString(),
+        "6666666666666666666666666666666666666666");
+    EXPECT_EQ(replicas[1].addr.ipAndPort(), "1.1.1.1:1234");
+    EXPECT_EQ(
+        replicas[2].unique_id.toString(),
+        "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
+    EXPECT_EQ(replicas[2].addr.ipAndPort(), "3.3.3.3:1234");
+  }
+
+  {
+    auto replicas = rscheme.replicasFor(
+        SHA1Hash::fromHexString("8888888888888888888888888888888888888888"));
+
+    EXPECT_EQ(replicas.size(), 3);
+    EXPECT_EQ(
+        replicas[0].unique_id.toString(),
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+    EXPECT_EQ(replicas[0].addr.ipAndPort(), "1.1.1.1:1234");
+    EXPECT_EQ(
+        replicas[1].unique_id.toString(),
+        "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
+    EXPECT_EQ(replicas[1].addr.ipAndPort(), "3.3.3.3:1234");
+    EXPECT_EQ(
+        replicas[2].unique_id.toString(),
+        "3333333333333333333333333333333333333333");
+    EXPECT_EQ(replicas[2].addr.ipAndPort(), "2.2.2.2:1234");
+  }
+
+  {
+    auto replicas = rscheme.replicasFor(
+        SHA1Hash::fromHexString("dddddddddddddddddddddddddddddddddddddddd"));
+
+    EXPECT_EQ(replicas.size(), 3);
+    EXPECT_EQ(
+        replicas[0].unique_id.toString(),
+        "fcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfcfc");
+    EXPECT_EQ(replicas[0].addr.ipAndPort(), "3.3.3.3:1234");
+    EXPECT_EQ(
+        replicas[1].unique_id.toString(),
+        "3333333333333333333333333333333333333333");
+    EXPECT_EQ(replicas[1].addr.ipAndPort(), "2.2.2.2:1234");
+    EXPECT_EQ(
+        replicas[2].unique_id.toString(),
+        "6666666666666666666666666666666666666666");
+    EXPECT_EQ(replicas[2].addr.ipAndPort(), "1.1.1.1:1234");
+  }
+
+  {
+    auto replicas = rscheme.replicasFor(
+        SHA1Hash::fromHexString("fdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfd"));
+
+    EXPECT_EQ(replicas.size(), 3);
+    EXPECT_EQ(
+        replicas[0].unique_id.toString(),
+        "3333333333333333333333333333333333333333");
+    EXPECT_EQ(replicas[0].addr.ipAndPort(), "2.2.2.2:1234");
+    EXPECT_EQ(
+        replicas[1].unique_id.toString(),
+        "6666666666666666666666666666666666666666");
+    EXPECT_EQ(replicas[1].addr.ipAndPort(), "1.1.1.1:1234");
+    EXPECT_EQ(
+        replicas[2].unique_id.toString(),
+        "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
+    EXPECT_EQ(replicas[2].addr.ipAndPort(), "3.3.3.3:1234");
+  }
+});
+
 
