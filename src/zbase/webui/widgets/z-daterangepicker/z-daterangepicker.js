3c2453ba7381123d29fcbc9ff49ca5472c376960
Laura Schlimmer <laura@etat-gray.com>
2015-10-05 17:45:39 +0200

    z-daterangepicker css

--- src/zbase/webui/widgets/z-daterangepicker/z-daterangepicker.js
+++ src/zbase/webui/widgets/z-daterangepicker/z-daterangepicker.js
@@ -10,10 +10,63 @@
  */
 var DateRangePickerComponent = function() {
   this.createdCallback = function() {
+    var tpl = $.getTemplate(
+        "widgets/z-daterangepicker",
+        "z-daterangepicker-base-tpl");
 
+    this.appendChild(tpl);
+
+    //you may want to customize this
+    this.selectableRanges = {
+      'custom' : 'Custom',
+      0 : 'Today',
+      86400000 : 'Yesterday',
+      604800000 : 'Last 7 Days',
+      2592000000 : 'Last 30 Days'
+    }
+
+    if (this.hasAttribute('data-resolved')) {
+      this.init();
+    }
   };
 
-  
+  this.attributeChangedCallback = function(attr, old_val, new_val) {
+    if (attr == 'data-resolved') {
+      this.init();
+    }
+  };
+
+  this.init = function() {
+    var _this = this;
+
+    this.querySelector("z-daterangepicker-field").onclick = function(e) {
+      e.stopPropagation();
+      _this.toggleWidgetVisibility();
+    };
+
+    this.querySelector("z-daterangepicker-widget").onclick = function(e) {
+      e.stopPropagation();
+    };
+
+    var from = DateUtil.getStartOfDay(
+      DateUtil.parseTimestamp(this.getAttribute('data-from')));
+    var until = DateUtil.getStartOfDay(
+      DateUtil.parseTimestamp(this.getAttribute('data-until')));
+
+    this.applied = {
+      'from' : from,
+      'until' : until
+    };
+
+    this.initWidget();
+    this.renderPreviewField();
+
+    this.observeRangeDropdown();
+    this.observeCalendar();
+    this.observeRangeFields();
+    this.observeButtons();
+  };
+
 };
 
 var proto = Object.create(HTMLElement.prototype);
