3d75353d2d461e55711f0f47ee713e7bb2433671
Laura Schlimmer <laura@etat-gray.com>
2015-10-06 16:57:20 +0200

    logfile editor: render fields

--- src/zbase/webui/views/logviewer/logfile_editor.js
+++ src/zbase/webui/views/logviewer/logfile_editor.js
@@ -20,8 +20,47 @@ ZBase.registerView((function() {
 
     $("h1", page).innerHTML = def.name;
     $("textarea.regex", page).value = def.regex;
+
+    renderSourceFields($(".editor_pane.source_fields", page), def.source_fields);
+    renderRowFields($(".editor_pane.row_fields", page), def.row_fields);
+
     $.replaceViewport(page);
     console.log(def);
+  };
+
+  var renderSourceFields = function(elem, fields) {
+    console.log(fields);
+    var tbody = $("tbody", elem);
+    var tr_tpl = $.getTemplate(
+        "views/logviewer",
+        "zbase_logviewer_logfile_editor_row_tpl");
+
+    fields.forEach(function(field) {
+      var tr = tr_tpl.cloneNode(true);
+      $(".name", tr).innerHTML = field.name;
+      $(".type", tr).innerHTML = field.type;
+      $(".format", tr).innerHTML = field.format;
+
+      tbody.appendChild(tr);
+    });
+  };
+
+  var renderRowFields = function(elem, fields) {
+    console.log(fields);
+    var tbody = $("tbody", elem);
+    var tr_tpl = $.getTemplate(
+        "views/logviewer",
+        "zbase_logviewer_logfile_editor_row_tpl");
+
+    fields.forEach(function(field) {
+      var tr = tr_tpl.cloneNode(true);
+      $(".name", tr).innerHTML = field.name;
+      $(".type", tr).innerHTML = field.type;
+      $(".format", tr).innerHTML = field.format;
+
+      tbody.appendChild(tr);
+    });
+
   };
 
   return {
