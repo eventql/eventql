aec75b1a0b048089d558194008f4ef35e2d31996
Laura Schlimmer <laura@etat-gray.com>
2015-10-06 15:33:50 +0200

    logfiles_list: add logfile modal

--- src/zbase/webui/views/logviewer/logfile_list.js
+++ src/zbase/webui/views/logviewer/logfile_list.js
@@ -44,8 +44,42 @@ ZBase.registerView((function() {
       tbody.appendChild(tr);
     });
 
+
+    $.onClick($(".link.add", page), displayAddLogfileModal);
+    var add_modal = $("z-modal.add_logfile", page);
+    $.onClick($("button.submit", add_modal), addLogfile);
+    $.onClick($("button.close", add_modal), function() {
+      add_modal.close();
+    });
+
+
     $.handleLinks(page);
     $.replaceViewport(page);
+  };
+
+  var displayAddLogfileModal = function() {
+    var modal = $(".zbase_logviewer .logfile_list z-modal.add_logfile");
+    var tpl = $.getTemplate(
+        "views/logviewer",
+        "zbase_logviewer_logfile_list_modal_tpl");
+
+    $.replaceContent($(".z-modal-content", modal), tpl);
+    modal.show();
+  };
+
+  var addLogfile = function() {
+    var input = $(".zbase_logviewer .logfile_list z-modal.add_logfile input");
+
+    if (input.value == 0) {
+      $(".zbase_logviewer .logfile_list z-modal.add_logfile .error_note")
+          .classList.remove("hidden");
+      input.classList.add("error");
+      return;
+    }
+
+    var url = "/api/v1/logfiles/add_logfile?name=" + $.escapeHTML(input.value);
+    alert("POST " + url);
+    //TODO redirect to edit schema page
   };
 
   return {
