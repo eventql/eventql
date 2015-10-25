43d36af44e5e52e08986347ad533c0ff74c9ad9d
Laura Schlimmer <laura@zscale.io>
2015-10-25 16:43:16 +0100

    wip main_menu in session_tracking

--- src/zbase/webui/views/session_tracking/dashboard.js
+++ src/zbase/webui/views/session_tracking/dashboard.js
@@ -14,8 +14,8 @@ ZBase.registerView((function() {
         "views/session_tracking",
         "zbase_session_tracking_dashboard_tpl");
 
-    var menu = SessionTrackingMenu(path);
-    menu.render($(".zbase_content_pane .session_tracking_sidebar", page));
+    var main_menu = ZBaseMainMenu();
+    main_menu.render($(".zbase_main_menu", page), path);
     $(".zbase_content_pane .session_tracking_content", page).appendChild(content);
 
     $.handleLinks(page);
