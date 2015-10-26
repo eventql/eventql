961ac753035d32d2546797b358d9c393c4c0716a
Laura Schlimmer <laura@zscale.io>
2015-10-26 18:11:31 +0100

    datastore tables: render add_pane/add_modal

--- src/zbase/webui/views/datastore_tables/datastore_tables.js
+++ src/zbase/webui/views/datastore_tables/datastore_tables.js
@@ -30,6 +30,13 @@ ZBase.registerView((function() {
     var main_menu = ZBaseMainMenu();
     main_menu.render($(".zbase_main_menu", page), url);
 
+    $.onClick($(".add_pane.create_table", page), displayCreateTableModal);
+    var modal = $("z-modal.create_table", page);
+    $.onClick($("button.submit", modal), createTable);
+    $.onClick($("button.close", modal), function() {
+      modal.close();
+    });
+
     $.handleLinks(page);
     $.replaceViewport(page);
   };
@@ -53,6 +60,38 @@ ZBase.registerView((function() {
     tbody.appendChild(elem);
   };
 
+  var displayCreateTableModal = function() {
+    var modal = $(".zbase_datastore_tables z-modal.create_table");
+    var tpl = $.getTemplate(
+        "views/datastore_tables",
+        "zbase_datastore_tables_add_modal_tpl");
+
+    $.replaceContent($(".z-modal-content", modal), tpl);
+    modal.show();
+    $("input", modal).focus();
+  };
+
+  var createTable = function() {
+    var input = $(".zbase_datastore_tables z-modal.create_table input");
+
+    if (input.value == 0) {
+      $(".zbase_datastore_tables z-modal.create_table .error_note")
+          .classList.remove("hidden");
+      input.classList.add("error");
+      input.focus();
+      return;
+    }
+
+    var url = "/api/v1/tables/add_table?table=" + input.value;
+    $.httpPost(url, "", function(r) {
+      if (r.status == 201) {
+        var def = JSON.parse(r.response);
+        //$.navigateTo("/a/logs/view/" + def.name);
+      } else {
+        $.fatalError();
+      }
+    });
+  };
 
   return {
     name: "datastore_tables",
