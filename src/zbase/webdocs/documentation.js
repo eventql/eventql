4ff33b2de6e31b87388e93775b859adfa1493308
Paul Asmuth <paul@paulasmuth.com>
2015-10-31 16:46:50 +0100

    proper toc.json

--- src/zbase/webdocs/documentation.js
+++ src/zbase/webdocs/documentation.js
@@ -3,36 +3,35 @@ var DocumentationMenu = function() {
   var createMenuItems = function(parent, items, index, current) {
     var j = 0;
 
-    for(var i in items) {
-      var item = i;
-      var key = item.toLowerCase().replace(/[ /]/g, "_");
+    items.forEach(function(item) {
+      var path = item.path;
 
       var section = document.createElement("z-menu-item");
       section.classList.add("link", "doc_section");
       parent.appendChild(section);
 
       var link = document.createElement("a");
-      if(key == current) link.classList.add("current");
-      link.href = "/docs/" + key;
+      if(path == current) link.classList.add("current");
+      link.href = "/docs/" + path;
       var indexSpan = document.createElement("span");
       var textSpan = document.createElement("span");
       indexSpan.classList.add("index");
       indexSpan.innerHTML = index.concat(++j).join(".") + ".";
-      textSpan.innerHTML = item;
+      textSpan.innerHTML = item.title;
       link.appendChild(indexSpan);
       link.appendChild(textSpan);
       section.appendChild(link);
 
-      if(Object.keys(items[i]).length > 0) {
-        createMenuItems(section, items[i], index.concat(+j), current);
+      if (item.children) {
+        createMenuItems(section, item.children, index.concat(+j), current);
       }
-    }
+    });
   };
 
-  var render = function(elem, key) {
+  var render = function(elem, current) {
     httpGet("/docs/_/d/toc.json", function(res) {
       var docStructure = JSON.parse(res.response);
-      createMenuItems(elem, docStructure.toc, [], key);
+      createMenuItems(elem, docStructure.toc, [], current);
     });
   };
 
