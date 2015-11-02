37a18785f5a56f2f7ff135d40dfda40884c14bac
Laura Schlimmer <laura@zscale.io>
2015-11-02 13:50:37 +0100

    seller metrics: printNumber:

--- src/zbase/webui/apps/dawanda_seller_analytics/seller_metrics.js
+++ src/zbase/webui/apps/dawanda_seller_analytics/seller_metrics.js
@@ -27,7 +27,7 @@ var ZBaseSellerMetrics = (function() {
     return DateUtil.printDate(Math.round(ts / 1000));
   };
 
-  var print = function(precision) {
+  var printNumber = function(precision) {
     return function(value) {
       if (isNaN(value)) {
         return "-";
@@ -37,34 +37,38 @@ var ZBaseSellerMetrics = (function() {
     }
   };
 
+  var print = function(value) {
+    return value;
+  }
 
   return {
+    shop_id: {print: print},
     time: {print: printTimestamp},
     gmv_eurcent: {aggr: sum, print: printEurcent},
     gmv_per_transaction_eurcent: {aggr: mean, print: printEurcent},
-    num_purchases: {aggr: sum, print: print(0)},
-    num_refunds: {aggr: sum, print: print(0)},
-    refund_rate: {aggr: sum, print: print(2)},
+    num_purchases: {aggr: sum, print: printNumber(0)},
+    num_refunds: {aggr: sum, print: printNumber(0)},
+    refund_rate: {aggr: sum, print: printNumber(2)},
     refunded_gmv_eurcent: {aggr: sum, print: printEurcent},
-    listview_views_ads: {aggr: sum, print: print(0)},
-    listview_views_search_page: {aggr: sum, print: print(0)},
-    listview_views_catalog_page: {aggr: sum, print: print(0)},
-    listview_views_recos: {aggr: sum, print: print(0)},
-    listview_views_shop_page: {aggr: sum, print: print(0)},
-    listview_clicks_ads: {aggr: sum, print: print(0)},
-    listview_clicks_search_page: {aggr: sum, print: print(0)},
-    listview_clicks_catalog_page: {aggr: sum, print: print(0)},
-    listview_clicks_recos: {aggr: sum, print: print(0)},
-    listview_clicks_shop_page: {aggr: sum, print: print(0)},
-    listview_ctr_ads: {aggr: mean, print: print(4)},
-    listview_ctr_search_page: {aggr: mean, print: print(4)},
-    listview_ctr_catalog_page: {aggr: mean, print: print(4)},
-    listview_ctr_recos: {aggr: mean, print: print(4)},
-    listview_ctr_shop_page: {aggr: mean, print: print(4)},
-    num_active_products: {aggr: sum, print: print(0)},
-    num_listed_products: {aggr: sum, print: print(0)},
-    shop_page_views: {aggr: sum, print: print(0)},
-    product_page_views: {aggr: sum, print: print(0)},
-    total_listviews: {aggr: sum, print: print(0)}
+    listview_views_ads: {aggr: sum, print: printNumber(0)},
+    listview_views_search_page: {aggr: sum, print: printNumber(0)},
+    listview_views_catalog_page: {aggr: sum, print: printNumber(0)},
+    listview_views_recos: {aggr: sum, print: printNumber(0)},
+    listview_views_shop_page: {aggr: sum, print: printNumber(0)},
+    listview_clicks_ads: {aggr: sum, print: printNumber(0)},
+    listview_clicks_search_page: {aggr: sum, print: printNumber(0)},
+    listview_clicks_catalog_page: {aggr: sum, print: printNumber(0)},
+    listview_clicks_recos: {aggr: sum, print: printNumber(0)},
+    listview_clicks_shop_page: {aggr: sum, print: printNumber(0)},
+    listview_ctr_ads: {aggr: mean, print: printNumber(4)},
+    listview_ctr_search_page: {aggr: mean, print: printNumber(4)},
+    listview_ctr_catalog_page: {aggr: mean, print: printNumber(4)},
+    listview_ctr_recos: {aggr: mean, print: printNumber(4)},
+    listview_ctr_shop_page: {aggr: mean, print: printNumber(4)},
+    num_active_products: {aggr: sum, print: printNumber(0)},
+    num_listed_products: {aggr: sum, print: printNumber(0)},
+    shop_page_views: {aggr: sum, print: printNumber(0)},
+    product_page_views: {aggr: sum, print: printNumber(0)},
+    total_listviews: {aggr: sum, print: printNumber(0)}
   }
 })();
