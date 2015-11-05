f1ce202fa08ce2460b31d26302b5762de20765ca
Laura Schlimmer <laura@zscale.io>
2015-11-05 12:36:05 +0100

    render seller_csv_download pane

--- src/zbase/webui/apps/dawanda_seller_analytics/seller_csv_download.js
+++ src/zbase/webui/apps/dawanda_seller_analytics/seller_csv_download.js
@@ -1,5 +1,10 @@
 var SellerCSVDownload = (function() {
   var render = function(elem) {
+    var tpl = $.getTemplate(
+        "apps/dawanda_seller_analytics",
+        "seller_csv_download_main_tpl");
+
+    $.replaceContent(elem, tpl);
     console.log("render csv download");
   }
 
