91c0be2084d10f5c6e1d58f3a0727adabd550e1f
Laura Schlimmer <laura@zscale.io>
2015-11-06 16:10:56 +0100

    render create table view

--- src/zbase/webui/views/datastore_tables/create_table.js
+++ src/zbase/webui/views/datastore_tables/create_table.js
@@ -5,20 +5,41 @@ var createTableView = (function() {
         "zbase_datastore_tables_create_table_tpl");
 
     var columns = $(".columns tbody", tpl);
-    renderColumnRow(columns);
-    renderColumnRow(columns);
 
-    $.onClick($(".add_column.main", tpl), function() {
+    $.onClick($(".add_column", tpl), function() {
       renderColumnRow(columns);
     });
 
+    renderColumnRow(columns);
+    renderColumnRow(columns);
+
     $.replaceContent(elem, tpl);
+
+    $(".create_table .table input").focus();
   };
 
   var renderColumnRow = function(elem) {
     var tpl = $.getTemplate(
         "views/datastore_table",
         "zbase_datastore_tables_create_table_column_tpl");
+
+    var nested_columns_tr = $("tr.nested", tpl);
+    var nested_columns = $("tr.nested .columns", tpl);
+
+    $("z-dropdown.type", tpl).addEventListener("change", function() {
+      if (this.getValue() == "object") {
+        nested_columns_tr.classList.remove("hidden");
+        nested_columns.innerHTML = "";
+        renderColumnRow(nested_columns);
+        renderColumnRow(nested_columns);
+      } else {
+        nested_columns_tr.classList.add("hidden");
+      }
+    }, false);
+
+    $.onClick($(".add_column", nested_columns_tr), function() {
+      renderColumnRow(nested_columns);
+    });
 
     elem.appendChild(tpl);
   };
