a28d3ebdd7313450dbbdad4311aec969817f2211
Paul Asmuth <paul@paulasmuth.com>
2015-11-08 16:38:54 +0100

    print status

--- src/zbase/zli.cc
+++ src/zbase/zli.cc
@@ -15,6 +15,7 @@
 #include "stx/thread/eventloop.h"
 #include "stx/thread/threadpool.h"
 #include "stx/thread/FixedSizeThreadPool.h"
+#include "stx/io/TerminalOutputStream.h"
 #include "stx/wallclock.h"
 #include "stx/json/json.h"
 #include "stx/json/jsonrpc.h"
@@ -42,32 +43,61 @@ void cmd_run(const cli::FlagParser& flags) {
   bool finished = false;
   bool error = false;
   String error_string;
+  bool line_dirty = false;
 
   auto event_handler = [&] (const http::HTTPSSEEvent& ev) {
     if (ev.name.isEmpty()) {
       return;
     }
 
+    if (ev.name.get() == "status") {
+      auto obj = json::parseJSON(ev.data);
+      auto tasks_completed = json::objectGetUInt64(obj, "num_tasks_completed");
+      auto tasks_total = json::objectGetUInt64(obj, "num_tasks_total");
+      auto tasks_running = json::objectGetUInt64(obj, "num_tasks_running");
+
+      stderr_os->write(
+          StringUtil::format(
+              "\r[$0/$1] $2 tasks running",
+              tasks_completed.isEmpty() ? 0 : tasks_completed.get(),
+              tasks_total.isEmpty() ? 0 : tasks_total.get(),
+              tasks_running.isEmpty() ? 0 : tasks_running.get()));
+
+      line_dirty = true;
+      return;
+    }
+
+    if (line_dirty) {
+      stderr_os->write("\r                                                \r");
+      line_dirty = false;
+    }
+
     if (ev.name.get() == "job_started") {
       stderr_os->write(">> Job started\n");
+      return;
     }
 
     if (ev.name.get() == "job_finished") {
       finished = true;
+      return;
     }
 
     if (ev.name.get() == "error") {
       error = true;
       error_string = URI::urlDecode(ev.data);
+      return;
     }
 
     if (ev.name.get() == "result") {
       stdout_os->write(URI::urlDecode(ev.data) + "\n");
+      return;
     }
 
     if (ev.name.get() == "log") {
       stderr_os->write(URI::urlDecode(ev.data) + "\n");
+      return;
     }
+
   };
 
   auto url = StringUtil::format(
