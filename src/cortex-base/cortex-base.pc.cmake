98d0d1f4082be6e3d068f2b3167bd9f4adff9ba3
Paul Asmuth <paul@paulasmuth.com>
2015-11-09 19:07:27 +0100

    cleaning up

--- src/cortex-base/cortex-base.pc.cmake
+++
@@ -1,9 +0,0 @@
-# libcortex-base.pc.cmake
-
-Name: libcortex-base
-Description: C++ Cortex Framework, containing general-purpose APIs
-Version: @CORTEX_BASE_VERSION@
-Requires: libpcre >= 7.0 
-# Conflicts: 
-Libs: -L@CMAKE_INSTALL_PREFIX@/lib -lcortex_base -lpthread -pthread @POSIX_RT_LIBS@
-Cflags: -I@CMAKE_INSTALL_PREFIX@/include -pthread @CFLAGS_RDYNAMIC@ @CXXFLAGS@
--- src/cortex-http/cortex-http.pc.cmake
+++
@@ -1,9 +0,0 @@
-# cortex-http.pc.cmake
-
-Name: cortex-http
-Description: Xzero HTTP framework
-Version: @CORTEX_HTTP_VERSION@
-Requires: cortex-base >= 0.10.0
-# Conflicts: 
-Libs: -lcortex-http
-# Cflags: 
