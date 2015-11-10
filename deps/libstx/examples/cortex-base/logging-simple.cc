e0effa64faaf24733ebff738f730f4d2440d0a2d
Paul Asmuth <paul@paulasmuth.com>
2015-11-10 13:35:48 +0100

    move deps/libstx out

--- deps/libstx/examples/cortex-base/logging-simple.cc
+++
@@ -1,7 +0,0 @@
-#include <cortex-base/logging/LogAggregator.h>
-#include <cortex-base/logging.h>
-
-int main() {
-  cortex::LogAggregator::get().setLogLevel(cortex::LogLevel::trace);
-  cortex::logDebug("example.logging-example", "Hello, %s", "World");
-}
