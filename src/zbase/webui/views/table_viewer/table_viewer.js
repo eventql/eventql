402e30a273a45a6f82f8466dc21c3b0dbc8371d7
Laura Schlimmer <laura@zscale.io>
2015-11-24 17:15:35 +0100

    renderLoader

--- src/zbase/webui/views/table_viewer/table_viewer.js
+++ src/zbase/webui/views/table_viewer/table_viewer.js
@@ -1,16 +1,13 @@
 ZBase.registerView((function() {
+  var query_mgr = null;
+
   var load = function(path) {
+    $.showLoader();
+    query_mgr = EventSourceHandler();
+
     var path_prefix = "/a/datastore/tables/view/";
     var table = path.substr(path_prefix.length);
 
-    render();
-  };
-
-  var destroy = function() {
-
-  };
-
-  var render = function() {
     var tpl = $.getTemplate(
         "views/table_viewer",
         "zbase_table_viewer_tpl");
@@ -18,8 +15,55 @@ ZBase.registerView((function() {
     var main_menu = ZBaseMainMenu();
     main_menu.render($(".zbase_main_menu", tpl), "/a/datastore/tables");
 
+    var table_link = $(".pagetitle a.table_name", tpl);
+    table_link.innerHTML = table;
+    table_link.setAttribute("href", path_prefix + table);
+
     $.handleLinks(tpl);
     $.replaceViewport(tpl);
+    $.hideLoader();
+
+    //REMOVEME
+    renderJSONView();
+    //REMOVEME END
+
+    var url = "/api/v1/events/scan?table=" + table + "&limit=2";
+    var query = query_mgr.get("table_viewer", url);
+
+    query.addEventListener("result", function(e) {
+      console.log(e.data);
+    }, false);
+
+    query.addEventListener("progress", function(e) {
+      var data = JSON.parse(e.data);
+      QueryProgressWidget.render($(".zbase_table_viewer .inner_view"), data);
+      if (data.status == "finished") {
+        query_mgr.close("table_viewer");
+      }
+    }, false);
+
+    query.addEventListener("error", function(e) {
+      query_mgr.close("table_viewer");
+      console.log("error", e);
+    }, false);
+
+  };
+
+  var destroy = function() {
+    if (query_mgr) {
+      query_mgr.closeAll();
+    }
+  };
+
+  var renderJSONView = function(json) {
+    $(".zbase_table_viewer .inner_view").innerHTML =
+        "<div id='table_viewer_json'></div>";
+
+    var inspector = new InspectorJSON({
+      element: 'table_viewer_json',
+      json: '{"hello":"world"}'
+    });
+    console.log(inspector);
   };
 
   return {
