a27d39a2e986d4913d2265af767eb1b6e9848ea4
Laura Schlimmer <laura@zscale.io>
2015-11-24 12:56:19 +0100

    render main page layout

--- src/zbase/webui/views/table_viewer/table_viewer.js
+++ src/zbase/webui/views/table_viewer/table_viewer.js
@@ -1,11 +1,25 @@
 ZBase.registerView((function() {
-
   var load = function(path) {
+    var path_prefix = "/a/datastore/tables/view/";
+    var table = path.substr(path_prefix.length);
 
+    render();
   };
 
   var destroy = function() {
 
+  };
+
+  var render = function() {
+    var tpl = $.getTemplate(
+        "views/table_viewer",
+        "zbase_table_viewer_tpl");
+
+    var main_menu = ZBaseMainMenu();
+    main_menu.render($(".zbase_main_menu", tpl), "/a/datastore/tables");
+
+    $.handleLinks(tpl);
+    $.replaceViewport(tpl);
   };
 
   return {
