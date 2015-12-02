2294071687c1a328a0ef3146d8594a638cf2f10b
Laura Schlimmer <laura@zscale.io>
2015-12-02 16:44:08 +0100

    report: set menu active path

--- src/zbase/webui/views/report/report.js
+++ src/zbase/webui/views/report/report.js
@@ -18,6 +18,7 @@ ZBase.registerView((function() {
     }
 
     $.showLoader();
+    ZBaseMainMenu.update("/a/datastore/queries");
     ZBaseMainMenu.hide();
 
     $.httpGet("/api/v1/documents/" + report_id, function(r) {
