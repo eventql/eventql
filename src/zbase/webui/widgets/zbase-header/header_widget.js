3c901eec7539fe822c1f7d287e721fff22ca6b73
Laura Schlimmer <laura@zscale.io>
2015-12-08 21:39:30 +0100

    header widget set main navi active item

--- src/zbase/webui/widgets/zbase-header/header_widget.js
+++ src/zbase/webui/widgets/zbase-header/header_widget.js
@@ -41,7 +41,31 @@ var HeaderWidget = (function() {
     $.replaceContent(zbase_header, hdr_tpl);
   };
 
-  var update = function(path) {};
+  var update = function(path) {
+    //update main navi
+    var main_navi = $(".zbase_header .main_navi");
+    var current_active = $("a.active", main_navi);
+    if (current_active) {
+      current_active.classList.remove("active");
+    }
+
+    var items = main_navi.querySelectorAll("a");
+    var active_path_length = 0;
+    var active_item;
+
+    for (var i = 0; i < items.length; i++) {
+      var current_path = items[i].getAttribute("href");
+      if (path.indexOf(current_path) == 0 &&
+          current_path.length > active_path_length) {
+              active_item = items[i];
+              active_path_length = current_path.length;
+      }
+    }
+
+    if (active_item) {
+      active_item.classList.add("active");
+    }
+  };
 
   var toggleDropdown = function() {
     this.classList.toggle("open");
