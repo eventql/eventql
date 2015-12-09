dd543f405ecebaefeea942e6162553322ea6b345
Paul Asmuth <paul@paulasmuth.com>
2015-12-09 17:23:39 +0100

    update to v0.2.1

--- src/zbase/z1.h
+++ src/zbase/z1.h
@@ -13,8 +13,8 @@ namespace zbase {
 
 static const uint32_t kVersionMajor = 0;
 static const uint32_t kVersionMinor = 2;
-static const uint32_t kVersionPatch = 0;
-static const std::string kVersionString = "v0.2.0";
+static const uint32_t kVersionPatch = 1;
+static const std::string kVersionString = "v0.2.1";
 
 #ifdef ZBASE_BUILD_ID
 static const std::string kBuildID = ZBASE_BUILD_ID;
