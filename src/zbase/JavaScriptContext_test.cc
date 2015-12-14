16c2b345d58c78420211d5ba5bdc1a4b74f059b2
Paul Asmuth <paul@paulasmuth.com>
2015-12-14 15:09:55 +0100

    jscontext test

---
+++ src/zbase/JavaScriptContext_test.cc
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2015 - zScale Technology GmbH <legal@zscale.io>
+ *   All Rights Reserved.
+ *
+ * Authors:
+ *   Paul Asmuth <paul@zscale.io>
+ *
+ * This file is CONFIDENTIAL -- Distribution or duplication of this material or
+ * the information contained herein is strictly forbidden unless prior written
+ * permission is obtained.
+ */
+#include <stx/stdtypes.h>
+#include <stx/test/unittest.h>
+#include <zbase/JavaScriptContext.h>
+
+using namespace stx;
+using namespace zbase;
+
+UNIT_TEST(JavaScriptContextTest);
+
+TEST_CASE(JavaScriptContextTest, TestMapFunction, [] () {
+  //auto job = mkRef(new MapReduceJobSpec());
+  //auto js_ctx = mkRef(new JavaScriptContext(
+  //    "acme_corp",
+  //    job,
+  //    nullptr,
+  //    nullptr,
+  //    nullptr));
+});
+
