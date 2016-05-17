8c5c695a2d8a3b03c029fc19583203096017cf8a
Paul Asmuth <paul@paulasmuth.com>
2016-05-17 19:12:33 +0200

    build mozjs in subdirectory

--- deps/3rdparty/mozjs/build.sh
+++ deps/3rdparty/mozjs/build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-CC=$1 CXX=$2 RANLIB=$3 AR=$4 CFLAGS="-DJS_THREADSAFE" CXXFLAGS="-DJS_THREADSAFE" ./js/src/configure --disable-shared-js || exit 1
+mkdir __build
+cd __build
+
+CC=$1 CXX=$2 RANLIB=$3 AR=$4 CFLAGS="-DJS_THREADSAFE" CXXFLAGS="-DJS_THREADSAFE" ../js/src/configure --disable-shared-js || exit 1
 CC=$1 CXX=$2 RANLIB=$3 AR=$4 CFLAGS="-DJS_THREADSAFE" CXXFLAGS="-DJS_THREADSAFE" make JS_THREADSAFE=1 || exit 1
