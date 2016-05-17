d9bfe56914b93629b384d196fb1e0bdf6ebaff44
Paul Asmuth <paul@paulasmuth.com>
2016-05-17 19:23:00 +0200

    spidermonkey build.sh...

--- deps/3rdparty/mozjs/build.sh
+++ deps/3rdparty/mozjs/build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-mkdir __build
+mkdir -p __build
 cd __build
 
-CC=$1 CXX=$2 RANLIB=$3 AR=$4 CFLAGS="-DJS_THREADSAFE" CXXFLAGS="-DJS_THREADSAFE" ../js/src/configure --disable-shared-js || exit 1
-CC=$1 CXX=$2 RANLIB=$3 AR=$4 CFLAGS="-DJS_THREADSAFE" CXXFLAGS="-DJS_THREADSAFE" make JS_THREADSAFE=1 || exit 1
+CC=$1 CXX=$2 RANLIB=$3 AR=$4 CFLAGS="-DJS_THREADSAFE" CXXFLAGS="-DJS_THREADSAFE" ../js/src/configure --disable-shared-js && make JS_THREADSAFE=1 || exit 1
