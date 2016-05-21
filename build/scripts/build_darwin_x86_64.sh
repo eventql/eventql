54dd58eeb40d8cf741b2b8fccb3cf3d3d9516020
Paul Asmuth <paul@paulasmuth.com>
2016-05-21 20:12:29 +0200

    makefile..

--- build/scripts/build_darwin_x86_64.sh
+++ build/scripts/build_darwin_x86_64.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 PACKAGE=$1
 VERSION=$2
+set -e
 
 if [[ -z "$PACKAGE" || -z "$VERSION" ]]; then
   echo "usage: $0 <package> <version>" >&2
@@ -12,10 +13,8 @@ if ! test -d $PACKAGE-$VERSION; then
 fi
 
 TARGET_DIR=build/target/$PACKAGE-$VERSION-darwin_x86_64
-test -d && rm -rf $TARGET_DIR
-mkdir $TARGET_DIR $TARGET_DIR/dist
+mkdir -p $TARGET_DIR $TARGET_DIR/dist || true
 cd $TARGET_DIR
-set -e
 
 export CC=x86_64-apple-darwin14-cc
 export CXX=x86_64-apple-darwin14-c++
@@ -29,6 +28,6 @@ export OBJDUMP=x86_64-apple-darwin14-objdump
 export CXXFLAGS="-stdlib=libc++"
 export MOZJS_CXXFLAGS="-DXP_MACOSX=1"
 
-./configure --host=x86_64-apple-darwin14
+../../../$PACKAGE-$VERSION/configure --host=x86_64-apple-darwin14 --prefix=/usr/local
 make
 make install DESTDIR=dist
