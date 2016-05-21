4352c37df19482e67255e0fcc891061e67565f1e
Paul Asmuth <paul@paulasmuth.com>
2016-05-21 17:11:03 +0200

    final mozjs crossbuild fixes

--- build/scripts/build_darwin_x86_64.sh
+++ build/scripts/build_darwin_x86_64.sh
@@ -7,26 +7,21 @@ if [[ -z "$PACKAGE" || -z "$VERSION" ]]; then
   exit 1
 fi
 
-if ! test -d $DIST_SRC_DIR/$PACKAGE-$VERSION; then
-  echo "ERROR: not found: $DIST_SRC_DIR/$PACKAGE-$VERSION"
+if ! test -d $PACKAGE-$VERSION; then
+  echo "ERROR: not found: $PACKAGE-$VERSION"
 fi
 
 TARGET_DIR=build/target/$PACKAGE-$VERSION-darwin_x86_64
-DIST_DIR=build/dist-bin/$PACKAGE-$VERSION-darwin_x86_64
-
-mkdir -p build/target;
-mkdir -p build/dist-bin;
-rm -rf $TARGET_DIR $DIST_DIR
-mkdir $TARGET_DIR $DIST_DIR
+test -d && rm -rf $TARGET_DIR
+mkdir $TARGET_DIR $TARGET_DIR/dist
 cd $TARGET_DIR
 set -e
 
-../../dist-src/$PACKAGE-$VERSION/configure \
+../../../$PACKAGE-$VERSION/configure \
     --host=x86_64-apple-darwin14 \
     CC="x86_64-apple-darwin14-cc" \
     CXX="x86_64-apple-darwin14-c++" \
     MOZJS_CXXFLAGS="-DXP_MACOSX=1"
 
 make
-make install DESTDIR=../../dist-bin/$PACKAGE-$VERSION-darwin_x86_64
-
+make install DESTDIR=dist
