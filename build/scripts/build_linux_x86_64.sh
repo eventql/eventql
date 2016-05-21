d0956845c1923d47ab5a637fb7c4c82ca3d15764
Paul Asmuth <paul@paulasmuth.com>
2016-05-21 20:43:28 +0200

    build_linux_x86_64

--- build/scripts/build_linux_x86_64.sh
+++ build/scripts/build_linux_x86_64.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 PACKAGE=$1
 VERSION=$2
+set -e
 
 if [[ -z "$PACKAGE" || -z "$VERSION" ]]; then
   echo "usage: $0 <package> <version>" >&2
   exit 1
 fi
 
-if ! test -d $DIST_SRC_DIR/$PACKAGE-$VERSION; then
-  echo "ERROR: not found: $DIST_SRC_DIR/$PACKAGE-$VERSION"
+if ! test -d $PACKAGE-$VERSION; then
+  echo "ERROR: not found: $PACKAGE-$VERSION"
 fi
 
 TARGET_DIR=build/target/$PACKAGE-$VERSION-linux_x86_64
-DIST_DIR=build/dist-bin/$PACKAGE-$VERSION-linux_x86_64
-
-mkdir -p build/target;
-mkdir -p build/dist-bin;
-rm -rf $TARGET_DIR $DIST_DIR
-mkdir $TARGET_DIR $DIST_DIR
+mkdir -p $TARGET_DIR $TARGET_DIR/dist || true
 cd $TARGET_DIR
-set -e
 
-../../dist-src/$PACKAGE-$VERSION/configure \
-    --host=x86_64-linux-gnu \
-    CC="x86_64-linux-gnu-gcc" \
-    CXX="x86_64-linux-gnu-g++"
+export CC=x86_64-linux-gnu-gcc
+export CXX=x86_64-linux-gnu-g++
+export LD=x86_64-linux-gnu-ld
+export AR=x86_64-linux-gnu-ar
+export AS=x86_64-linux-gnu-as
+export NM=x86_64-linux-gnu-nm
+export STRIP=x86_64-linux-gnu-strip
+export RANLIB=x86_64-linux-gnu-ranlib
+export OBJDUMP=x86_64-linux-gnu-objdump
 
+../../../$PACKAGE-$VERSION/configure --host=x86_64-linux-gnu --prefix=/usr/local
 make
-make install DESTDIR=../../dist-bin/$PACKAGE-$VERSION-linux_x86_64
-
+make install DESTDIR=dist
