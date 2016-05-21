4518ddfe950520c09c04a3f5a59e1120022ae12e
Paul Asmuth <paul@paulasmuth.com>
2016-05-21 02:05:44 +0200

    wip

--- build/scripts/build_osx.sh
+++ build/scripts/build_osx.sh
@@ -2,13 +2,16 @@
 set -e
 
 mkdir -p build/target/x86_64-apple-darwin
+mkdir -p build/target/i386-apple-darwin
+
 ( \
   cd build/target/x86_64-apple-darwin && \
-  ../../../configure --host=x86_64-apple-darwin \
-        CC=clang \
-        CXX=clang++ \
-        CFLAGS="-mmacosx-version-min=10.6 -isysroot $OSXSDK" \
-        CXXFLAGS="-mmacosx-version-min=10.6 -isysroot $OSXSDK" \
-        LDFLAGS="-mmacosx-version-min=10.6 -isysroot $OSXSDK" && \
-  make \
+  ../../../configure \
+      --host=x86_64-apple-darwin10 \
+      CC=o64-clang \
+      CXX=o64-clang++ \
+      CFLAGS="-mmacosx-version-min=10.6" \
+      CXXFLAGS="-mmacosx-version-min=10.6" \
+      LDFLAGS="-mmacosx-version-min=10.6" && \
+  make
 )
