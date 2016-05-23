5bc3838ca4ac8a89bacec0210c4483a24bb0f224
Paul Asmuth <paul@paulasmuth.com>
2016-05-23 18:16:12 +0200

    output tarballs to PKGDIR

--- build/scripts/package_tarballs.sh
+++ build/scripts/package_tarballs.sh
@@ -2,7 +2,7 @@
 
 PACKAGE=$1
 VERSION=$2
-PKG_DIR=${PKG_DIR:-.}
+PKGDIR=${PKGDIR:-.}
 
 if [[ -z "$PACKAGE" || -z "$VERSION" ]]; then
   echo "usage: $0 <package> <version>" >&2
@@ -14,10 +14,10 @@ if ! test -d $PACKAGE-$VERSION; then
 fi
 
 # source packages
-tar cz $PACKAGE-$VERSION > $PACKAGE-$VERSION.tgz
+tar cz $PACKAGE-$VERSION > $PKGDIR/$PACKAGE-$VERSION.tgz
 
 # binary packages
 for triple in darwin_x86_64 linux_x86_64; do
   tar cz -C build/target/$PACKAGE-$VERSION-$triple/dist . \
-      > $PACKAGE-$VERSION-$triple.tgz
+      > $PKGDIR/$PACKAGE-$VERSION-$triple.tgz
 done
