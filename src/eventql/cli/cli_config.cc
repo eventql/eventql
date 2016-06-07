5ec51179252aa0e3261a3dc2aefad9cb0ef87438
Laura Schlimmer <laura@zscale.io>
2016-06-07 18:36:28 +0200

    refactored CLIConfig iface

--- src/eventql/cli/cli_config.cc
+++ src/eventql/cli/cli_config.cc
@@ -25,239 +25,143 @@
 #include <eventql/cli/cli_config.h>
 #include <eventql/util/inspect.h>
 #include <eventql/util/io/fileutil.h>
-#include <inih/ini.h>
 
 namespace eventql {
 namespace cli {
 
-struct IniParserState {
-  IniParserState(CLIConfig* _cfg) : cfg(_cfg), status(Status::success()) {}
-  CLIConfig* cfg;
-  Status status;
-};
+const String& CLIConfig::kDefaultHost = "localhost";
+const int CLIConfig::kDefaultPort = 9175;
+const String& CLIConfig::kDefaultUser = getenv("USER");
+const bool CLIConfig::kDefaultBatchMode = false;
 
-static int ini_parse_handler(
-    void* user,
-    const char* section,
-    const char* name,
-    const char* value) {
-  auto parser_state = (IniParserState*) user;
-  auto status = parser_state->cfg->setConfigOption(section, name, value);
-  if (status.isSuccess()) {
-    return 1;
-  } else {
-    parser_state->status = status;
-    return 0;
-  }
-}
-
-CLIConfig::CLIConfig() :
-    server_host_("localhost"),
-    server_port_(9175) {
-  user_ = getenv("USER");
-  if (user_.empty()) {
-    user_ = "nobody";
-  }
-}
-
-Status CLIConfig::loadDefaultConfigFile() {
-  char* homedir = getenv("HOME");
-  if (!homedir) {
-    return Status::success();
-  }
-
-  String confg_file_path = FileUtil::joinPaths(homedir, ".evqlrc");
-  if (!FileUtil::exists(confg_file_path)) {
-    return Status::success();
-  }
-
-  return loadConfigFile(confg_file_path);
-}
-
-Status CLIConfig::loadConfigFile(const String& config_file) {
-  IniParserState parser_state(this);
-  if (ini_parse(config_file.c_str(), &ini_parse_handler, &parser_state) < 0) {
-    parser_state.status = Status(eParseError, "invalid config file");
-  }
-
-  return parser_state.status;
-}
-
-Status CLIConfig::setConfigOption(
-    const String& section,
-    const String& key,
-    const String& value) {
-  if (section != "evql") {
-    return Status(
-        eParseError,
-        StringUtil::format("section '$0' is not valid", section));
-  }
-
-  if (value.size() == 0) {
-    return Status(
-        eParseError,
-        StringUtil::format("'$0' value '$1' is not valid", key, value));
-  }
-
-  if (key == "host") {
-    return setHost(value);
-
-  } else if (key == "port") {
-    return setPort(value);
-
-  } else if (key == "auth_token") {
-    return setAuthToken(value);
-
-  } else if (key == "batch_mode") {
-    return setBatchMode(value);
-
-  } else {
-    return Status(
-        eParseError,
-        StringUtil::format("config file option '$0' is not valid", key));
-  }
-}
-
-Status CLIConfig::setDatabase(const String& database) {
-  database_ = database;
-  return Status::success();
-}
-
-String CLIConfig::getDatabase() const {
-  return database_;
-}
-
-Status CLIConfig::setUser(const String& user) {
-  user_ = user;
-  return Status::success();
-}
-
-String CLIConfig::getUser() const {
-  return user_;
-}
-
-Status CLIConfig::setPassword(const String& password) {
-  password_ = password;
-  return Status::success();
-}
-
-String CLIConfig::getPassword() const {
-  return password_;
-}
-
-Status CLIConfig::setHost(const String& host /* = "localhost" */) {
-  server_host_ = host; //FIXME check host format
-  return Status::success();
-}
-
-Status CLIConfig::setPort(const int port /* = 80 */) {
-  if (port < 0 || port > 65535) {
-    return Status(
-        eFlagError,
-        StringUtil::format("'port' value '$0' is not valid", port));
-  }
-
-  server_port_ = port;
-  return Status::success();
-}
-
-Status CLIConfig::setPort(const String& port) {
-  if (!StringUtil::isNumber(port)) {
-    return Status(
-        eFlagError,
-        StringUtil::format("'port' value '$0' is not a valid integer", port));
-  }
-
-  return setPort(stoi(port));
-}
-
-Status CLIConfig::setAuthToken(const String& auth_token) {
-  server_auth_token_ = auth_token;
-  return Status::success();
-}
-
-Status CLIConfig::setBatchMode(const String& batch_mode) {
-  if (batch_mode == "false") {
-    batch_mode_ = false;
-    return Status::success();
-  } else if (batch_mode == "true") {
-    batch_mode_ = true;
-    return Status::success();
-  } else {
-    return Status(eParseError);
-  }
-}
+CLIConfig::CLIConfig(RefPtr<ProcessConfig> cfg) : cfg_(cfg) {}
 
 String CLIConfig::getHost() const {
-  return server_host_;
+  auto host = cfg_->getString("evql", "host");
+  if (host.isEmpty()) {
+    return kDefaultHost;
+  } else {
+    return host.get();
+  }
 }
 
 int CLIConfig::getPort() const {
-  return server_port_;
-}
-
-Option<String> CLIConfig::getAuthToken() const {
-  if (server_auth_token_.size() > 0) {
-    return Some<String>(server_auth_token_);
+  auto port = cfg_->getInt("evql", "port");
+  if (port.isEmpty()) {
+    return kDefaultPort;
   } else {
-    return None<String>();
+    return port.get();
   }
 }
 
-Status CLIConfig::setFile(const String& file) {
-  file_ = file;
-  return Status::success();
+String CLIConfig::getUser() const {
+  auto user = cfg_->getString("evql", "user");
+  if (user.isEmpty()) {
+    return kDefaultUser;
+  } else {
+    return user.get();
+  }
+}
+
+bool CLIConfig::getBatchMode() const {
+  auto batch_mode = cfg_->getBool("evql", "batch");
+  if (batch_mode.isEmpty()) {
+    return kDefaultBatchMode;
+  } else {
+    return batch_mode.get();
+  }
+}
+//Status CLIConfig::loadDefaultConfigFile() {
+//  char* homedir = getenv("HOME");
+//  if (!homedir) {
+//    return Status::success();
+//  }
+//
+//  String confg_file_path = FileUtil::joinPaths(homedir, ".evqlrc");
+//  if (!FileUtil::exists(confg_file_path)) {
+//    return Status::success();
+//  }
+//
+//  return cfg_builder_.loadFile(confg_file_path);
+//}
+//
+Option<String> CLIConfig::getDatabase() const {
+  return cfg_->getString("evql", "database");
+}
+
+
+Option<String> CLIConfig::getPassword() const {
+  return cfg_->getString("evql", "password");
+}
+
+//Status CLIConfig::setPort(const int port /* = 80 */) {
+//  if (port < 0 || port > 65535) {
+//    return Status(
+//        eFlagError,
+//        StringUtil::format("'port' value '$0' is not valid", port));
+//  }
+//
+//  server_port_ = port;
+//  return Status::success();
+//}
+
+//Status CLIConfig::setPort(const String& port) {
+//  if (!StringUtil::isNumber(port)) {
+//    return Status(
+//        eFlagError,
+//        StringUtil::format("'port' value '$0' is not a valid integer", port));
+//  }
+//
+//  return setPort(stoi(port));
+//}
+
+
+Option<String> CLIConfig::getAuthToken() const {
+  return cfg_->getString("evql", "auth_token");
 }
 
 Option<String> CLIConfig::getFile() const {
-  if (file_.empty()) {
-    return None<String>();
-  } else {
-    return Some(file_);
-  }
+  return cfg_->getString("evql", "file");
 }
 
-Status CLIConfig::setLanguage(String language) {
-  StringUtil::toUpper(&language);
-
-  if (language == "SQL") {
-    language_ = Some(CLIConfig::kLanguage::SQL);
-    return Status::success();
-
-  } else if (language == "JS" || language == "JAVASCRIPT") {
-    language_ = Some(CLIConfig::kLanguage::JAVASCRIPT);
-    return Status::success();
-
-  } else {
-    return Status(
-        eFlagError,
-        StringUtil::format("invalid language '$0'", language));
-  }
-}
-
+//Status CLIConfig::setLanguage(String language) {
+//  StringUtil::toUpper(&language);
+//
+//  if (language == "SQL") {
+//    language_ = Some(CLIConfig::kLanguage::SQL);
+//    return Status::success();
+//
+//  } else if (language == "JS" || language == "JAVASCRIPT") {
+//    language_ = Some(CLIConfig::kLanguage::JAVASCRIPT);
+//    return Status::success();
+//
+//  } else {
+//    return Status(
+//        eFlagError,
+//        StringUtil::format("invalid language '$0'", language));
+//  }
+//}
+//
 Option<CLIConfig::kLanguage> CLIConfig::getLanguage() {
-  if (!language_.isEmpty()) {
-    return language_;
-  }
+  //if (!language_.isEmpty()) {
+  //  return language_;
+  //}
 
-  if (!file_.empty()) {
-    if (StringUtil::endsWith(file_, ".sql")) {
-      return Some(CLIConfig::kLanguage::SQL);
-    }
-    if (StringUtil::endsWith(file_, ".js")) {
-      return Some(CLIConfig::kLanguage::JAVASCRIPT);
-    }
-  }
+  //if (!file_.empty()) {
+  //  if (StringUtil::endsWith(file_, ".sql")) {
+  //    return Some(CLIConfig::kLanguage::SQL);
+  //  }
+  //  if (StringUtil::endsWith(file_, ".js")) {
+  //    return Some(CLIConfig::kLanguage::JAVASCRIPT);
+  //  }
+  //}
 
   return None<CLIConfig::kLanguage>();
 }
 Option<String> CLIConfig::getExec() const {
-  return exec_;
+  return cfg_->getString("evql", "exec");
 }
 
-bool CLIConfig::getBatchMode() const {
-  return batch_mode_;
-}
 
 } // namespace cli
 } // namespace eventql
