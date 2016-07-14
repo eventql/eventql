7191d6b5bc189c7bfb079c2296cb83b65969704f
Paul Asmuth <paul@paulasmuth.com>
2016-07-14 14:51:17 +0200

    upgrade exception to crash

--- src/eventql/io/cstable/columns/column_writer_uint.cc
+++ src/eventql/io/cstable/columns/column_writer_uint.cc
@@ -119,11 +119,11 @@ void UnsignedIntColumnWriter::writeString(
     uint64_t dlvl,
     const char* data,
     size_t size) {
-  uint64_t value;
+  uint64_t value = 0;
   try {
     value = std::stoull(String(data, size));
   } catch (...) {
-    RAISEF(kIllegalArgumentError, "can't convert '$0' to uint", String(data, size));
+    assert(false); // "can't convert '$0' to uint"
   }
 
   writeUnsignedInt(rlvl, dlvl, value);
