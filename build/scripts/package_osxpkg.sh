b960065a363d13b537ccc82ee043503a4affe9d6
Paul Asmuth <paul@paulasmuth.com>
2016-07-24 21:05:46 +0200

    osx .pkg build...

--- build/scripts/package_osxpkg.sh
+++ build/scripts/package_osxpkg.sh
@@ -19,8 +19,8 @@ for triple in darwin_x86_64; do
   rm -rf $TMPDIR
   mkdir -p $TMPDIR $TMPDIR/flat/base.pkg $TMPDIR/flat/Resources/en.lproj $TMPDIR/root
   cp -r build/target/$PACKAGE-$VERSION-$triple/dist/* $TMPDIR/root
-  cp build/osxpkg/distribution.xml $TMPDIR/flat
-  cp build/osxpkg/package_info.xml $TMPDIR/base.pkg
+  cp build/osxpkg/distribution.xml $TMPDIR/flat/Distribution
+  cp build/osxpkg/package_info.xml $TMPDIR/base.pkg/PackageInfo
 
   (cd $TMPDIR/root && find . | cpio -o --format odc --owner 0:80 | gzip -c) \
       > $TMPDIR/flat/base.pkg/Payload
