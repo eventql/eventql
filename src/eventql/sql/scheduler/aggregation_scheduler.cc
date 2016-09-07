dd2840bbbfefdf44ac4ea2ddbdf2321b8c8b0c30
Laura Schlimmer <laura@zscale.io>
2016-09-07 14:43:17 +0200

    include AggregationScheduler

---
+++ src/eventql/sql/scheduler/aggregation_scheduler.cc
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2016 DeepCortex GmbH <legal@eventql.io>
+ * Authors:
+ *   - Laura Schlimmer <laura@eventql.io>
+ *   - Paul Asmuth <paul@eventql.io>
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License ("the license") as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or any later version.
+ *
+ * In accordance with Section 7(e) of the license, the licensing of the Program
+ * under the license does not imply a trademark license. Therefore any rights,
+ * title and interest in our trademarks remain entirely with us.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the license for more details.
+ *
+ * You can be released from the requirements of the license by purchasing a
+ * commercial license. Buying such a license is mandatory as soon as you develop
+ * commercial activities involving this program without disclosing the source
+ * code of your own applications
+ */
+#include "eventql/sql/scheduler/aggregation_scheduler.h"
+
+namespace eventql {
+
+
+//void Aggregatio::addLocalPart(
+//    const csql::GroupByNode* query);
+//
+void AggregationScheduler::addRemotePart(
+    const csql::GroupByNode* query,
+    const std::vector<std::string>& hosts) {
+
+}
+
+void AggregationScheduler::setResultCallback(
+    const std::function<void ()> fn) {
+
+}
+
+ReturnCode AggregationScheduler::execute() {
+  return ReturnCode::success();
+}
+
+
+//void sendFrame();
+//void recvFrame();
+
+} // namespace eventql
+
+
