6b89649de10b95ea16ea58c93ecaac20c427830e
Paul Asmuth <paul@paulasmuth.com>
2016-09-11 19:18:20 +0200

    more partition split unit tests

--- test/unit/metadata_operation_split.cc
+++ test/unit/metadata_operation_split.cc
@@ -444,6 +444,168 @@ TEST_CASE(MetadataOperationSplit, TestSplitEnd, [] () {
   EXPECT_EQ(output2[4].servers[2].placement_id, 17);
 });
 
+TEST_CASE(MetadataOperationSplit, TestFiniteSplitBegin, [] () {
+  Vector<MetadataFile::PartitionMapEntry> pmap;
+
+  {
+    MetadataFile::PartitionMapEntry e;
+    e.begin = encodePartitionKey(KEYSPACE_UINT64, "1");
+    e.end = encodePartitionKey(KEYSPACE_UINT64, "3");
+    e.partition_id = SHA1::compute("A");
+    e.splitting = false;
+    pmap.emplace_back(e);
+  }
+
+  {
+    MetadataFile::PartitionMapEntry e;
+    e.begin = encodePartitionKey(KEYSPACE_UINT64, "3");
+    e.end = encodePartitionKey(KEYSPACE_UINT64, "5");
+    e.partition_id = SHA1::compute("B");
+    e.splitting = false;
+    pmap.emplace_back(e);
+  }
+
+  {
+    MetadataFile::PartitionMapEntry e;
+    e.begin = encodePartitionKey(KEYSPACE_UINT64, "8");
+    e.end = encodePartitionKey(KEYSPACE_UINT64, "12");
+    e.partition_id = SHA1::compute("C");
+    e.splitting = false;
+    pmap.emplace_back(e);
+  }
+
+  {
+    MetadataFile::PartitionMapEntry e;
+    e.begin = encodePartitionKey(KEYSPACE_UINT64, "14");
+    e.end = encodePartitionKey(KEYSPACE_UINT64, "99");
+    e.partition_id = SHA1::compute("D");
+    e.splitting = false;
+    pmap.emplace_back(e);
+  }
+
+  MetadataFile input(
+      SHA1::compute("mytx"),
+      0,
+      KEYSPACE_UINT64,
+      pmap,
+      MFILE_FINITE);
+
+  auto split_partition_id = SHA1::compute("A");
+  auto split_partition_id_low = SHA1::compute("X1");
+  auto split_partition_id_high = SHA1::compute("X2");
+
+  SplitPartitionOperation op;
+  op.set_partition_id(split_partition_id.data(), split_partition_id.size());
+  op.set_split_point(encodePartitionKey(KEYSPACE_UINT64, "2"));
+  op.set_split_partition_id_low(
+      split_partition_id_low.data(),
+      split_partition_id_low.size());
+  op.set_split_partition_id_high(
+      split_partition_id_high.data(),
+      split_partition_id_high.size());
+  op.set_placement_id(42);
+  *op.add_split_servers_low() = "l1";
+  *op.add_split_servers_low() = "l2";
+  *op.add_split_servers_low() = "l3";
+  *op.add_split_servers_high() = "h1";
+  *op.add_split_servers_high() = "h2";
+  *op.add_split_servers_high() = "h3";
+
+  std::string opdata_buf;
+  op.SerializeToString(&opdata_buf);
+
+  MetadataOperation openv(
+      "test",
+      "test",
+      METAOP_SPLIT_PARTITION,
+      SHA1::compute("mytx"),
+      SHA1::compute("mytxout"),
+      Buffer(opdata_buf));
+
+  Vector<MetadataFile::PartitionMapEntry> output;
+  auto rc = openv.perform(input, &output);
+  EXPECT(rc.isSuccess());
+  EXPECT_EQ(output.size(), 4);
+  EXPECT_EQ(output[0].begin, encodePartitionKey(KEYSPACE_UINT64, "1"));
+  EXPECT_EQ(output[0].end, encodePartitionKey(KEYSPACE_UINT64, "3"));
+  EXPECT_EQ(output[0].partition_id, SHA1::compute("A"));
+  EXPECT_EQ(output[0].splitting, true);
+  EXPECT_EQ(output[1].begin, encodePartitionKey(KEYSPACE_UINT64, "3"));
+  EXPECT_EQ(output[1].end, encodePartitionKey(KEYSPACE_UINT64, "5"));
+  EXPECT_EQ(output[1].partition_id, SHA1::compute("B"));
+  EXPECT_EQ(output[1].splitting, false);
+  EXPECT_EQ(output[2].begin, encodePartitionKey(KEYSPACE_UINT64, "8"));
+  EXPECT_EQ(output[2].end, encodePartitionKey(KEYSPACE_UINT64, "12"));
+  EXPECT_EQ(output[2].partition_id, SHA1::compute("C"));
+  EXPECT_EQ(output[2].splitting, false);
+  EXPECT_EQ(output[3].begin, encodePartitionKey(KEYSPACE_UINT64, "14"));
+  EXPECT_EQ(output[3].end, encodePartitionKey(KEYSPACE_UINT64, "99"));
+  EXPECT_EQ(output[3].partition_id, SHA1::compute("D"));
+  EXPECT_EQ(output[3].splitting, false);
+
+  MetadataFile input2(
+      SHA1::compute("mytx2"),
+      0,
+      KEYSPACE_UINT64,
+      output,
+      MFILE_FINITE);
+
+  FinalizeSplitOperation op2;
+  op2.set_partition_id(split_partition_id.data(), split_partition_id.size());
+
+  std::string opdata_buf2;
+  op2.SerializeToString(&opdata_buf2);
+
+  MetadataOperation openv2(
+      "test",
+      "test",
+      METAOP_FINALIZE_SPLIT,
+      SHA1::compute("mytx2"),
+      SHA1::compute("mytxout2"),
+      Buffer(opdata_buf2));
+
+  EXPECT(input2.hasFinitePartitions() == true);
+
+  Vector<MetadataFile::PartitionMapEntry> output2;
+  rc = openv2.perform(input2, &output2);
+  EXPECT(rc.isSuccess());
+  EXPECT_EQ(output2.size(), 5);
+  EXPECT_EQ(output2[0].begin, encodePartitionKey(KEYSPACE_UINT64, "1"));
+  EXPECT_EQ(output2[0].end, encodePartitionKey(KEYSPACE_UINT64, "2"));
+  EXPECT_EQ(output2[0].partition_id, SHA1::compute("X1"));
+  EXPECT_EQ(output2[0].splitting, false);
+  EXPECT_EQ(output2[0].servers.size(), 3);
+  EXPECT_EQ(output2[0].servers[0].server_id, "l1");
+  EXPECT_EQ(output2[0].servers[0].placement_id, 42);
+  EXPECT_EQ(output2[0].servers[1].server_id, "l2");
+  EXPECT_EQ(output2[0].servers[1].placement_id, 42);
+  EXPECT_EQ(output2[0].servers[2].server_id, "l3");
+  EXPECT_EQ(output2[0].servers[2].placement_id, 42);
+  EXPECT_EQ(output2[1].begin, encodePartitionKey(KEYSPACE_UINT64, "2"));
+  EXPECT_EQ(output2[1].end, encodePartitionKey(KEYSPACE_UINT64, "3"));
+  EXPECT_EQ(output2[1].partition_id, SHA1::compute("X2"));
+  EXPECT_EQ(output2[1].splitting, false);
+  EXPECT_EQ(output2[1].servers.size(), 3);
+  EXPECT_EQ(output2[1].servers[0].server_id, "h1");
+  EXPECT_EQ(output2[1].servers[0].placement_id, 42);
+  EXPECT_EQ(output2[1].servers[1].server_id, "h2");
+  EXPECT_EQ(output2[1].servers[1].placement_id, 42);
+  EXPECT_EQ(output2[1].servers[2].server_id, "h3");
+  EXPECT_EQ(output2[1].servers[2].placement_id, 42);
+  EXPECT_EQ(output2[2].partition_id, SHA1::compute("B"));
+  EXPECT_EQ(output2[2].begin, encodePartitionKey(KEYSPACE_UINT64, "3"));
+  EXPECT_EQ(output2[2].end, encodePartitionKey(KEYSPACE_UINT64, "5"));
+  EXPECT_EQ(output2[2].splitting, false);
+  EXPECT_EQ(output2[3].partition_id, SHA1::compute("C"));
+  EXPECT_EQ(output2[3].begin, encodePartitionKey(KEYSPACE_UINT64, "8"));
+  EXPECT_EQ(output2[3].end, encodePartitionKey(KEYSPACE_UINT64, "12"));
+  EXPECT_EQ(output2[3].splitting, false);
+  EXPECT_EQ(output2[4].partition_id, SHA1::compute("D"));
+  EXPECT_EQ(output2[4].begin, encodePartitionKey(KEYSPACE_UINT64, "14"));
+  EXPECT_EQ(output2[4].end, encodePartitionKey(KEYSPACE_UINT64, "99"));
+  EXPECT_EQ(output2[4].splitting, false);
+});
+
 TEST_CASE(MetadataOperationSplit, TestFiniteSplitMiddle, [] () {
   Vector<MetadataFile::PartitionMapEntry> pmap;
 
@@ -604,5 +766,167 @@ TEST_CASE(MetadataOperationSplit, TestFiniteSplitMiddle, [] () {
   EXPECT_EQ(output2[4].end, encodePartitionKey(KEYSPACE_UINT64, "99"));
   EXPECT_EQ(output2[4].partition_id, SHA1::compute("D"));
   EXPECT_EQ(output2[4].splitting, false);
+});
+
+TEST_CASE(MetadataOperationSplit, TestFiniteSplitEnd, [] () {
+  Vector<MetadataFile::PartitionMapEntry> pmap;
+
+  {
+    MetadataFile::PartitionMapEntry e;
+    e.begin = encodePartitionKey(KEYSPACE_UINT64, "1");
+    e.end = encodePartitionKey(KEYSPACE_UINT64, "3");
+    e.partition_id = SHA1::compute("A");
+    e.splitting = false;
+    pmap.emplace_back(e);
+  }
+
+  {
+    MetadataFile::PartitionMapEntry e;
+    e.begin = encodePartitionKey(KEYSPACE_UINT64, "3");
+    e.end = encodePartitionKey(KEYSPACE_UINT64, "5");
+    e.partition_id = SHA1::compute("B");
+    e.splitting = false;
+    pmap.emplace_back(e);
+  }
+
+  {
+    MetadataFile::PartitionMapEntry e;
+    e.begin = encodePartitionKey(KEYSPACE_UINT64, "8");
+    e.end = encodePartitionKey(KEYSPACE_UINT64, "12");
+    e.partition_id = SHA1::compute("C");
+    e.splitting = false;
+    pmap.emplace_back(e);
+  }
+
+  {
+    MetadataFile::PartitionMapEntry e;
+    e.begin = encodePartitionKey(KEYSPACE_UINT64, "14");
+    e.end = encodePartitionKey(KEYSPACE_UINT64, "99");
+    e.partition_id = SHA1::compute("D");
+    e.splitting = false;
+    pmap.emplace_back(e);
+  }
+
+  MetadataFile input(
+      SHA1::compute("mytx"),
+      0,
+      KEYSPACE_UINT64,
+      pmap,
+      MFILE_FINITE);
+
+  auto split_partition_id = SHA1::compute("D");
+  auto split_partition_id_low = SHA1::compute("X1");
+  auto split_partition_id_high = SHA1::compute("X2");
+
+  SplitPartitionOperation op;
+  op.set_partition_id(split_partition_id.data(), split_partition_id.size());
+  op.set_split_point(encodePartitionKey(KEYSPACE_UINT64, "50"));
+  op.set_split_partition_id_low(
+      split_partition_id_low.data(),
+      split_partition_id_low.size());
+  op.set_split_partition_id_high(
+      split_partition_id_high.data(),
+      split_partition_id_high.size());
+  op.set_placement_id(42);
+  *op.add_split_servers_low() = "l1";
+  *op.add_split_servers_low() = "l2";
+  *op.add_split_servers_low() = "l3";
+  *op.add_split_servers_high() = "h1";
+  *op.add_split_servers_high() = "h2";
+  *op.add_split_servers_high() = "h3";
+
+  std::string opdata_buf;
+  op.SerializeToString(&opdata_buf);
+
+  MetadataOperation openv(
+      "test",
+      "test",
+      METAOP_SPLIT_PARTITION,
+      SHA1::compute("mytx"),
+      SHA1::compute("mytxout"),
+      Buffer(opdata_buf));
+
+  Vector<MetadataFile::PartitionMapEntry> output;
+  auto rc = openv.perform(input, &output);
+  EXPECT(rc.isSuccess());
+  EXPECT_EQ(output.size(), 4);
+  EXPECT_EQ(output[0].begin, encodePartitionKey(KEYSPACE_UINT64, "1"));
+  EXPECT_EQ(output[0].end, encodePartitionKey(KEYSPACE_UINT64, "3"));
+  EXPECT_EQ(output[0].partition_id, SHA1::compute("A"));
+  EXPECT_EQ(output[0].splitting, false);
+  EXPECT_EQ(output[1].begin, encodePartitionKey(KEYSPACE_UINT64, "3"));
+  EXPECT_EQ(output[1].end, encodePartitionKey(KEYSPACE_UINT64, "5"));
+  EXPECT_EQ(output[1].partition_id, SHA1::compute("B"));
+  EXPECT_EQ(output[1].splitting, false);
+  EXPECT_EQ(output[2].begin, encodePartitionKey(KEYSPACE_UINT64, "8"));
+  EXPECT_EQ(output[2].end, encodePartitionKey(KEYSPACE_UINT64, "12"));
+  EXPECT_EQ(output[2].partition_id, SHA1::compute("C"));
+  EXPECT_EQ(output[2].splitting, false);
+  EXPECT_EQ(output[3].begin, encodePartitionKey(KEYSPACE_UINT64, "14"));
+  EXPECT_EQ(output[3].end, encodePartitionKey(KEYSPACE_UINT64, "99"));
+  EXPECT_EQ(output[3].partition_id, SHA1::compute("D"));
+  EXPECT_EQ(output[3].splitting, true);
+
+  MetadataFile input2(
+      SHA1::compute("mytx2"),
+      0,
+      KEYSPACE_UINT64,
+      output,
+      MFILE_FINITE);
+
+  FinalizeSplitOperation op2;
+  op2.set_partition_id(split_partition_id.data(), split_partition_id.size());
+
+  std::string opdata_buf2;
+  op2.SerializeToString(&opdata_buf2);
+
+  MetadataOperation openv2(
+      "test",
+      "test",
+      METAOP_FINALIZE_SPLIT,
+      SHA1::compute("mytx2"),
+      SHA1::compute("mytxout2"),
+      Buffer(opdata_buf2));
+
+  EXPECT(input2.hasFinitePartitions() == true);
+
+  Vector<MetadataFile::PartitionMapEntry> output2;
+  rc = openv2.perform(input2, &output2);
+  EXPECT(rc.isSuccess());
+  EXPECT_EQ(output2.size(), 5);
+  EXPECT_EQ(output2[0].begin, encodePartitionKey(KEYSPACE_UINT64, "1"));
+  EXPECT_EQ(output2[0].end, encodePartitionKey(KEYSPACE_UINT64, "3"));
+  EXPECT_EQ(output2[0].partition_id, SHA1::compute("A"));
+  EXPECT_EQ(output2[0].splitting, false);
+  EXPECT_EQ(output2[1].begin, encodePartitionKey(KEYSPACE_UINT64, "3"));
+  EXPECT_EQ(output2[1].end, encodePartitionKey(KEYSPACE_UINT64, "5"));
+  EXPECT_EQ(output2[1].partition_id, SHA1::compute("B"));
+  EXPECT_EQ(output2[1].splitting, false);
+  EXPECT_EQ(output2[2].begin, encodePartitionKey(KEYSPACE_UINT64, "8"));
+  EXPECT_EQ(output2[2].end, encodePartitionKey(KEYSPACE_UINT64, "12"));
+  EXPECT_EQ(output2[2].partition_id, SHA1::compute("C"));
+  EXPECT_EQ(output2[2].splitting, false);
+  EXPECT_EQ(output2[3].begin, encodePartitionKey(KEYSPACE_UINT64, "14"));
+  EXPECT_EQ(output2[3].end, encodePartitionKey(KEYSPACE_UINT64, "50"));
+  EXPECT_EQ(output2[3].partition_id, SHA1::compute("X1"));
+  EXPECT_EQ(output2[3].splitting, false);
+  EXPECT_EQ(output2[3].servers.size(), 3);
+  EXPECT_EQ(output2[3].servers[0].server_id, "l1");
+  EXPECT_EQ(output2[3].servers[0].placement_id, 42);
+  EXPECT_EQ(output2[3].servers[1].server_id, "l2");
+  EXPECT_EQ(output2[3].servers[1].placement_id, 42);
+  EXPECT_EQ(output2[3].servers[2].server_id, "l3");
+  EXPECT_EQ(output2[3].servers[2].placement_id, 42);
+  EXPECT_EQ(output2[4].begin, encodePartitionKey(KEYSPACE_UINT64, "50"));
+  EXPECT_EQ(output2[4].end, encodePartitionKey(KEYSPACE_UINT64, "99"));
+  EXPECT_EQ(output2[4].partition_id, SHA1::compute("X2"));
+  EXPECT_EQ(output2[4].splitting, false);
+  EXPECT_EQ(output2[4].servers.size(), 3);
+  EXPECT_EQ(output2[4].servers[0].server_id, "h1");
+  EXPECT_EQ(output2[4].servers[0].placement_id, 42);
+  EXPECT_EQ(output2[4].servers[1].server_id, "h2");
+  EXPECT_EQ(output2[4].servers[1].placement_id, 42);
+  EXPECT_EQ(output2[4].servers[2].server_id, "h3");
+  EXPECT_EQ(output2[4].servers[2].placement_id, 42);
 });
 
