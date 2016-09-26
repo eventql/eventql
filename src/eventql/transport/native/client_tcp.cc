40197633da8ee9aaa5ec7c27fd19dc3fa6f9b7b0
Paul Asmuth <paul@paulasmuth.com>
2016-09-26 18:40:35 +0200

    reduce max connection pool size if ulimit is too small

--- src/eventql/transport/native/client_tcp.cc
+++ src/eventql/transport/native/client_tcp.cc
@@ -33,6 +33,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <poll.h>
+#include <sys/resource.h>
 #include "eventql/util/logging.h"
 #include "eventql/util/wallclock.h"
 #include "eventql/util/util/binarymessagereader.h"
@@ -838,13 +839,37 @@ void TCPAsyncClient::closeConnection(Connection* connection, bool graceful) {
 }
 
 TCPConnectionPool::TCPConnectionPool(
-  uint64_t max_conns,
-  uint64_t max_conns_per_host,
-  uint64_t max_conn_age) :
-  max_conns_(max_conns),
-  max_conns_per_host_(max_conns_per_host),
-  max_conn_age_(max_conn_age),
-  num_conns_(0) {}
+    uint64_t max_conns,
+    uint64_t max_conns_per_host,
+    uint64_t max_conn_age) :
+    max_conns_(max_conns),
+    max_conns_per_host_(max_conns_per_host),
+    max_conn_age_(max_conn_age),
+    num_conns_(0) {
+  struct rlimit fd_limit;
+  memset(&fd_limit, 0, sizeof(fd_limit));
+  ::getrlimit(RLIMIT_NOFILE, &fd_limit);
+
+  static const size_t kReservedFDs = 64;
+  size_t maxfds = fd_limit.rlim_cur;
+  if (maxfds > kReservedFDs) {
+    maxfds -= kReservedFDs;
+  } else {
+    maxfds = 0;
+  }
+
+  if (maxfds < max_conns_) {
+    logWarning(
+        "evlqd",
+        "RLIMIT_NOFILE is too small ($0), reducing maximum connection pool "
+        "size from $1 to $2. Consider increasing the file descriptor limit.",
+        fd_limit.rlim_cur,
+        max_conns_,
+        maxfds);
+
+    max_conns_ = maxfds;
+  }
+}
 
 bool TCPConnectionPool::getConnection(
     const std::string& addr,
