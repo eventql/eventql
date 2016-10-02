5ab5106f164a23a0f30973f8ed41acf3a41412d5
Paul Asmuth <paul@paulasmuth.com>
2016-10-02 19:23:13 +0200

    build with -fno-builtin-malloc

--- build/scripts/build_linux_x86_64.sh
+++ build/scripts/build_linux_x86_64.sh
@@ -26,7 +26,7 @@ export NM=x86_64-linux-gnu-nm
 export STRIP=x86_64-linux-gnu-strip
 export RANLIB=x86_64-linux-gnu-ranlib
 export OBJDUMP=x86_64-linux-gnu-objdump
-export CXXFLAGS="-static-libstdc++ -static-libgcc"
+export CXXFLAGS="-static-libstdc++ -static-libgcc -fno-builtin-malloc -fno-builtin-calloc -fno-builtin-realloc -fno-builtin-free"
 export LDFLAGS="-ltcmalloc"
 
 ../../../$PACKAGE-$VERSION/configure --host=x86_64-linux-gnu --prefix=/usr/local
