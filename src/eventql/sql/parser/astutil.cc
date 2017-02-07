2feae18da59b4538f6f11fabee8c093da4b90aff
Paul Asmuth <paul@asmuth.com>
2017-02-07 21:46:38 +0100

    move sql/backends -> sql/drivers, remove dead code

--- src/eventql/sql/parser/astutil.cc
+++ src/eventql/sql/parser/astutil.cc
@@ -29,52 +29,6 @@
 
 namespace csql {
 
-std::vector<std::string> ASTUtil::columnNamesFromSelectList(
-    ASTNode* select_list,
-    TableRef* tbl_ref /* = nullptr */) {
-  std::vector<std::string> column_names;
-  for (auto col : select_list->getChildren()) {
-    if (col->getType() != ASTNode::T_DERIVED_COLUMN) {
-      RAISE(kRuntimeError, "corrupt AST");
-    }
-
-    auto derived = col->getChildren();
-
-    // column with AS clause
-    if (derived.size() == 2) {
-      if (derived[1]->getType() != ASTNode::T_COLUMN_ALIAS) {
-        RAISE(kRuntimeError, "corrupt AST");
-      }
-
-      auto colname_token = derived[1]->getToken();
-
-      if (!(colname_token && *colname_token == Token::T_IDENTIFIER)) {
-        RAISE(kRuntimeError, "corrupt AST");
-      }
-
-      column_names.emplace_back(colname_token->getString());
-      continue;
-    }
-
-    // resolved column name
-    if (derived.size() == 1 && *derived[0] == ASTNode::T_RESOLVED_COLUMN) {
-      if (tbl_ref == nullptr) {
-        column_names.emplace_back("col" + std::to_string(derived[0]->getID()));
-      } else {
-        auto col_name = tbl_ref->getColumnName(derived[0]->getID());
-        column_names.emplace_back(col_name);
-      }
-
-      continue;
-    }
-
-    // expression
-    column_names.emplace_back("<expr>"); // FIXPAUL!!
-  }
-
-  return column_names;
-}
-
 String ASTUtil::columnNameForExpression(ASTNode* expr) {
   switch (expr->getType()) {
 
