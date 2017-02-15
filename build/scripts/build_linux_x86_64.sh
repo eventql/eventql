47712af8cf92fee7c8f552d8433de3692034a4a5
Paul Asmuth <paul@asmuth.com>
2017-02-15 21:39:11 +0100

    build x86 linux with -Ofast and friends

--- build/scripts/build_linux_x86_64.sh
+++ build/scripts/build_linux_x86_64.sh
@@ -26,7 +26,7 @@ export NM=x86_64-linux-gnu-nm
 export STRIP=x86_64-linux-gnu-strip
 export RANLIB=x86_64-linux-gnu-ranlib
 export OBJDUMP=x86_64-linux-gnu-objdump
-export CXXFLAGS="-static-libstdc++ -static-libgcc"
+export CXXFLAGS="-static-libstdc++ -static-libgcc -ffast-math -mfpmath=sse -Ofast -flto -march=native -funroll-loops -DNDEBUG"
 export LDFLAGS=""
 
 ../../../$PACKAGE-$VERSION/configure --host=x86_64-linux-gnu --prefix=/usr/local
