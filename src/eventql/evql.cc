a910f2a4abc6b0b2181cc7b486828c89e361066e
Laura Schlimmer <laura@zscale.io>
2017-02-27 13:11:14 +0100

    cleanup

--- src/eventql/evql.cc
+++ src/eventql/evql.cc
@@ -346,7 +346,6 @@ int main(int argc, const char** argv) {
     cfg_builder.setProperty("client.quiet", "true");
   }
 
-
   /* cli config */
   eventql::cli::CLIConfig cli_cfg(cfg_builder.getConfig());
 
